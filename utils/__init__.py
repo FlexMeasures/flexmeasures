c639b29cc2e0683689125af740eb62a801ac285c
nhoening <iam@nicolashoening.de>
2018-02-22 23:02:22 +0100

    refactoring new locations and names, not all view code working again yet

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+"""Utilities for the BVP project."""
---
+++ utils/calculations.py
@@ -0,0 +1,20 @@
+""" Calculations """
+
+import numpy as np
+
+
+def mean_absolute_error(y_true, y_forecast):
+    y_true, y_forecast = np.array(y_true), np.array(y_forecast)
+    return np.mean(np.abs((y_true - y_forecast)))
+
+
+def mean_absolute_percentage_error(y_true, y_forecast):
+    y_true, y_forecast = np.array(y_true), np.array(y_forecast)
+    return np.mean(np.abs((y_true - y_forecast) / y_true)) * 100
+
+
+def weighted_absolute_percentage_error(y_true, y_forecast):
+    y_true, y_forecast = np.array(y_true), np.array(y_forecast)
+    return np.sum(np.abs((y_true - y_forecast))) / np.sum(y_true) * 100
+
+
