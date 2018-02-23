af69f8d11c33caf51f7018bc9e45755b06570e81
Claessen <claessen@seita.nl>
2018-02-23 18:20:50 +0100

    Hover tool fixed for day and week resolution, Hover tool reflects that data refers to time slots rather than time stamps

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from flask import current_app
 from bokeh.models import Range1d
 from bokeh.plotting import figure, Figure
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
@@ -10,15 +11,16 @@ import numpy as np
 def create_hover_tool(y_unit: str, resolution: str) -> HoverTool:
     """Describe behaviour of default tooltips
     (we could also return html for custom tooltips)"""
-    date_format = "@X{%F}"
+    date_format = "@x{%F} to @next_x{%F}"
     if resolution in ("15T", "1h"):
-        date_format = "@x{%F %H:%M}"
+        date_format = "@x{%F %H:%M} to @next_x{%F %H:%M}"
 
     return HoverTool(tooltips=[
         ('Time', date_format),
         ('Value', "@y{0.000a} %s" % y_unit),
     ], formatters={
         "x": "datetime",
+        "next_x": "datetime",
         "y": "numeral"
     })
 
@@ -39,15 +41,18 @@ def create_graph(series: pd.Series, forecasts: pd.DataFrame = None,
     :param show_y_floats: if True, y axis will show floating numbers (defaults False, will be True if y values are < 2)
     :return: a Bokeh Figure
     """
+
+    # set the x axis range
     xdr = None
-    if series.size > 0:
+    if series.size > 0:  # if there is some actual data, use that to set the range
         xdr = Range1d(start=min(series.index), end=max(series.index))
-    if forecasts is not None:
+    if forecasts is not None:  # if there is some forecast data where no actual data is available, use that
         xdr = Range1d(start=min(series.index.append(forecasts.index)),
                       end=max(series.index.append(forecasts.index)))
     if xdr is None:
-        raise Exception("Not sufficient data to show anything.")
+        current_app.logger.warn("Not sufficient data to show anything.")
 
+    # set tools
     tools = ["box_zoom", "reset", "save"]
     if hover_tool is not None:
         tools = [hover_tool] + tools
@@ -58,8 +63,13 @@ def create_graph(series: pd.Series, forecasts: pd.DataFrame = None,
         else:
             show_y_floats = max(max(series.values), max(forecasts.yhat)) < 2
 
-    data_source = ColumnDataSource(dict(x=series.index.values, y=series.values))
-
+    x = series.index.values
+    if x.size and series.index.freq is not None:  # i.e. if there is data and with a clearly defined frequency
+        next_x = pd.DatetimeIndex(start=x[1], freq=series.index.freq, periods=len(series)).values
+    else:
+        next_x = []
+    y = series.values
+    data_source = ColumnDataSource(dict(x=x, next_x=next_x, y=y))
     fig = figure(title=title,
                  x_range=xdr,
                  min_border=0,
