63ddae1aa4e3fc90b3bda4714be80f70a8c198a6
nhoening <iam@nicolashoening.de>
2018-04-30 13:04:50 +0200

    asset viewing/editing is not authorized for anyone but the owner of the asset or admins

--- bvp/crud/assets.py
+++ bvp/crud/assets.py
@@ -2,7 +2,7 @@ from flask_classful import FlaskView
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired
-from flask_security import login_required
+from flask_security import login_required, current_user
 
 from bvp.utils.data_access import get_assets
 from bvp.utils.view_utils import render_bvp_template
@@ -32,6 +32,8 @@ class AssetCrud(FlaskView):
         """GET from /assets/<id>"""
         asset: Asset = Asset.query.filter_by(id=int(id)).one_or_none()
         if asset is not None:
+            if asset.owner != current_user and not current_user.has_role("admin"):
+                return "Unauthorized", 401
             asset_form = AssetForm()
             asset_form.process(obj=asset)
             return render_bvp_template("crud/asset.html", asset=asset, asset_form=asset_form, msg="")
@@ -42,8 +44,14 @@ class AssetCrud(FlaskView):
     def post(self, id: str):
         """POST to /assets/<id>"""
         asset: Asset = Asset.query.filter_by(id=int(id)).one_or_none()
-        asset_form = AssetForm()
-        asset_form.populate_obj(asset)
-        db.session.add(asset)
-        db.session.commit()
-        return render_bvp_template("crud/asset.html", asset=asset, asset_form=asset_form, msg="Saving was successful.")
+        if asset is not None:
+            if asset.owner != current_user and not current_user.has_role("admin"):
+                return "Unauthorized", 401
+            asset_form = AssetForm()
+            asset_form.populate_obj(asset)
+            db.session.add(asset)
+            db.session.commit()
+            return render_bvp_template("crud/asset.html", asset=asset, asset_form=asset_form,
+                                       msg="Editing was successful.")
+        else:
+            return "Not Found", 404
