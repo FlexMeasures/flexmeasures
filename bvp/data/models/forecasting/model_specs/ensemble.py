380d6fa3c50d15a564d9c35afab2bdafebad2a30
Felix Claessen <felix@seita.nl>
2019-12-13 19:00:13 +0100

    Tune ensemble hyper-parameters.

--- bvp/data/models/forecasting/model_specs/ensemble.py
+++ bvp/data/models/forecasting/model_specs/ensemble.py
@@ -10,8 +10,6 @@ from bvp.data.models.forecasting.model_spec_factory import create_initial_model_
 Ensemble models.
 """
 
-# update this version if small things like parametrisation change
-version: int = 1
 # if a forecasting job using this model fails, fall back on this one
 fallback_model_search_term: Optional[str] = "linear-OLS"
 
@@ -19,13 +17,17 @@ fallback_model_search_term: Optional[str] = "linear-OLS"
 def adaboost_decision_tree_specs_configurator(**args):
     """Create and customize initial specs with AdaBoost (decision tree as weak learner).
     See model__spec_factory for param docs."""
+
+    # update this version if small things like parametrisation change
+    version: int = 1
+
     model_specs = create_initial_model_specs(**args)
     model_specs.set_model(
         (
             AdaBoostRegressor,
             dict(
                 base_estimator=DecisionTreeRegressor(max_depth=4),
-                n_estimators=300,
+                n_estimators=100,
                 random_state=np.random.RandomState(1),
             ),
         )
@@ -37,13 +39,17 @@ def adaboost_decision_tree_specs_configurator(**args):
 def bagging_decision_tree_specs_configurator(**args):
     """Create and customize initial specs with Bagging (decision tree as weak learner).
     See model__spec_factory for param docs."""
+
+    # update this version if small things like parametrisation change
+    version: int = 1
+
     model_specs = create_initial_model_specs(**args)
     model_specs.set_model(
         (
             BaggingRegressor,
             dict(
                 base_estimator=DecisionTreeRegressor(max_depth=4),
-                n_estimators=300,
+                n_estimators=100,
                 random_state=np.random.RandomState(1),
             ),
         )
@@ -54,11 +60,21 @@ def bagging_decision_tree_specs_configurator(**args):
 
 def random_forest_specs_configurator(**args):
     """Create and customize initial specs with Random Forest. See model__spec_factory for param docs."""
+
+    # update this version if small things like parametrisation change
+    version: int = 1
+
     model_specs = create_initial_model_specs(**args)
     model_specs.set_model(
         (
             RandomForestRegressor,
-            dict(n_estimators=300, random_state=np.random.RandomState(1),),
+            dict(
+                n_estimators=100,
+                min_samples_split=2,
+                min_samples_leaf=2,
+                random_state=np.random.RandomState(1),
+                oob_score=True,
+            ),
         )
     )
     model_identifier = "Random Forest model (v%d)" % version
