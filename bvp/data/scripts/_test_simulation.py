b8d305d6e6fe5299d52386624e27ea55b854ce6d
Felix Claessen <felix@seita.nl>
2020-02-25 16:22:20 +0100

    Update manual simulation test with use of rq and record CPU usage.

--- bvp/data/scripts/_test_simulation.py
+++ bvp/data/scripts/_test_simulation.py
@@ -1,21 +1,30 @@
 """Tests a small simulation against the BVP running on a server."""
 from datetime import timedelta
+import time
 
 from isodate import parse_datetime
 
 from bvp.data.scripts.simulation_utils import (
     check_version,
     check_services,
+    get_PA_token,
     get_auth_token,
     get_connections,
+    get_cpu_seconds,
     post_meter_data,
     post_price_forecasts,
-    post_weather_data,
+    post_weather_forecasts,
 )
 
 
 # Setup
-server = "local"
+server = "staging"
+num_days_sim = 140
+batch_size = 96  # number of 15-minute meter readings sent in one POST request (we recommend 96, i.e. daily batches)
+automate_simulation = True
+seconds_of_sleep_between_steps = (
+    61  # Python Anywhere recommends > 60 (due to CPU budget update frequency)
+)
 
 if server == "play":
     host = "https://play.a1-bvp.com"
@@ -23,6 +32,7 @@ elif server == "staging":
     host = "https://staging.a1-bvp.com"
 else:
     host = "http://localhost:5000"
+PA_API_TOKEN = get_PA_token()
 
 latest_version = check_version(host)
 services = check_services(host, latest_version)
@@ -30,50 +40,90 @@ auth_token = get_auth_token(host)
 connections = get_connections(host, latest_version, auth_token)
 
 # Initialisation
-num_days = 50
-sim_start = "2018-01-01T00:00:00+09:00"
+num_days_init = 50
+sim_start = "2050-01-01T00:00:00+09:00"
 post_price_forecasts(
-    host, latest_version, auth_token, start=parse_datetime(sim_start), num_days=num_days
+    host,
+    latest_version,
+    auth_token,
+    start=parse_datetime(sim_start),
+    num_days=num_days_init + num_days_sim,
 )
-post_weather_data(
-    host, latest_version, auth_token, start=parse_datetime(sim_start), num_days=num_days
+post_weather_forecasts(
+    host,
+    latest_version,
+    auth_token,
+    start=parse_datetime(sim_start),
+    num_days=num_days_init + num_days_sim,
 )
 post_meter_data(
     host,
     latest_version,
     auth_token,
     start=parse_datetime(sim_start),
-    num_days=num_days,
+    batch_size=num_days_init * 96,
     connection=connections[1],
 )
 
-# Run forecasting jobs
-input(
-    "Run all forecasting jobs, then press Enter to continue ...\n"
-    "You can run this in another bvp-venv shell:\n\n"
-    "flask run_forecasting_worker\n"
-)
-
-# Steps
-num_steps = 10
-for i in range(num_steps):
-    print("Running step %s out of %s" % (i, num_steps))
-
-    post_weather_data(
-        host,
-        latest_version,
-        auth_token,
-        start=parse_datetime(sim_start) + timedelta(days=num_days + i),
-        num_days=1,
+if automate_simulation is False:
+    input(
+        f"Finished initialising simulation on {server}."
+        f"Go to {host}/rq/forecasting to check forecasting jobs have been created."
+        f"Assign a worker to process these jobs, then press Enter to continue ...\n"
+        "You can run a local worker in another bvp-venv shell:\n\n"
+        'flask run-worker --name "RQ worker [SIM]" --queue "forecasting"\n\n'
+        f"Or assign a continuous task on PA on work on {server}:\n\n"
+        f'go {server} && flask run-worker --name "RQ worker [SIM]" --queue "forecasting"\n\n'
     )
+else:
+    time.sleep(seconds_of_sleep_between_steps)
+
+# Simulation steps
+for i in range(num_days_sim * 96 // batch_size):
+    if automate_simulation is False:
+        print("Sending batch %s out of %s" % (i + 1, num_days_sim * 96 // batch_size))
+
+    # post_weather_data(
+    #     host,
+    #     latest_version,
+    #     auth_token,
+    #     start=parse_datetime(sim_start) + timedelta(days=num_days_init + i),
+    #     num_days=1,
+    # )
     post_meter_data(
         host,
         latest_version,
         auth_token,
-        start=parse_datetime(sim_start) + timedelta(days=num_days + i),
-        num_days=1,
+        start=parse_datetime(sim_start)
+        + timedelta(days=num_days_init, seconds=i * batch_size * 15 * 60),
+        batch_size=batch_size,  # send meter data in batches of a certain size, e.g. 96 values sent every 24 hours
         connection=connections[1],
     )
 
     # Run forecasting jobs
-    input("Run all forecasting jobs again and press enter to continue..\n")
+    cpu_seconds_before, cpu_seconds_budget = get_cpu_seconds(
+        "seita", PA_API_TOKEN
+    )  # Track CPU time spent
+    if automate_simulation is False:
+        input(
+            f"Finished sending simulation to {server}. "
+            f"Go to {host}/rq/forecasting to check forecasting jobs have been created. "
+            f"Assign a worker to process these jobs, wait at least 60 seconds for CPU seconds budget on PA to update, then press Enter to continue ...\n"
+            "You can run a local worker in another bvp-venv shell:\n\n"
+            'flask run-worker --name "RQ worker [SIM]" --queue "forecasting"\n\n'
+            f"Or assign a continuous task on PA on work on {server}:\n\n"
+            f'go {server} && flask run-worker --name "RQ worker [SIM]" --queue "forecasting"\n\n'
+        )
+    else:
+        time.sleep(seconds_of_sleep_between_steps)
+    cpu_seconds_after, _ = get_cpu_seconds("seita", PA_API_TOKEN)
+    print(
+        f"Batch {i+1} out of {num_days_sim * 96 // batch_size} - CPU seconds spent on forecasting: {round(cpu_seconds_after - cpu_seconds_before, 3)} out of {cpu_seconds_budget}"
+    )
+
+print(
+    f"Don't forget to delete your simulation results from the {server} database! Using:\n\n"
+    f"delete from power where datetime > '2049-12-30';\n"
+    f"delete from price where datetime > '2049-12-30';\n"
+    f"delete from weather where datetime > '2049-12-30';\n"
+)
--- bvp/data/scripts/simulation_utils.py
+++ bvp/data/scripts/simulation_utils.py
@@ -2,12 +2,33 @@ from typing import List
 import requests
 from random import random
 from datetime import datetime, timedelta
+import time
+import os
 
 from numpy import sin, tile
 from isodate import duration_isoformat, datetime_isoformat
 
 from bvp.api.v1.tests.utils import message_for_post_meter_data
 from bvp.api.v1_1.tests.utils import message_for_post_price_data
+
+
+def get_PA_token() -> str:
+    """Retrieve Python Anywhere token from environment (if set as env variable) or from dev config."""
+    token = os.environ.get("API_TOKEN")
+    if token is None:
+        from bvp.development_config import PA_API_TOKEN as token
+    return token
+
+
+def get_cpu_seconds(username: str, auth_token: str) -> (float, int):
+    response = requests.get(
+        f"https://www.pythonanywhere.com/api/v0/user/{username}/cpu/",
+        headers={"Authorization": f"Token {auth_token}"},
+    )
+    return (
+        response.json()["daily_cpu_total_usage_seconds"],
+        response.json()["daily_cpu_limit_seconds"],
+    )
 
 
 def check_version(host: str) -> str:
@@ -65,14 +86,21 @@ def post_meter_data(
     latest_version: str,
     auth_token: str,
     start: datetime,
-    num_days: int,
+    batch_size: int,
     connection: str,
 ):
     message = message_for_post_meter_data(
-        tile_n=num_days * 16, production=True
-    )  # Original message is just 1.5 hours
+        tile_n=round(batch_size / 6), production=True, single_connection=True
+    )  # Original message is just 6 values (1.5 hours)
     message["start"] = datetime_isoformat(start)
     message["connection"] = connection
+    if batch_size < 6:
+        import random
+
+        message["values"] = random.sample(
+            range(0, -300, -1), batch_size
+        )  # production is negative
+        message["duration"] = duration_isoformat(timedelta(minutes=15 * batch_size))
     response = requests.post(
         "%s/api/%s/postMeterData" % (host, latest_version),
         headers={"Authorization": auth_token},
@@ -97,12 +125,12 @@ def post_price_forecasts(
     assert response.status_code == 200
 
 
-def post_weather_data(
+def post_weather_forecasts(
     host: str, latest_version: str, auth_token: str, start: datetime, num_days: int
 ):
     lat = 33.4843866
-    lng = 126
-    values = [random() * 600 * (1 + sin(x / 15)) for x in range(96 * num_days)]
+    lng = 126.477859
+    values = [round(random() * 600 * (1 + sin(x / 15))) for x in range(96 * num_days)]
     message = {
         "type": "PostWeatherDataRequest",
         "sensor": "%s:%s:%s:%s"
@@ -110,7 +138,7 @@ def post_weather_data(
         "values": tile(values, 1).tolist(),
         "start": datetime_isoformat(start),
         "duration": duration_isoformat(timedelta(hours=24 * num_days)),
-        "horizon": "R/PT0H",
+        "horizon": "R/PT48H",
         "unit": "kW/m²",
     }
     response = requests.post(
@@ -119,3 +147,41 @@ def post_weather_data(
         json=message,
     )
     assert response.status_code == 200
+
+
+class Timer(object):
+    """Usage example:
+
+    >>> with Timer("Calling max function"):
+    >>>     a = max(range(10**6))
+    <<<
+    [Calling max function] Starting (at Friday, October 18, 2019 16:16:18) ...
+    [Calling max function] Elapsed: 35 ms
+    """
+
+    def __init__(self, name=None, filename=None):
+        self.name = name
+        self.filename = filename
+
+    def __enter__(self):
+        self.tstart = time.time()
+        print(
+            "[%s] Starting (at %s) ..."
+            % (
+                self.name,
+                datetime.fromtimestamp(self.tstart).strftime("%A, %B %d, %Y %H:%M:%S"),
+            )
+        )
+
+    def __exit__(self, type, value, traceback):
+        duration = time.time() - self.tstart
+        if duration > 1:
+            message = "Elapsed: %.2f seconds" % duration
+        else:
+            message = "Elapsed: %.0f ms" % (duration * 1000)
+        if self.name:
+            message = "[%s] " % self.name + message
+        print(message)
+        if self.filename:
+            with open(self.filename, "a") as file:
+                print(str(datetime.now()) + ": ", message, file=file)
