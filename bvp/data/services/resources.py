cf18f78d144107498350f78d0f1b98b473fbec78
F.N. Claessen <felix@seita.nl>
2020-12-07 14:35:17 +0100

    Refactor util function for manipulating units of flows and stocks.

--- bvp/data/services/resources.py
+++ bvp/data/services/resources.py
@@ -682,27 +682,3 @@ def group_assets_by_location(asset_list: List[Asset]) -> List[List[Asset]]:
     for _k, g in groupby(asset_list, lambda x: x.location):
         groups.append(list(g))
     return groups
-
-
-def determine_flow_unit(stock_unit: str, time_unit: str = "h"):
-    """For example:
-    >>> determine_flow_unit("m3")  # m3/h
-    >>> determine_flow_unit("kWh")  # kW
-    """
-    return (
-        stock_unit.rpartition(time_unit)[0]
-        if stock_unit.endswith(time_unit)
-        else f"{stock_unit}/{time_unit}"
-    )
-
-
-def determine_stock_unit(flow_unit: str, time_unit: str = "h"):
-    """For example:
-    >>> determine_stock_unit("m3/h")  # m3
-    >>> determine_stock_unit("kW")  # kWh
-    """
-    return (
-        flow_unit.rpartition(f"/{time_unit}")[0]
-        if flow_unit.endswith(f"/{time_unit}")
-        else f"{flow_unit}{time_unit}"
-    )
