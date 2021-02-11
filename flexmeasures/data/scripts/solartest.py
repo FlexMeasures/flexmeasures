1c2f85eaade766de58eb5c4961f4d6c5890ca9f7
F.N. Claessen <felix@seita.nl>
2021-02-11 16:52:26 +0100

    Check solarpy for clear sky irradiance. Add location on southern hemisphere.

--- flexmeasures/data/scripts/solartest.py
+++ flexmeasures/data/scripts/solartest.py
@@ -8,10 +8,9 @@ There might be errors or misunderstandings still in here.
 """
 
 
-import numpy as np
-from solarpy import irradiance_on_plane, standard2solar_time
+from solarpy import irradiance_on_plane, solar_vector_ned, standard2solar_time
 from pvlib import location
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from pandas import DatetimeIndex
 from tzwhere import tzwhere
@@ -21,22 +20,23 @@ from flexmeasures.utils.geo_utils import compute_radiation
 
 tzwhere = tzwhere.tzwhere()
 
-locations = dict(
-    Amsterdam=(52.370216, 4.895168),
-    Tokyo=(35.6684415, 139.6007844),
-    Dallas=(32.779167, -96.808891),
-)
-datetimes = [datetime(2021, 2, 10, i, tzinfo=pytz.utc) for i in range(24)]
+locations = {
+    "Amsterdam": (52.370216, 4.895168),
+    "Tokyo": (35.6684415, 139.6007844),
+    "Dallas": (32.779167, -96.808891),
+    "Cape Town": (-33.943707, 18.588740),  # check southern hemisphere, too
+}
+datetimes = [datetime(2021, 2, 10, tzinfo=pytz.utc) + timedelta(minutes=i) for i in range(24*60)]
 timezones = {k: tzwhere.tzNameAt(*v) for k, v in locations.items()}
 
 
 def solarpy(latitude: float, longitude: float, dt: datetime, z: str) -> float:
-    vnorm = np.array([0, 0, -1])  # plane pointing zenith
     h = 0  # sea-level
     dt = dt.astimezone(pytz.timezone(z)).replace(tzinfo=None)  # local time
     dt = standard2solar_time(dt, longitude)  # solar time
+    vnorm = solar_vector_ned(dt, latitude)  # plane pointing directly to the sun -> for clear sky irradiance
+    vnorm[-1] = vnorm[-1] * 0.99999  # avoid floating point error
     return irradiance_on_plane(vnorm, h, dt, latitude)
-
 
 def pysolar(latitude: float, longitude: float, dt: datetime) -> float:
     return compute_radiation(latitude, longitude, dt, cloud_coverage_in_percent=0)
@@ -46,7 +46,7 @@ def pvlib(latitude: float, longitude: float, dt: datetime) -> float:
     """
     https://firstgreenconsulting.wordpress.com/2012/04/26/differentiate-between-the-dni-dhi-and-ghi/
     """
-    site = location.Location(lat, lon, tz=pytz.utc)
+    site = location.Location(latitude, longitude, tz=pytz.utc)
     return site.get_clearsky(DatetimeIndex([dt])).loc[dt]["ghi"]
 
 
