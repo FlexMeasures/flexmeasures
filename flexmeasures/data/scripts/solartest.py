85b72963251bfef922e01c459cb07fd78ee42c67
Nicolas Höning <nicolas@seita.nl>
2021-02-12 23:18:03 +0100

    also plot dawn and dusk; remove flexmeasures dependency of test script

--- flexmeasures/data/scripts/solartest.py
+++ flexmeasures/data/scripts/solartest.py
@@ -6,11 +6,12 @@ from three different libraries.
 
 There might be errors or misunderstandings still in here.
 """
-from typing import List, Dict, Tuple
-
-from solarpy import irradiance_on_plane, solar_vector_ned, standard2solar_time
-from pvlib import location
+from typing import List, Dict
 from datetime import datetime, timedelta
+
+import solarpy
+import pvlib
+import pysolar
 import matplotlib.dates as mpl_dates
 import matplotlib.pyplot as plt
 import pytz
@@ -19,7 +20,6 @@ from tzwhere import tzwhere
 from astral import LocationInfo
 from astral.sun import sun
 
-from flexmeasures.utils.geo_utils import compute_radiation
 
 DAY = datetime(2021, 2, 10, tzinfo=pytz.utc)
 tzwhere = tzwhere.tzwhere()
@@ -34,34 +34,38 @@ datetimes = [DAY + timedelta(minutes=i * 20) for i in range(24 * 3)]
 timezones = {k: tzwhere.tzNameAt(*v) for k, v in locations.items()}
 
 
-def solarpy(latitude: float, longitude: float, dt: datetime, z: str) -> float:
+def irradiation_by_solarpy(
+    latitude: float, longitude: float, dt: datetime, z: str
+) -> float:
     h = 0  # sea-level
     dt = dt.astimezone(pytz.timezone(z)).replace(tzinfo=None)  # local time
-    dt = standard2solar_time(dt, longitude)  # solar time
-    vnorm = solar_vector_ned(
+    dt = solarpy.standard2solar_time(dt, longitude)  # solar time
+    vnorm = solarpy.solar_vector_ned(
         dt, latitude
     )  # plane pointing directly to the sun -> for clear sky irradiance
     vnorm[-1] = vnorm[-1] * 0.99999  # avoid floating point error
-    return irradiance_on_plane(vnorm, h, dt, latitude)
+    return solarpy.irradiance_on_plane(vnorm, h, dt, latitude)
 
 
-def pysolar(latitude: float, longitude: float, dt: datetime) -> float:
-    return compute_radiation(latitude, longitude, dt, cloud_coverage_in_percent=0)
+def irradiation_by_pysolar(latitude: float, longitude: float, dt: datetime) -> float:
+    altitude_deg = pysolar.solar.get_altitude(latitude, longitude, dt)
+    return pysolar.radiation.get_radiation_direct(dt, altitude_deg)
 
 
-def pvlib(latitude: float, longitude: float, dt: datetime) -> float:
+def irradiation_by_pvlib(latitude: float, longitude: float, dt: datetime) -> float:
     """
     https://firstgreenconsulting.wordpress.com/2012/04/26/differentiate-between-the-dni-dhi-and-ghi/
     """
-    site = location.Location(latitude, longitude, tz=pytz.utc)
-    return site.get_clearsky(DatetimeIndex([dt])).loc[dt]["ghi"]
+    site = pvlib.location.Location(latitude, longitude, tz=pytz.utc)
+    solpos = site.get_solarposition(DatetimeIndex([dt]))
+    return site.get_clearsky(DatetimeIndex([dt]), solar_position=solpos).loc[dt]["ghi"]
 
 
 def plot_irradiation(
     city: str,
     datetimes: List[datetime],
     values: Dict[str, List[float]],
-    sun_times: Tuple[datetime],
+    sun_times: Dict[str, datetime],
 ):
 
     fig, ax = plt.subplots()
@@ -81,20 +85,48 @@ def plot_irradiation(
     plt.gca().xaxis.set_major_locator(mpl_dates.HourLocator())
     plt.setp(plt.gca().xaxis.get_majorticklabels(), "rotation", 40)
 
-    # draw sunrise to sunset box
-    sun_dawn_tick, sun_noon_tick, sun_set_tick = mpl_dates.date2num(sun_times)
-    rectangle = plt.Rectangle(
-        (sun_dawn_tick, -100),
-        sun_set_tick - sun_dawn_tick,
+    # draw day phases boxes
+    dawn_tick, sunrise_tick, noon_tick, sunset_tick, dusk_tick = mpl_dates.date2num(
+        (
+            sun_times["dawn"],
+            sun_times["sunrise"],
+            sun_times["noon"],
+            sun_times["sunset"],
+            sun_times["dusk"],
+        )
+    )
+    dawn_to_sunrise = plt.Rectangle(
+        (dawn_tick, -100),
+        sunrise_tick - dawn_tick,
+        1100,
+        fc="floralwhite",
+        ec="lemonchiffon",
+        label="Dawn to Sunrise",
+    )
+    plt.gca().add_patch(dawn_to_sunrise)
+
+    sunrise_to_sunset = plt.Rectangle(
+        (sunrise_tick, -100),
+        sunset_tick - sunrise_tick,
         1100,
         fc="lightyellow",
         ec="lemonchiffon",
         label="Sunrise to sunset",
     )
-    plt.gca().add_patch(rectangle)
+    plt.gca().add_patch(sunrise_to_sunset)
+
+    sunset_to_dusk = plt.Rectangle(
+        (sunset_tick, -100),
+        dusk_tick - sunset_tick,
+        1100,
+        fc="oldlace",
+        ec="lemonchiffon",
+        label="Sunset to dusk",
+    )
+    plt.gca().add_patch(sunset_to_dusk)
 
     # draw noon
-    plt.axvline(x=sun_noon_tick, color="gold", label="Noon")
+    plt.axvline(x=noon_tick, color="gold", label="Noon")
 
     plt.legend()
 
@@ -115,18 +147,13 @@ if __name__ == "__main__":
         ]
 
         for dt in local_datetimes:
-            irrad_pysolar = pysolar(lat, lon, dt)
+            irrad_pysolar = irradiation_by_pysolar(lat, lon, dt)
             values["pysolar"].append(irrad_pysolar)
-            irrad_solarpy = solarpy(lat, lon, dt, timezone)
+            irrad_solarpy = irradiation_by_solarpy(lat, lon, dt, timezone)
             values["solarpy"].append(irrad_solarpy)
-            irrad_pvlib = pvlib(lat, lon, dt)
+            irrad_pvlib = irradiation_by_pvlib(lat, lon, dt)
             values["pvlib"].append(irrad_pvlib)
             print(
                 f"For {city} at {dt} {timezones[city]} ― pysolar: {irrad_pysolar:.2f}, solarpy: {irrad_solarpy:.2f}, pvlib: {irrad_pvlib:.2f}"
             )
-        plot_irradiation(
-            city,
-            local_datetimes,
-            values,
-            (sun_times["sunrise"], sun_times["noon"], sun_times["sunset"]),
-        )
+        plot_irradiation(city, local_datetimes, values, sun_times)
