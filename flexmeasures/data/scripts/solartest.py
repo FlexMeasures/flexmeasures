1247b59e2d26e5211ef27444e61d7f5dfafc5572
Nicolas Höning <nicolas@seita.nl>
2021-02-12 22:31:51 +0100

    add information about sunrise, noon and sunset

--- flexmeasures/data/scripts/solartest.py
+++ flexmeasures/data/scripts/solartest.py
@@ -6,7 +6,7 @@ from three different libraries.
 
 There might be errors or misunderstandings still in here.
 """
-
+from typing import List, Dict, Tuple
 
 from solarpy import irradiance_on_plane, solar_vector_ned, standard2solar_time
 from pvlib import location
@@ -16,10 +16,12 @@ import matplotlib.pyplot as plt
 import pytz
 from pandas import DatetimeIndex
 from tzwhere import tzwhere
+from astral import LocationInfo
+from astral.sun import sun
 
 from flexmeasures.utils.geo_utils import compute_radiation
 
-
+DAY = datetime(2021, 2, 10, tzinfo=pytz.utc)
 tzwhere = tzwhere.tzwhere()
 
 locations = {
@@ -28,10 +30,7 @@ locations = {
     "Dallas": (32.779167, -96.808891),
     "Cape-Town": (-33.943707, 18.588740),  # check southern hemisphere, too
 }
-datetimes = [
-    datetime(2021, 2, 10, tzinfo=pytz.utc) + timedelta(minutes=i * 20)
-    for i in range(24 * 3)
-]
+datetimes = [DAY + timedelta(minutes=i * 20) for i in range(24 * 3)]
 timezones = {k: tzwhere.tzNameAt(*v) for k, v in locations.items()}
 
 
@@ -58,21 +57,44 @@ def pvlib(latitude: float, longitude: float, dt: datetime) -> float:
     return site.get_clearsky(DatetimeIndex([dt])).loc[dt]["ghi"]
 
 
-def plot_irradiation(city, datetimes, values):
-    date_ticks = mpl_dates.date2num(datetimes)
-    # matplotlib.pyplot.plot_date(date_ticks, values)
+def plot_irradiation(
+    city: str,
+    datetimes: List[datetime],
+    values: Dict[str, List[float]],
+    sun_times: Tuple[datetime],
+):
 
     fig, ax = plt.subplots()
 
-    for lib in ("pysolar", "solarpy", "pvlib"):
-        ax.plot(date_ticks, values[lib], label=lib)
-
     ax.set(
         xlabel="Time (20m)",
-        ylabel="irradiation (?)",
-        title=f"Irradiation for {city} on 10 Feb 2021",
+        ylabel="Irradiation (W/m²)",
+        title=f"Irradiation for {city} on {DAY.date()}",
     )
-    ax.grid()
+
+    # draw values
+    date_ticks = mpl_dates.date2num(datetimes)
+    for lib in ("pysolar", "solarpy", "pvlib"):
+        plt.plot_date(date_ticks, values[lib], "-", label=lib)
+
+    # make date ticks look okay
+    plt.gca().xaxis.set_major_locator(mpl_dates.HourLocator())
+    plt.setp(plt.gca().xaxis.get_majorticklabels(), "rotation", 40)
+
+    # draw sunrise to sunset box
+    sun_dawn_tick, sun_noon_tick, sun_set_tick = mpl_dates.date2num(sun_times)
+    rectangle = plt.Rectangle(
+        (sun_dawn_tick, -100),
+        sun_set_tick - sun_dawn_tick,
+        1100,
+        fc="lightyellow",
+        ec="lemonchiffon",
+        label="Sunrise to sunset",
+    )
+    plt.gca().add_patch(rectangle)
+
+    # draw noon
+    plt.axvline(x=sun_noon_tick, color="gold", label="Noon")
 
     plt.legend()
 
@@ -85,7 +107,14 @@ if __name__ == "__main__":
         values = dict(pysolar=[], solarpy=[], pvlib=[])
         lat, lon = locations[city]
         timezone = timezones[city]
-        for dt in datetimes:
+        loc_info = LocationInfo(timezone=timezone, latitude=lat, longitude=lon)
+        # this gives 'dawn', 'sunrise', 'noon', 'sunset' and 'dusk'
+        sun_times = sun(loc_info.observer, date=DAY.date(), tzinfo=loc_info.timezone)
+        local_datetimes = [
+            dt.replace(tzinfo=pytz.timezone(timezones[city])) for dt in datetimes
+        ]
+
+        for dt in local_datetimes:
             irrad_pysolar = pysolar(lat, lon, dt)
             values["pysolar"].append(irrad_pysolar)
             irrad_solarpy = solarpy(lat, lon, dt, timezone)
@@ -93,6 +122,11 @@ if __name__ == "__main__":
             irrad_pvlib = pvlib(lat, lon, dt)
             values["pvlib"].append(irrad_pvlib)
             print(
-                f"For {city} at {dt} UTC ― pysolar: {irrad_pysolar:.2f}, solarpy: {irrad_solarpy:.2f}, pvlib: {irrad_pvlib:.2f}"
+                f"For {city} at {dt} {timezones[city]} ― pysolar: {irrad_pysolar:.2f}, solarpy: {irrad_solarpy:.2f}, pvlib: {irrad_pvlib:.2f}"
             )
-        plot_irradiation(city, datetimes, values)
+        plot_irradiation(
+            city,
+            local_datetimes,
+            values,
+            (sun_times["sunrise"], sun_times["noon"], sun_times["sunset"]),
+        )
