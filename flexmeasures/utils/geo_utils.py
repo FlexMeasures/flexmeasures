c07e3f3d7c0cf8c0739bd4bcb9d4175238d37531
F.N. Claessen <felix@seita.nl>
2021-02-14 15:40:04 +0100

    Switch to empirically validated function.

--- flexmeasures/utils/geo_utils.py
+++ flexmeasures/utils/geo_utils.py
@@ -2,7 +2,6 @@ from typing import Tuple, Union
 from datetime import datetime
 
 from pvlib.location import Location
-from pvlib.forecast import ForecastModel
 import pandas as pd
 
 
@@ -33,22 +32,34 @@ def parse_lat_lng(kwargs) -> Union[Tuple[float, float], Tuple[None, None]]:
     return None, None
 
 
-def compute_radiation(
-    latitude: float, longitude: float, dt: datetime, cloud_coverage_in_percent: int
+def compute_irradiance(
+    latitude: float, longitude: float, dt: datetime, cloud_coverage: float
 ) -> float:
-    """Compute the radiation received on a location at a specific time.
+    """Compute the irradiance received on a location at a specific time.
     This uses pvlib to
-    1) compute clear-sky radiation as Global Horizontal Irradiance (GHI),
-       which includes both Direct Normal Irradiance (DNI)
-       and Diffuse Horizontal Irradiance (DHI).
-    2) adjust the GHI for cloud coverage, using an algorithm described in
-       Larson et. al. "Day-ahead forecasting of solar power output from
-         photovoltaic plants in the American Southwest" Renewable Energy
-         91, 11-20 (2016).
+    1)  compute clear-sky irradiance as Global Horizontal Irradiance (GHI),
+        which includes both Direct Normal Irradiance (DNI)
+        and Diffuse Horizontal Irradiance (DHI).
+    2)  adjust the GHI for cloud coverage
     """
     site = Location(latitude, longitude, tz=dt.tzinfo)
     solpos = site.get_solarposition(pd.DatetimeIndex([dt]))
-    ghi = site.get_clearsky(pd.DatetimeIndex([dt]), solar_position=solpos).loc[dt][
-        "ghi"
-    ]
-    return ForecastModel.cloud_cover_to_ghi_linear(None, cloud_coverage_in_percent, ghi)
+    ghi_clear = site.get_clearsky(pd.DatetimeIndex([dt]), solar_position=solpos).loc[
+        dt
+    ]["ghi"]
+    return ghi_clear_to_ghi(ghi_clear, cloud_coverage)
+
+
+def ghi_clear_to_ghi(ghi_clear: float, cloud_coverage: float) -> float:
+    """Compute global horizontal irradiance (GHI) from clear-sky GHI, given a cloud coverage between 0 and 1.
+
+    References
+    ----------
+    Perez, R., Moore, K., Wilcox, S., Renne, D., Zelenka, A., 2007.
+    Forecasting solar radiation – preliminary evaluation of an
+    approach based upon the national forecast database. Solar Energy
+    81, 809–812.
+    """
+    if cloud_coverage < 0 or cloud_coverage > 1:
+        raise ValueError("cloud_coverage should lie in the interval [0, 1]")
+    return (1 - 0.87 * cloud_coverage ** 1.9) * ghi_clear
