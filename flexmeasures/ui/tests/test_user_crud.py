26cb95fb45fb2cd971c8dd19c568084a193716e5
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2021-02-16 15:14:40 +0100

    Typos.

--- flexmeasures/ui/tests/test_user_crud.py
+++ flexmeasures/ui/tests/test_user_crud.py
@@ -50,7 +50,7 @@ def test_user_page(client, as_admin, requests_mock):
 
 
 def test_deactivate_user(client, as_admin, requests_mock):
-    """Test it does not fail (logic is tested in API tests) and diplays an answer."""
+    """Test it does not fail (logic is tested in API tests) and displays an answer."""
     prosumer2 = find_user_by_email("test_prosumer2@seita.nl", keep_in_session=False)
     requests_mock.patch(
         f"http://localhost//api/v2_0/user/{prosumer2.id}",
@@ -67,7 +67,7 @@ def test_deactivate_user(client, as_admin, requests_mock):
 
 
 def test_reset_password(client, as_admin, requests_mock):
-    """Test it does not fail (logic is tested in API tests) and diplays an answer."""
+    """Test it does not fail (logic is tested in API tests) and displays an answer."""
     prosumer2 = find_user_by_email("test_prosumer2@seita.nl", keep_in_session=False)
     requests_mock.get(
         f"http://localhost//api/v2_0/user/{prosumer2.id}/password-reset",
