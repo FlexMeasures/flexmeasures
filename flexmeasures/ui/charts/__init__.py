461409106217f0f635e73babd12a7efae6374028
F.N. Claessen <felix@seita.nl>
2021-04-15 11:37:50 +0200

    Clarify what belief_charts_mapping does

--- flexmeasures/ui/charts/__init__.py
+++ flexmeasures/ui/charts/__init__.py
@@ -3,8 +3,13 @@ from inspect import getmembers, isfunction
 from . import belief_charts
 from .defaults import apply_chart_defaults
 
+# Create a dictionary mapping chart types to chart specs, and apply defaults to the chart specs, too.
+# chart types:  Name of a variable defining chart specs or a function returning chart specs.
+# chart specs:  A dictionary or Altair chart specification.
+#               In case of a dictionary, the creator needs to ensure that the dictionary contains valid vega-lite specs
+#               In case of an Altair chart specification, Altair checks for you
 belief_charts_mapping = {
-    k: apply_chart_defaults(v)
-    for k, v in getmembers(belief_charts)
-    if isfunction(v) or isinstance(v, dict)
+    chart_type: apply_chart_defaults(chart_specs)
+    for chart_type, chart_specs in getmembers(belief_charts)
+    if isfunction(chart_specs) or isinstance(chart_specs, dict)
 }
