f67552fbba265d1047d02418ebefe22c7c6c575a
Nicolas Höning <nicolas@seita.nl>
2021-06-12 13:12:09 +0200

    mypy types support

--- ci/run_mypy.sh
+++ ci/run_mypy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-pip install mypy
+pip install --upgrade mypy>=0.902
+pip install types-pytz types-requests types-Flask types-click types-redis types-tzlocal types-python-dateutil
 # We are checking python files which have type hints, and leave out bigger issues we made issues for
 # * data/scripts: We'll remove legacy code: https://trello.com/c/1wEnHOkK/7-remove-custom-data-scripts
 # * data/models and data/services: https://trello.com/c/rGxZ9h2H/540-makequery-call-signature-is-incoherent
