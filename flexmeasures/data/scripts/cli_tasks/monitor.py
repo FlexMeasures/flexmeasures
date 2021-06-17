4037b392d55cec77225ed896e8a0227b710069e7
Nicolas Höning <nicolas@seita.nl>
2021-06-17 13:38:16 +0200

    simpler alert titles (for better grouping in Sentry), adding latest run info as extra context. Also alert if no latest run exist.

--- flexmeasures/data/scripts/cli_tasks/monitor.py
+++ flexmeasures/data/scripts/cli_tasks/monitor.py
@@ -1,10 +1,14 @@
 from datetime import timedelta
+from typing import Optional
 
 import click
 from flask import current_app as app
 from flask.cli import with_appcontext
 from flask_mail import Message
-from sentry_sdk import capture_message as capture_message_for_sentry
+from sentry_sdk import (
+    capture_message as capture_message_for_sentry,
+    set_context as set_sentry_context,
+)
 
 from flexmeasures.data.models.task_runs import LatestTaskRun
 from flexmeasures.utils.time_utils import server_now
@@ -15,16 +19,30 @@ def fm_monitor():
     """FlexMeasures: Monitor tasks."""
 
 
-def send_monitoring_alert(task_name: str, msg: str):
+def send_monitoring_alert(
+    task_name: str, msg: str, latest_run: Optional[LatestTaskRun] = None
+):
     """
-    Send any monitoring message per Sentry and per email.
+    Send any monitoring message per Sentry and per email. Also log an error.
     """
+    latest_run_txt = ""
+    if latest_run:
+        set_sentry_context(
+            "latest_run", {"time": latest_run.datetime, "status": latest_run.status}
+        )
+        latest_run_txt = (
+            f"Last run was at {latest_run.datetime}, status was: {latest_run.status}"
+        )
+
     capture_message_for_sentry(msg)
+
     email_recipients = app.config.get("MAIL_MONITORING_RECIPIENTS", "").split(",")
     if len(email_recipients) > 0:
         email = Message(subject=f"Problem with task {task_name}", bcc=email_recipients)
-        email.body = msg
+        email.body = f"{msg}\n\n{latest_run_txt}\nWe suggest to check the logs."
         app.mail.send(email)
+
+    app.logger.error(f"msg  {latest_run_txt}")
 
 
 @fm_monitor.command("tasks")
@@ -46,8 +64,8 @@ def monitor_tasks(task):
         app.logger.info(f"Checking latest run of task {task_name} ...")
         latest_run: LatestTaskRun = LatestTaskRun.query.get(task_name)
         if latest_run is None:
-            msg = f"Task {task_name} has no last run. Is it configured properly?"
-            app.logger.warning(msg)
+            msg = f"Task {task_name} has no last run and thus cannot be monitored. Is it configured properly?"
+            send_monitoring_alert(task_name, msg)
             return
         now = server_now()
         acceptable_interval = timedelta(minutes=t[1])
@@ -58,16 +76,15 @@ def monitor_tasks(task):
         ):
             # last time is okay, let's check the status
             if latest_run.status is False:
-                msg = f"Error: Failure reported on task {task_name} at {latest_run.datetime}."
-                app.logger.error(msg)
-                send_monitoring_alert(task_name, msg)
+                msg = f"A failure has been reported on task {task_name}."
+                send_monitoring_alert(task_name, msg, latest_run)
         else:
             msg = (
-                f"Task {task_name} is outside of the acceptable {acceptable_interval}"
-                f" minute range. Last run was {latest_run.datetime}"
+                f"Task {task_name}'s latest run time is outside of the acceptable range "
+                f"({acceptable_interval})."
             )
             app.logger.error(msg)
-            send_monitoring_alert(task_name, msg)
+            send_monitoring_alert(task_name, msg, latest_run)
     app.logger.info("Done checking task runs ...")
 
 
