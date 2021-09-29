d8951e3ead198f1d812d5ad3aebc4816c1ea7e23
F.N. Claessen <felix@seita.nl>
2021-09-29 21:11:37 +0200

    Add test for updating source

--- flexmeasures/data/tests/test_time_series_services.py
+++ flexmeasures/data/tests/test_time_series_services.py
@@ -2,7 +2,7 @@ import pandas as pd
 from timely_beliefs import utils as tb_utils
 
 from flexmeasures.api.common.utils.api_utils import save_to_db
-
+from flexmeasures.data.models.data_sources import DataSource
 from flexmeasures.data.models.time_series import Sensor
 
 
@@ -25,3 +25,18 @@ def test_drop_unchanged_beliefs(setup_beliefs):
     bdf = sensor.search_beliefs()
     assert len(bdf) == num_beliefs_before
     assert list(bdf.belief_times) == list(belief_times_before)
+
+
+def test_do_not_drop_beliefs_copied_by_another_source(setup_beliefs):
+    """Trying to copy beliefs from one source to another should double the number of beliefs."""
+    sensor = Sensor.query.filter_by(name="epex_da").one_or_none()
+    bdf = sensor.search_beliefs()
+    num_beliefs_before = len(bdf)
+    new_source = DataSource(name="Not Seita", type="demo script")
+    bdf = tb_utils.replace_multi_index_level(
+        bdf, "source", pd.Index([new_source] * num_beliefs_before)
+    )
+    save_to_db(bdf)
+    bdf = sensor.search_beliefs()
+    num_beliefs_after = len(bdf)
+    assert num_beliefs_after == 2 * num_beliefs_before
