b0b06541d6ef21ceca65a7ac6934e9a4e5288b95
F.N. Claessen <felix@seita.nl>
2021-09-29 22:31:49 +0200

    Document tests

--- flexmeasures/data/tests/test_time_series_services.py
+++ flexmeasures/data/tests/test_time_series_services.py
@@ -11,17 +11,27 @@ def test_drop_unchanged_beliefs(setup_beliefs):
 
     Even after updating the belief time, we expect to persist only the older belief time.
     """
+
+    # Set a reference for the number of beliefs stored and their belief times
     sensor = Sensor.query.filter_by(name="epex_da").one_or_none()
     bdf = sensor.search_beliefs()
     num_beliefs_before = len(bdf)
     belief_times_before = bdf.belief_times
-    save_to_db(bdf)  # try saving verbatim
+
+    # See what happens when storing all existing beliefs verbatim
+    save_to_db(bdf)
+
+    # Verify that no new beliefs were saved
     bdf = sensor.search_beliefs()
     assert len(bdf) == num_beliefs_before
+
+    # See what happens when storing all beliefs with their belief time updated
     bdf = tb_utils.replace_multi_index_level(
         bdf, "belief_time", bdf.belief_times + pd.Timedelta("1H")
     )
-    save_to_db(bdf)  # try saving with updated belief time
+    save_to_db(bdf)
+
+    # Verify that no new beliefs were saved
     bdf = sensor.search_beliefs()
     assert len(bdf) == num_beliefs_before
     assert list(bdf.belief_times) == list(belief_times_before)
@@ -29,14 +39,20 @@ def test_drop_unchanged_beliefs(setup_beliefs):
 
 def test_do_not_drop_beliefs_copied_by_another_source(setup_beliefs):
     """Trying to copy beliefs from one source to another should double the number of beliefs."""
+
+    # Set a reference for the number of beliefs stored
     sensor = Sensor.query.filter_by(name="epex_da").one_or_none()
     bdf = sensor.search_beliefs()
     num_beliefs_before = len(bdf)
+
+    # See what happens when storing all belief with their source updated
     new_source = DataSource(name="Not Seita", type="demo script")
     bdf = tb_utils.replace_multi_index_level(
         bdf, "source", pd.Index([new_source] * num_beliefs_before)
     )
     save_to_db(bdf)
+
+    # Verify that all the new beliefs were added
     bdf = sensor.search_beliefs()
     num_beliefs_after = len(bdf)
     assert num_beliefs_after == 2 * num_beliefs_before
@@ -49,9 +65,13 @@ def test_do_not_drop_changed_probabilistic_belief(setup_beliefs):
     if that belief becomes more certain (e.g. cp=0.3 and cp=0.5),
     we expect to see the full new belief stored, rather than just the cp=0.3 value.
     """
+
+    # Set a reference for the number of beliefs stored
     sensor = Sensor.query.filter_by(name="epex_da").one_or_none()
     bdf = sensor.search_beliefs(source="Seita")
     num_beliefs_before = len(bdf)
+
+    # See what happens when storing a belief with more certainty one hour later
     old_belief = bdf.loc[
         (
             bdf.index.get_level_values("event_start")
@@ -62,7 +82,6 @@ def test_do_not_drop_changed_probabilistic_belief(setup_beliefs):
             == pd.Timestamp("2021-03-28 14:00:00+00:00")
         )
     ]
-    # More certainty one hour later
     new_belief = tb_utils.replace_multi_index_level(
         old_belief, "cumulative_probability", pd.Index([0.3, 0.5])
     )
@@ -70,6 +89,8 @@ def test_do_not_drop_changed_probabilistic_belief(setup_beliefs):
         new_belief, "belief_time", new_belief.belief_times + pd.Timedelta("1H")
     )
     save_to_db(new_belief)
+
+    # Verify that the whole probabilistic belief was added
     bdf = sensor.search_beliefs(source="Seita")
     num_beliefs_after = len(bdf)
     assert num_beliefs_after == num_beliefs_before + len(new_belief)
