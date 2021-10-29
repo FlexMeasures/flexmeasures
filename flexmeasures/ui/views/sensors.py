038fac80bf7f11498c5d2bbee7548582292d2fd1
Nicolas Höning <nicolas@seita.nl>
2021-10-29 16:40:41 +0200

    remove unused imports

--- flexmeasures/ui/views/sensors.py
+++ flexmeasures/ui/views/sensors.py
@@ -7,8 +7,6 @@ from flask_security import login_required
 from marshmallow import fields
 from webargs.flaskparser import use_kwargs
 
-from flexmeasures.auth.policy import ADMIN_ROLE
-from flexmeasures.auth.decorators import roles_required
 from flexmeasures.data.schemas.times import AwareDateTimeField
 from flexmeasures.api.dev.sensors import SensorAPI
 from flexmeasures.ui.utils.view_utils import render_flexmeasures_template
