81a9d386e687a4095fbf2516914afefbe7584646
F.N. Claessen <felix@seita.nl>
2021-11-02 10:27:13 +0100

    Move to setting defaults for entire field definitions, and add test

--- flexmeasures/data/models/charts/belief_charts.py
+++ flexmeasures/data/models/charts/belief_charts.py
@@ -1,47 +1,28 @@
-from flexmeasures.data.models.charts.defaults import ENCODING_TITLES
+from flexmeasures.data.models.charts.defaults import FIELD_DEFINITIONS
 
 
 def bar_chart(title: str, quantity: str = "unknown quantity", unit: str = "a.u."):
     if not unit:
         unit = "a.u."
+    event_value_field_definition = dict(
+        title=f"{quantity} ({unit})",
+        format=".3s",
+        stack=None,
+        **FIELD_DEFINITIONS["event_value"],
+    )
     return {
         "description": "A simple bar chart.",
         "title": title,
         "mark": "bar",
         "encoding": {
-            "x": {
-                "field": "event_start",
-                "type": "T",
-                "title": ENCODING_TITLES["event_start"],
-            },
-            "y": {
-                "field": "event_value",
-                "type": "quantitative",
-                "title": quantity + " (" + unit + ")",
-                "stack": None,
-            },
-            "color": {
-                "field": "source",
-                "type": "ordinal",
-                "title": ENCODING_TITLES["source"],
-            },
+            "x": FIELD_DEFINITIONS["event_start"],
+            "y": event_value_field_definition,
+            "color": FIELD_DEFINITIONS["source"],
             "opacity": {"value": 0.7},
             "tooltip": [
-                {
-                    "field": "event_value",
-                    "title": quantity + " (" + unit + ")",
-                    "type": "quantitative",
-                },
-                {
-                    "field": "full_date",
-                    "title": ENCODING_TITLES["full_date"],
-                    "type": "nominal",
-                },
-                {
-                    "field": "source",
-                    "title": ENCODING_TITLES["source"],
-                    "type": "ordinal",
-                },
+                FIELD_DEFINITIONS["full_date"],
+                event_value_field_definition,
+                FIELD_DEFINITIONS["source"],
             ],
         },
     }
