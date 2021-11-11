8734e8d2025bc87e4e8aac5893a13f973e9217c8
F.N. Claessen <felix@seita.nl>
2021-11-11 14:38:15 +0100

    Add test for multiple roles and multiple account roles

--- flexmeasures/auth/tests/test_principal_matching.py
+++ flexmeasures/auth/tests/test_principal_matching.py
@@ -34,38 +34,52 @@ class MockUser:
         return role in self.roles
 
 
-def make_mock_user(user_id, user_role, account_id, account_role) -> MockUser:
-    account = MockAccount(account_id, [account_role])
-    return MockUser(id=user_id, username="Tester", roles=[user_role], account=account)
+def make_mock_user(
+    user_id: int, user_roles: List[str], account_id: int, account_roles: List[str]
+) -> MockUser:
+    account = MockAccount(account_id, account_roles)
+    return MockUser(id=user_id, username="Tester", roles=user_roles, account=account)
 
 
 @pytest.mark.parametrize(
     "mock_user,principals,should_match",
     [
-        (make_mock_user(19, "", 1, ""), "user:19", True),
-        (make_mock_user(19, "", 1, ""), "user:28", False),
-        (make_mock_user(19, "gardener", 1, ""), "role:gardener", True),
+        (make_mock_user(19, [], 1, []), "user:19", True),
+        (make_mock_user(19, [], 1, []), "user:28", False),
+        (make_mock_user(19, ["gardener"], 1, []), "role:gardener", True),
         (
-            make_mock_user(19, "gardener", 1, "castle"),
+            make_mock_user(19, ["gardener"], 1, ["castle"]),
             ("role:gardener", "account-role:castle"),
             True,
         ),
         (
-            make_mock_user(19, "gardener", 1, "castle"),
+            make_mock_user(19, ["gardener"], 1, ["castle"]),
             ("role:gardener", "account-role:villa"),
             False,
         ),
-        (make_mock_user(19, "", 113, ""), "account:114", False),
-        (make_mock_user(19, "", 113, ""), "account:113", True),
+        (make_mock_user(19, [], 113, []), "account:114", False),
+        (make_mock_user(19, [], 113, []), "account:113", True),
         (
-            make_mock_user(19, "waitress", 113, "restaurant"),
+            make_mock_user(19, ["waitress"], 113, ["restaurant"]),
             ("user:19", "account:113", "role:waitress", "account-role:restaurant"),
             True,
         ),
         (
-            make_mock_user(19, "waitress", 113, "hotel"),
+            make_mock_user(19, ["waitress"], 113, ["hotel"]),
             ("user:13", "account:113", "role:waitress", "role:chef"),
             False,
+        ),
+        (
+            make_mock_user(19, ["waitress", "chef"], 113, ["hotel", "cinema"]),
+            (
+                "user:19",
+                "account:113",
+                "role:waitress",
+                "role:chef",
+                "account-role:hotel",
+                "account-role:cinema",
+            ),
+            True,
         ),
     ],
 )
