3b40e2e16f492a43559e4dc635277938a6203e6f
F.N. Claessen <felix@seita.nl>
2021-12-03 11:07:19 +0100

    Make sensor name optional, too, in query util

--- flexmeasures/data/queries/sensors.py
+++ flexmeasures/data/queries/sensors.py
@@ -7,14 +7,17 @@ from flexmeasures.data.models.time_series import Sensor
 
 
 def query_sensor_by_name_and_generic_asset_type_name(
-    sensor_name: str, generic_asset_type_names: Optional[List[str]] = None
+    sensor_name: Optional[str] = None,
+    generic_asset_type_names: Optional[List[str]] = None,
 ) -> Query:
     """Match a sensor by its own name and that of its generic asset type.
 
-    :param sensor_name: should match
+    :param sensor_name: should match (if None, no match is needed)
     :param generic_asset_type_names: should match at least one of these (if None, no match is needed)
     """
-    query = Sensor.query.filter(Sensor.name == sensor_name)
+    query = Sensor.query
+    if sensor_name is not None:
+        query = query.filter(Sensor.name == sensor_name)
     if generic_asset_type_names is not None:
         query = (
             query.join(GenericAsset)
