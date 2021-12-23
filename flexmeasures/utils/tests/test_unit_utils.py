e48f44eb96cd710afbc3bb2c3092049ec81774a3
F.N. Claessen <felix@seita.nl>
2021-12-23 11:06:34 +0100

    Extra testing of unit util functions, incl. conversion of offset units
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/utils/tests/test_unit_utils.py
+++ flexmeasures/utils/tests/test_unit_utils.py
@@ -1,8 +1,11 @@
+from datetime import timedelta
 import pytest
 
 from flexmeasures.utils.unit_utils import (
     determine_flow_unit,
     determine_stock_unit,
+    determine_unit_conversion_multiplier,
+    units_are_convertible,
     is_energy_unit,
     is_power_unit,
     u,
@@ -49,12 +52,30 @@ def test_determine_stock_unit(
         assert determine_stock_unit(unit, time_unit) == expected_unit
 
 
+def test_determine_unit_conversion_multiplier():
+    assert determine_unit_conversion_multiplier("kW", "W") == 1000
+    assert determine_unit_conversion_multiplier("J/s", "W") == 1
+    assert determine_unit_conversion_multiplier("Wh", "W", timedelta(minutes=10)) == 6
+    assert determine_unit_conversion_multiplier("kWh", "MJ") == 3.6
+    assert determine_unit_conversion_multiplier("°C", "K") == 274.15
+
+
 def test_h_denotes_hour_and_not_planck_constant():
     assert u.Quantity("h").dimensionality == u.Quantity("hour").dimensionality
     assert (
         u.Quantity("hbar").dimensionality
         == u.Quantity("planck_constant").dimensionality
     )
+
+
+def test_units_are_convertible():
+    assert units_are_convertible("kW", "W")  # units just have different prefixes
+    assert units_are_convertible("J/s", "W")  # units can be converted using some multiplier
+    assert units_are_convertible("Wh", "W")  # units that represent a stock delta can, knowing the duration, be converted to a flow
+    assert units_are_convertible("toe", "W")  # tonne of oil equivalent
+    assert units_are_convertible("°C", "K")  # offset unit to absolute unit
+    assert not units_are_convertible("°C", "W")
+    assert not units_are_convertible("EUR/MWh", "W")
 
 
 @pytest.mark.parametrize(
