72c5d8e3c7ef71ae8eb619b0d648cbcbfd10fc7a
Nicolas Höning <nicolas@seita.nl>
2022-02-01 23:37:40 +0100

    make add structure into a command adding modern-style asset types; and also add user and account roles
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/cli/data_add.py
+++ flexmeasures/cli/data_add.py
@@ -293,7 +293,7 @@ def add_weather_sensor(**args):
 @fm_add_data.command("structure")
 @with_appcontext
 def add_initial_structure():
-    """Initialize structural data like asset types, market types and weather sensor types."""
+    """Initialize structural data like asset types and roles."""
     from flexmeasures.data.scripts.data_gen import populate_structure
 
     populate_structure(db)
