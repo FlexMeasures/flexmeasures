fb9e062611aab431b3e7ed72a9e7a72672a9af29
F.N. Claessen <felix@seita.nl>
2022-02-02 19:44:53 +0100

    Add UniqueConstraints on roles_accounts and roles_users tables
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/migrations/versions/7f8b8920355f_create_annotation_table.py
+++ flexmeasures/data/migrations/versions/7f8b8920355f_create_annotation_table.py
@@ -22,12 +22,24 @@ def upgrade():
     create_annotation_account_relationship_table()
     create_annotation_asset_relationship_table()
     create_annotation_sensor_relationship_table()
+    create_user_roles_unique_constraints()
+    create_account_roles_unique_constraints()
 
 
 def downgrade():
     click.confirm(
         "This downgrade drops the tables 'annotations_accounts', 'annotations_assets', 'annotations_sensors' and 'annotation'. Continue?",
         abort=True,
+    )
+    op.drop_constraint(
+        op.f("roles_accounts_role_id_key"),
+        "roles_accounts",
+        type_="unique",
+    )
+    op.drop_constraint(
+        op.f("roles_users_role_id_key"),
+        "roles_users",
+        type_="unique",
     )
     op.drop_constraint(
         op.f("annotations_accounts_annotation_id_key"),
@@ -50,6 +62,30 @@ def downgrade():
     op.drop_constraint(op.f("annotation_content_key"), "annotation", type_="unique")
     op.drop_table("annotation")
     op.execute("DROP TYPE annotation_type;")
+
+
+def create_account_roles_unique_constraints():
+    """Remove any duplicate relationships, then constrain any new relationships to be unique."""
+    op.execute(
+        "DELETE FROM roles_accounts WHERE id in (SELECT r1.id FROM roles_accounts r1, roles_accounts r2 WHERE r1.id > r2.id AND r1.role_id = r2.role_id and r1.account_id = r2.account_id);"
+    )
+    op.create_unique_constraint(
+        op.f("roles_accounts_role_id_key"),
+        "roles_accounts",
+        ["role_id", "account_id"],
+    )
+
+
+def create_user_roles_unique_constraints():
+    """Remove any duplicate relationships, then constrain any new relationships to be unique."""
+    op.execute(
+        "DELETE FROM roles_users WHERE id in (SELECT r1.id FROM roles_users r1, roles_users r2 WHERE r1.id > r2.id AND r1.role_id = r2.role_id and r1.user_id = r2.user_id);"
+    )
+    op.create_unique_constraint(
+        op.f("roles_users_role_id_key"),
+        "roles_users",
+        ["role_id", "user_id"],
+    )
 
 
 def create_annotation_sensor_relationship_table():
