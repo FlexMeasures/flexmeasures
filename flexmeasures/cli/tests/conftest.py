21f93d0da16d80d703e877015b0206571fcacfa6
F.N. Claessen <felix@seita.nl>
2022-02-10 09:13:41 +0100

    Stop using setup_accounts in CLI tests
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/cli/tests/conftest.py
+++ flexmeasures/cli/tests/conftest.py
@@ -2,15 +2,24 @@ import pytest
 from typing import Dict
 
 from flexmeasures.auth.policy import ADMIN_ROLE
-from flexmeasures.data.services.users import create_user, User
+from flexmeasures.data.services.users import Account, create_user, User
 
 
 @pytest.fixture(scope="module")
-def setup_account_owner(db, setup_accounts) -> Dict[str, User]:
+def setup_one_account(db) -> Dict[str, Account]:
+    prosumer_account = Account(
+        name="Test MDC Account",
+    )
+    db.session.add(prosumer_account)
+    return {prosumer_account.name: prosumer_account}
+
+
+@pytest.fixture(scope="module")
+def setup_account_owner(db, setup_one_account) -> Dict[str, User]:
     account_owner = create_user(
         username="Test Account Owner",
         email="test_account_owner@seita.nl",
-        account_name=setup_accounts["Prosumer"].name,
+        account_name=setup_one_account["Test MDC Account"].name,
         password="testtest",
         # TODO: change ADMIN_ROLE to ACCOUNT_ADMIN
         user_roles=dict(
