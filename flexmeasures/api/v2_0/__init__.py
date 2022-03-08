cb205ba868e73e58871abe656d267eec400711f2
F.N. Claessen <felix@seita.nl>
2022-03-08 11:30:15 +0100

    Move import to its preferred position
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v2_0/__init__.py
+++ flexmeasures/api/v2_0/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, Blueprint
 
+from flexmeasures.api.v2_0.implementations.sensor_data import SensorDataAPI
+
 flexmeasures_api = Blueprint("flexmeasures_api_v2_0", __name__)
 
 
 def register_at(app: Flask):
     """This can be used to register this blueprint together with other api-related things"""
 
-    from flexmeasures.api.v2_0.implementations.sensor_data import SensorDataAPI
     import flexmeasures.api.v2_0.routes  # noqa: F401 this is necessary to load the endpoints
 
     v2_0_api_prefix = "/api/v2_0"
