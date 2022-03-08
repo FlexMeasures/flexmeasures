e6db62f3a6e978613462b38bcccb55b6b870772b
F.N. Claessen <felix@seita.nl>
2022-03-08 16:42:41 +0100

    Implement [GET] sensorData
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v2_0/implementations/sensor_data.py
+++ flexmeasures/api/v2_0/implementations/sensor_data.py
@@ -1,17 +1,27 @@
+import json
+
+from isodate import datetime_isoformat, duration_isoformat
+import pandas as pd
 from flask_classful import FlaskView, route
-from flask_security import auth_token_required
+from flask_security import login_required
 from webargs.flaskparser import use_args
 
-from flexmeasures.api.common.schemas.sensor_data import SensorDataSchema
+from flexmeasures.api.common.schemas.sensor_data import (
+    SensorDataSchema,
+    SensorDataDescriptionSchema,
+)
 from flexmeasures.api.common.utils.api_utils import save_and_enqueue
+from flexmeasures import Sensor
 from flexmeasures.auth.decorators import account_roles_accepted
+from flexmeasures.data.services.time_series import simplify_index
+from flexmeasures.utils.unit_utils import convert_units
 
 
 class SensorDataAPI(FlaskView):
 
     route_base = "/sensorData"
+    decorators = [login_required]
 
-    @auth_token_required
     @account_roles_accepted("MDC", "Prosumer")
     @use_args(
         SensorDataSchema(),
@@ -51,12 +61,51 @@ class SensorDataAPI(FlaskView):
         return response, code
 
     @route("/", methods=["GET"])
-    def get(self):
+    @use_args(
+        SensorDataDescriptionSchema(),
+        location="query",
+    )
+    def get(self, sensor_data_description):
         """Get sensor data from FlexMeasures.
 
         .. :quickref: Data; Download sensor data
         """
-        # - use data.models.time_series.Sensor::search_beliefs() - might need to add a belief_horizon parameter
-        # - create the serialize method on the schema, to turn the resulting BeliefsDataFrame
-        #   to the JSON the API should respond with.
-        pass
+        # todo: respect passed horizon and prior
+        # todo: move some of the below logic to the dump_bdf (serialize) method on the schema
+        sensor: Sensor = sensor_data_description["sensor"]
+        start = sensor_data_description["start"]
+        duration = sensor_data_description["duration"]
+        end = sensor_data_description["start"] + duration
+        unit = sensor_data_description["unit"]
+
+        df = simplify_index(
+            sensor.search_beliefs(
+                event_starts_after=start,
+                event_ends_before=end,
+                one_deterministic_belief_per_event=True,
+                as_json=False,
+            )
+        )
+
+        # Convert to desired time range
+        index = pd.date_range(
+            start=start, end=end, freq=sensor.event_resolution, closed="left"
+        )
+        df = df.reindex(index).reset_index()
+
+        # Convert to desired unit
+        values = convert_units(
+            df["event_value"],
+            from_unit=sensor.unit,
+            to_unit=unit,
+        ).to_list()
+
+        response = dict(
+            values=values,
+            start=datetime_isoformat(start),
+            duration=duration_isoformat(duration),
+            unit=unit,
+        )
+
+        response.update(type="GetSensorDataResponse")
+        return json.dumps(response)
