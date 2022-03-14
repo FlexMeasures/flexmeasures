d2a2e5f0d582efbaca034584710d4755261544c3
F.N. Claessen <felix@seita.nl>
2022-03-14 12:13:22 +0100

    Fix test
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/tests/conftest.py
+++ flexmeasures/api/v3_0/tests/conftest.py
@@ -2,6 +2,14 @@ import pytest
 from flask_security import SQLAlchemySessionUserDatastore, hash_password
 
 
+@pytest.fixture(scope="module", autouse=True)
+def setup_api_test_data(db, setup_roles_users):
+    """
+    Set up data for API v3.0 tests.
+    """
+    print("Setting up data for API v3.0 tests on %s" % db.engine)
+
+
 @pytest.fixture(scope="module")
 def setup_inactive_user(db, setup_accounts, setup_roles_users):
     """
