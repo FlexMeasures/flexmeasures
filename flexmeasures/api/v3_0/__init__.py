77ffdaf6fdc019cf9872313ef9f414a7acdc44a7
F.N. Claessen <felix@seita.nl>
2022-03-14 11:01:10 +0100

    Move user API to class for v3
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/__init__.py
+++ flexmeasures/api/v3_0/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 
 from flexmeasures.api.v3_0.implementations.sensor_data import SensorDataAPI
+from flexmeasures.api.v3_0.implementations.users import UserAPI
 
 
 def register_at(app: Flask):
@@ -9,3 +10,4 @@ def register_at(app: Flask):
     v3_0_api_prefix = "/api/v3_0"
 
     SensorDataAPI.register(app, route_prefix=v3_0_api_prefix)
+    UserAPI.register(app, route_prefix=v3_0_api_prefix)
