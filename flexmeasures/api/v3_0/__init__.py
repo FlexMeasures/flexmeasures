0f2a8f0a124f1fd6b116311b8c3bd42c33bf73e2
F.N. Claessen <felix@seita.nl>
2022-03-15 15:24:05 +0100

    Rename module
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/__init__.py
+++ flexmeasures/api/v3_0/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from flexmeasures.api.v3_0.implementations.sensor_data import SensorAPI
+from flexmeasures.api.v3_0.implementations.sensors import SensorAPI
 from flexmeasures.api.v3_0.implementations.users import UserAPI
 
 
