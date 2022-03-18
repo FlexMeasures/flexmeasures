d8b27909d71fd23a1a75e4612e649976d654663d
F.N. Claessen <felix@seita.nl>
2022-03-18 23:36:52 +0100

    Finish moving sensor data tests from dev to v3_0
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/dev/tests/conftest.py
+++ flexmeasures/api/dev/tests/conftest.py
@@ -1,9 +1,4 @@
-from datetime import timedelta
-
 import pytest
-
-from flexmeasures.data.models.generic_assets import GenericAssetType, GenericAsset
-from flexmeasures.data.models.time_series import Sensor
 
 
 @pytest.fixture(scope="module")
@@ -12,7 +7,6 @@ def setup_api_test_data(db, setup_roles_users, setup_generic_assets):
     Set up data for API dev tests.
     """
     print("Setting up data for API dev tests on %s" % db.engine)
-    add_gas_sensor(db, setup_roles_users["Test Supplier User"])
 
 
 @pytest.fixture(scope="function")
@@ -23,29 +17,3 @@ def setup_api_fresh_test_data(
     Set up fresh data for API dev tests.
     """
     print("Setting up fresh data for API dev tests on %s" % fresh_db.engine)
-    for sensor in Sensor.query.all():
-        fresh_db.delete(sensor)
-    add_gas_sensor(fresh_db, setup_roles_users_fresh_db["Test Supplier User"])
-
-
-def add_gas_sensor(db, test_supplier_user):
-    incineration_type = GenericAssetType(
-        name="waste incinerator",
-    )
-    db.session.add(incineration_type)
-    db.session.flush()
-    incineration_asset = GenericAsset(
-        name="incineration line",
-        generic_asset_type=incineration_type,
-        account_id=test_supplier_user.account_id,
-    )
-    db.session.add(incineration_asset)
-    db.session.flush()
-    gas_sensor = Sensor(
-        name="some gas sensor",
-        unit="m³/h",
-        event_resolution=timedelta(minutes=10),
-        generic_asset=incineration_asset,
-    )
-    db.session.add(gas_sensor)
-    gas_sensor.owner = test_supplier_user.account
