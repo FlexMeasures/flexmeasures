0589009812d23e4ea2351ddf6615648cd9221f69
F.N. Claessen <felix@seita.nl>
2022-03-18 23:47:00 +0100

    Fix asset tests in dev API by moving back some conftest logic
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/dev/tests/conftest.py
+++ flexmeasures/api/dev/tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 
+from flexmeasures.api.v3_0.tests.conftest import add_gas_sensor
+from flexmeasures.data.models.time_series import Sensor
+
 
 @pytest.fixture(scope="module")
 def setup_api_test_data(db, setup_roles_users, setup_generic_assets):
@@ -7,6 +10,7 @@ def setup_api_test_data(db, setup_roles_users, setup_generic_assets):
     Set up data for API dev tests.
     """
     print("Setting up data for API dev tests on %s" % db.engine)
+    add_gas_sensor(db, setup_roles_users["Test Supplier User"])
 
 
 @pytest.fixture(scope="function")
@@ -17,3 +21,6 @@ def setup_api_fresh_test_data(
     Set up fresh data for API dev tests.
     """
     print("Setting up fresh data for API dev tests on %s" % fresh_db.engine)
+    for sensor in Sensor.query.all():
+        fresh_db.delete(sensor)
+    add_gas_sensor(fresh_db, setup_roles_users_fresh_db["Test Supplier User"])
