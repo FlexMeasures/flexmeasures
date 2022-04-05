fd5e93956d62e34aaf7cb18054bcd85628de8542
F.N. Claessen <felix@seita.nl>
2022-04-05 11:30:50 +0200

    Increase default font size in charts
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/charts/defaults.py
+++ flexmeasures/data/models/charts/defaults.py
@@ -4,6 +4,7 @@ from typing import Callable, Union
 import altair as alt
 
 
+FONT_SIZE = 16
 HEIGHT = 300
 WIDTH = 600
 REDUCED_HEIGHT = REDUCED_WIDTH = 60
@@ -59,6 +60,34 @@ def apply_chart_defaults(fn):
             chart_specs["height"] = HEIGHT
         if "width" not in chart_specs:
             chart_specs["width"] = WIDTH
+
+        # Improve default legibility
+        if "config" not in chart_specs:
+            chart_specs["config"] = {}
+        if "axis" not in chart_specs["config"]:
+            chart_specs["config"]["axis"] = {}
+        chart_specs["config"]["axis"] = {
+            **chart_specs["config"]["axis"],
+            **dict(
+                titleFontSize=FONT_SIZE,
+                labelFontSize=FONT_SIZE,
+            ),
+        }
+        if "title" in chart_specs:
+            if isinstance(chart_specs["title"], str):
+                chart_specs["title"] = dict(text=chart_specs["title"])
+            chart_specs["title"] = {
+                **chart_specs["title"],
+                **dict(
+                    fontSize=FONT_SIZE,
+                ),
+            }
+        if (
+            "color" in chart_specs["encoding"]
+            and "legend" in chart_specs["encoding"]["color"]
+        ):
+            chart_specs["encoding"]["color"]["legend"]["titleFontSize"] = FONT_SIZE
+            chart_specs["encoding"]["color"]["legend"]["labelFontSize"] = FONT_SIZE
 
         # Add transform function to calculate full date
         if "transform" not in chart_specs:
