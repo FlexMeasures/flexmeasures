2f020d06cb27de30832cd5ac46193b67e15667de
F.N. Claessen <felix@seita.nl>
2022-04-16 11:09:32 +0200

    Add AccountIdField to schema imports
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/__init__.py
+++ flexmeasures/data/schemas/__init__.py
@@ -1,3 +1,4 @@
+from .account import AccountIdField  # noqa F401
 from .generic_assets import GenericAssetIdField as AssetIdField  # noqa F401
 from .sensors import SensorIdField  # noqa F401
 from .sources import DataSourceIdField as SourceIdField  # noqa F401
