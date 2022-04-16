16ec8326e75a0746f2b7ab26210ba91dee3adfad
F.N. Claessen <felix@seita.nl>
2022-04-16 11:07:56 +0200

    Add SourceIdField to schema imports
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/__init__.py
+++ flexmeasures/data/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .generic_assets import GenericAssetIdField as AssetIdField  # noqa F401
 from .sensors import SensorIdField  # noqa F401
+from .sources import DataSourceIdField as SourceIdField  # noqa F401
 from .times import AwareDateTimeField, DurationField  # noqa F401
