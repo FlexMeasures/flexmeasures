d21cf3f03fbc51441d23c12ccbfb40d0964910ad
F.N. Claessen <felix@seita.nl>
2022-06-20 09:44:41 +0200

    Fix test
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/charts/test_chart_defaults.py
+++ flexmeasures/data/models/charts/test_chart_defaults.py
@@ -6,4 +6,4 @@ from flexmeasures.data.models.charts.defaults import FIELD_DEFINITIONS
 def test_default_encodings():
     """Check default encodings for valid vega-lite specifications."""
     for field_name, field_definition in FIELD_DEFINITIONS.items():
-        assert alt.StringFieldDefWithCondition(**field_definition)
+        assert alt.PositionFieldDef(**field_definition)
