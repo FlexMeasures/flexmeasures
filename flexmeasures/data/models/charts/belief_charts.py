046574ae421998ba5bfdeb88522ae743963ba7ff
F.N. Claessen <felix@seita.nl>
2022-06-23 14:27:12 +0200

    Add chart specs for showing multiple sensors
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/charts/belief_charts.py
+++ flexmeasures/data/models/charts/belief_charts.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from flexmeasures.data.models.charts.defaults import FIELD_DEFINITIONS
 from flexmeasures.utils.flexmeasures_inflection import capitalize
 
@@ -40,6 +42,65 @@ def bar_chart(
             },
         ],
     }
+    for k, v in override_chart_specs.items():
+        chart_specs[k] = v
+    return chart_specs
+
+
+def point_chart_for_multiple_sensors(
+    sensors: list["Sensor"],  # noqa F821
+    **override_chart_specs: dict,
+):
+    sensors_specs = []
+    for sensor in sensors:
+        unit = sensor.unit if sensor.unit else "a.u."
+        event_value_field_definition = dict(
+            title=f"{capitalize(sensor.sensor_type)} ({unit})",
+            format=[".3s", unit],
+            formatType="quantityWithUnitFormat",
+            stack=None,
+            **{
+                **FIELD_DEFINITIONS["event_value"],
+                **dict(field=sensor.id),
+            },
+        )
+        sensor_specs = {
+            "title": capitalize(sensor.name) if sensor.name != sensor.sensor_type else None,
+            "mark": "point",
+            "encoding": {
+                "x": FIELD_DEFINITIONS["event_start"],
+                "x2": FIELD_DEFINITIONS["event_end"],
+                "y": event_value_field_definition,
+                "opacity": {"value": 0.7},
+                "tooltip": [
+                    FIELD_DEFINITIONS["full_date"],
+                    {
+                        **event_value_field_definition,
+                        **dict(title=f"{capitalize(sensor.sensor_type)}"),
+                    },
+                ],
+            },
+            "width": "container",
+            "transform": [
+                {
+                    "calculate": f"datum.event_start + {sensor.event_resolution.total_seconds() * 1000}",
+                    "as": "event_end",
+                },
+            ],
+        }
+        sensors_specs.append(sensor_specs)
+    chart_specs = dict(
+        description="A vertically concatenated bar chart.",
+        vconcat=[*sensors_specs],
+        spacing=100,
+        bounds="flush",
+    )
+    chart_specs["config"] = {
+        "view": {"continuousWidth": 800, "continuousHeight": 100},
+        "autosize": {"type": "fit-x", "contains": "padding"},
+        "legend": {"orient": "bottom", "columns": 1, "direction": "vertical"},
+    }
+    chart_specs["resolve"] = {'scale': {'x': 'shared'}}
     for k, v in override_chart_specs.items():
         chart_specs[k] = v
     return chart_specs
