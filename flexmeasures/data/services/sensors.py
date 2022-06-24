4e1833211d3f1d98b77cdd6816ab01f37e59228c
F.N. Claessen <felix@seita.nl>
2022-06-24 10:40:30 +0200

    Refactor: avoid redundant join and filter in case account_name is None
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/services/sensors.py
+++ flexmeasures/data/services/sensors.py
@@ -13,14 +13,16 @@ def get_sensors(
 
     :param account_name: optionally, filter by account name.
     """
-    sensor_query = Sensor.query.join(GenericAsset).filter(
-        Sensor.generic_asset_id == GenericAsset.id
-    )
+    sensor_query = Sensor.query
 
     if account_name is not None:
         account = Account.query.filter(Account.name == account_name).one_or_none()
         if not account:
             raise NotFound(f"There is no account named {account_name}!")
-        sensor_query = sensor_query.filter(GenericAsset.owner == account)
+        sensor_query = (
+            sensor_query.join(GenericAsset)
+            .filter(Sensor.generic_asset_id == GenericAsset.id)
+            .filter(GenericAsset.owner == account)
+        )
 
     return sensor_query.all()
