116d42205137c90f77711ba83d0197b6f7b81e5c
F.N. Claessen <felix@seita.nl>
2022-06-30 10:11:16 +0200

    Split fixtures, set up the building with a flexible device and two inflexible devices
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/tests/conftest.py
+++ flexmeasures/data/models/planning/tests/conftest.py
@@ -19,55 +19,125 @@ def setup_planning_test_data(db, add_market_prices, add_charging_station_assets)
 
 
 @pytest.fixture(scope="module")
-def add_inflexible_devices(db, setup_accounts) -> dict[str, Sensor]:
+def building(db, setup_accounts, setup_markets) -> GenericAsset:
     """
-    Set up inflexible devices.
+    Set up a building.
     """
     building_type = GenericAssetType(name="building")
     db.session.add(building_type)
-    test_building = GenericAsset(
-        name="Test building",
+    building = GenericAsset(
+        name="building",
         generic_asset_type=building_type,
         owner=setup_accounts["Prosumer"],
+        attributes=dict(
+            market_id=setup_markets["epex_da"].id,
+            capacity_in_mw=2,
+        ),
     )
-    db.session.add(test_building)
+    db.session.add(building)
+    return building
+
+
+@pytest.fixture(scope="module")
+def flexible_devices(db, building) -> dict[str, Sensor]:
+    """
+    Set up power sensors for flexible devices:
+    - A battery
+    - A Charge Point (todo)
+    """
+    battery_sensor = Sensor(
+        name="battery power sensor",
+        generic_asset=building,
+        event_resolution=timedelta(minutes=15),
+        attributes=dict(
+            capacity_in_mw=2,
+            max_soc_in_mwh=5,
+            min_soc_in_mwh=0,
+        ),
+        unit="MW",
+    )
+    db.session.add(battery_sensor)
+    return {
+        battery_sensor.name: battery_sensor,
+    }
+
+
+@pytest.fixture(scope="module")
+def inflexible_devices(db, building) -> dict[str, Sensor]:
+    """
+    Set up power sensors for inflexible devices:
+    - A PV panel
+    - Residual building demand
+    """
     pv_sensor = Sensor(
         name="PV power sensor",
-        generic_asset=test_building,
+        generic_asset=building,
         event_resolution=timedelta(hours=1),
         unit="kW",
         attributes={"capacity_in_mw": 2},
     )
     db.session.add(pv_sensor)
-    return {pv_sensor.name: pv_sensor}
+    residual_demand_sensor = Sensor(
+        name="residual demand power sensor",
+        generic_asset=building,
+        event_resolution=timedelta(hours=1),
+        unit="kW",
+        attributes={"capacity_in_mw": 2},
+    )
+    db.session.add(residual_demand_sensor)
+    return {
+        pv_sensor.name: pv_sensor,
+        residual_demand_sensor.name: residual_demand_sensor,
+    }
 
 
 @pytest.fixture(scope="module")
 def add_inflexible_device_forecasts(
-    db, add_inflexible_devices, setup_sources
+    db, inflexible_devices, setup_sources
 ) -> dict[Sensor, list[int | float]]:
     """
     Set up inflexible devices and forecasts.
     """
-    # 2 days of test data (each 8 hours at zero capacity, 8 hours at 90% capacity, and again 8 hours at zero capacity)
+    # 2 days of test data
     time_slots = pd.date_range(
-        datetime(2015, 1, 1), datetime(2015, 1, 3), freq="1H", closed="left"
+        datetime(2015, 1, 1), datetime(2015, 1, 3), freq="15T", closed="left"
     )
+
+    # PV (8 hours at zero capacity, 8 hours at 90% capacity, and again 8 hours at zero capacity)
     headroom = 0.1  # 90% of nominal capacity
-    sensor = add_inflexible_devices["PV power sensor"]
-    capacity = sensor.get_attribute("capacity_in_mw")
-    values = ([0] * 8 + [(1 - headroom) * capacity] * 8 + [0] * 8) * (
-        len(time_slots) // 24
+    pv_sensor = inflexible_devices["PV power sensor"]
+    capacity = pv_sensor.get_attribute("capacity_in_mw")
+    pv_values = (
+        [0] * (8 * 4) + [(1 - headroom) * capacity] * (8 * 4) + [0] * (8 * 4)
+    ) * (len(time_slots) // (24 * 4))
+    add_as_beliefs(db, pv_sensor, pv_values, time_slots, setup_sources["Seita"])
+
+    # Residual demand (1 MW continuously)
+    residual_demand_sensor = inflexible_devices["residual demand power sensor"]
+    residual_demand_values = [-1] * len(time_slots)
+    add_as_beliefs(
+        db,
+        residual_demand_sensor,
+        residual_demand_values,
+        time_slots,
+        setup_sources["Seita"],
     )
-    day1_beliefs = [
+
+    return {
+        pv_sensor: pv_values,
+        residual_demand_sensor: residual_demand_values,
+    }
+
+
+def add_as_beliefs(db, sensor, values, time_slots, source):
+    beliefs = [
         TimedBelief(
             event_start=as_server_time(dt),
             belief_horizon=timedelta(hours=0),
             event_value=val,
-            source=setup_sources["Seita"],
+            source=source,
             sensor=sensor,
         )
         for dt, val in zip(time_slots, values)
     ]
-    db.session.add_all(day1_beliefs)
-    return {sensor: values}
+    db.session.add_all(beliefs)
