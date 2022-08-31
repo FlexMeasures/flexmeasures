f024c2938a0d63352094915e3a91e5b7c9d6d5aa
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2022-08-31 11:24:54 +0200

    Remove bokeh dependency and obsolete UI views (#476)
    * Remove bokeh dependency, latest state plot, and the analytics and portfolio pages
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * black
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * flake8
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * better placement for button in asset popup
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * changelog entry
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    Co-authored-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/ui/tests/test_views.py
+++ flexmeasures/ui/tests/test_views.py
@@ -1,5 +1,4 @@
 from flask import url_for
-from flask_security import SQLAlchemySessionUserDatastore
 
 from flexmeasures.ui.tests.utils import logout
 
@@ -9,8 +8,7 @@ def test_dashboard_responds(client, setup_assets, as_prosumer_user1):
         url_for("flexmeasures_ui.dashboard_view"), follow_redirects=True
     )
     assert dashboard.status_code == 200
-    assert b"This dashboard shows you" in dashboard.data
-    assert b"Status of my assets" in dashboard.data
+    assert b"Dashboard" in dashboard.data
 
 
 def test_dashboard_responds_only_for_logged_in_users(client, as_prosumer_user1):
@@ -19,14 +17,6 @@ def test_dashboard_responds_only_for_logged_in_users(client, as_prosumer_user1):
         url_for("flexmeasures_ui.dashboard_view"), follow_redirects=True
     )
     assert b"Please log in" in dashboard.data
-
-
-def test_portfolio_responds(client, setup_assets, as_prosumer_user1):
-    portfolio = client.get(
-        url_for("flexmeasures_ui.portfolio_view"), follow_redirects=True
-    )
-    assert portfolio.status_code == 200
-    assert b"Portfolio status" in portfolio.data
 
 
 def test_assets_responds(client, requests_mock, as_prosumer_user1):
@@ -44,21 +34,6 @@ def test_control_responds(client, as_prosumer_user1):
     control = client.get(url_for("flexmeasures_ui.control_view"), follow_redirects=True)
     assert control.status_code == 200
     assert b"Control actions" in control.data
-
-
-def test_analytics_responds(db, client, setup_assets, as_prosumer_user1):
-    analytics = client.get(
-        url_for("flexmeasures_ui.analytics_view"), follow_redirects=True
-    )
-    assert analytics.status_code == 200
-    assert b"Client analytics" in analytics.data
-
-    from flexmeasures.data.models.user import User, Role
-
-    user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
-    test_prosumer = user_datastore.find_user(email="test_prosumer_user@seita.nl")
-
-    assert str.encode(f"{test_prosumer.username}") in analytics.data
 
 
 def test_logout(client, as_prosumer_user1):
