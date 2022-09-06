FlexMeasures/flexmeasures

commit 2dfd08033413ce2081cf64626922a364f276678c

Do not resample data of sensors recording instantaneous values (#491)
Do not fail asset page if one of the shown sensors records instantaneous values.


* Do not resample data of sensors recording instantaneous values

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Use linear interpolation for instantaneous sensors

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Changelog entry

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Fix tooltip

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Fix case with only instantaneous sensors

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Fix changelog entry for PR #493 / Issue #492

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Fix changelog date format

Signed-off-by: F.N. Claessen <felix@seita.nl>

* Add release date for 0.11.1

Signed-off-by: F.N. Claessen <felix@seita.nl>

Signed-off-by: F.N. Claessen <felix@seita.nl>

flexmeasures/data/models/charts/belief_charts.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flexmeasures.data.models.charts.defaults import FIELD_DEFINITIONS
 from flexmeasures.utils.flexmeasures_inflection import capitalize
@@ -72,8 +72,13 @@ def chart_for_multiple_sensors(
     **override_chart_specs: dict,
 ):
     sensors_specs = []
-    min_resolution_in_ms = (
-        min(sensor.event_resolution for sensor in sensors).total_seconds() * 1000
+    condition = (
+        sensor.event_resolution
+        for sensor in sensors
+        if sensor.event_resolution > timedelta(0)
+    )
+    minimum_non_zero_resolution_in_ms = (
+        min(condition).total_seconds() * 1000 if any(condition) else 0
     )
     for sensor in sensors:
         unit = sensor.unit if sensor.unit else "a.u."
@@ -112,7 +117,9 @@ def chart_for_multiple_sensors(
                 {
                     "mark": {
                         "type": "line",
-                        "interpolate": "step-after",
+                        "interpolate": "step-after"
+                        if sensor.event_resolution != timedelta(0)
+                        else "linear",
                         "clip": True,
                     },
                     "encoding": {
@@ -143,7 +150,7 @@ def chart_for_multiple_sensors(
                     },
                     "transform": [
                         {
-                            "calculate": f"datum.event_start + {min_resolution_in_ms}",
+                            "calculate": f"datum.event_start + {minimum_non_zero_resolution_in_ms}",
                             "as": "event_end",
                         },
                     ],

flexmeasures/data/models/generic_assets.py
@@ -381,10 +381,18 @@ class GenericAsset(db.Model, AuthModelMixin):
             from flexmeasures.data.services.time_series import simplify_index
 
             if sensors:
-                min_resolution = min(bdf.event_resolution for bdf in bdf_dict.values())
+                condition = (
+                    bdf.event_resolution
+                    for bdf in bdf_dict.values()
+                    if bdf.event_resolution > timedelta(0)
+                )
+                minimum_non_zero_resolution = (
+                    min(condition) if any(condition) else timedelta(0)
+                )
                 df_dict = {}
                 for sensor, bdf in bdf_dict.items():
-                    bdf = bdf.resample_events(min_resolution)
+                    if bdf.event_resolution > timedelta(0):
+                        bdf = bdf.resample_events(minimum_non_zero_resolution)
                     df = simplify_index(
                         bdf, index_levels_to_columns=["source"]
                     ).set_index(["source"], append=True)

commit 9316e12fb09d106a875ac289bcc0733ceb5dba6d

Fix for bug introduced in PR #491
Signed-off-by: F.N. Claessen <felix@seita.nl>

flexmeasures/data/models/charts/belief_charts.py
@@ -72,7 +72,7 @@ def chart_for_multiple_sensors(
     **override_chart_specs: dict,
 ):
     sensors_specs = []
-    condition = (
+    condition = list(
         sensor.event_resolution
         for sensor in sensors
         if sensor.event_resolution > timedelta(0)

flexmeasures/data/models/generic_assets.py
@@ -381,7 +381,7 @@ class GenericAsset(db.Model, AuthModelMixin):
             from flexmeasures.data.services.time_series import simplify_index
 
             if sensors:
-                condition = (
+                condition = list(
                     bdf.event_resolution
                     for bdf in bdf_dict.values()
                     if bdf.event_resolution > timedelta(0)