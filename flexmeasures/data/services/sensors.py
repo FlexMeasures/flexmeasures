983becfadd16afeec125dc669b98974a0758818c
F.N. Claessen <felix@seita.nl>
2022-09-12 13:36:27 +0200

    flake8
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/services/sensors.py
+++ flexmeasures/data/services/sensors.py
@@ -19,13 +19,13 @@ def get_sensors(
 
     if account is not None:
         if isinstance(account, int):
-            account = Account.query.filter_by(id=account_id).one_or_none()
+            account = Account.query.filter_by(id=account).one_or_none()
             if not account:
-                raise NotFound(f"There is no account with id {account_id}!")
+                raise NotFound(f"There is no account with id {account}!")
         elif isinstance(account, str):
-            account = Account.query.filter_by(name=account_name).one_or_none()
+            account = Account.query.filter_by(name=account).one_or_none()
             if not account:
-                raise NotFound(f"There is no account named {account_name}!")
+                raise NotFound(f"There is no account named {account}!")
         sensor_query = (
             sensor_query.join(GenericAsset)
             .filter(Sensor.generic_asset_id == GenericAsset.id)
