433102bab6466b6285a22be93d7691978093c2e5
F.N. Claessen <felix@seita.nl>
2022-10-09 19:39:45 +0200

    Must pass a list of accounts
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/services/sensors.py
+++ flexmeasures/data/services/sensors.py
@@ -7,33 +7,32 @@ from flexmeasures.data.models.generic_assets import GenericAsset
 
 
 def get_sensors(
-    accounts: list[Account] = None,
+    accounts: list[Account],
     include_public_assets: bool = False,
     filter_by_sensor_ids: list[int] | None = None,
     filter_by_sensor_names: list[str] | None = None,
 ) -> list[Sensor]:
     """Return a list of Sensor objects that belong to any of the given accounts, and/or public sensors.
 
-    :param accounts:                optionally, select only sensors from this list of accounts
+    :param accounts:                select only sensors from this list of accounts
     :param include_public_assets:   if True, include sensors that belong to a public asset
     :param filter_by_sensor_ids:    optionally, filter by sensor id
     :param filter_by_sensor_names:  optionally, filter by sensor name
     """
     sensor_query = Sensor.query
-    if accounts is not None:
-        account_ids = [account.id for account in accounts]
-        sensor_query = sensor_query.join(GenericAsset).filter(
-            Sensor.generic_asset_id == GenericAsset.id
-        )
-        if include_public_assets:
-            sensor_query = sensor_query.filter(
-                sa.or_(
-                    GenericAsset.account_id.in_(account_ids),
-                    GenericAsset.account_id.is_(None),
-                )
+    account_ids = [account.id for account in accounts]
+    sensor_query = sensor_query.join(GenericAsset).filter(
+        Sensor.generic_asset_id == GenericAsset.id
+    )
+    if include_public_assets:
+        sensor_query = sensor_query.filter(
+            sa.or_(
+                GenericAsset.account_id.in_(account_ids),
+                GenericAsset.account_id.is_(None),
             )
-        else:
-            sensor_query = sensor_query.filter(GenericAsset.account_id.in_(account_ids))
+        )
+    else:
+        sensor_query = sensor_query.filter(GenericAsset.account_id.in_(account_ids))
     if filter_by_sensor_ids:
         sensor_query = sensor_query.filter(Sensor.id.in_(filter_by_sensor_ids))
     if filter_by_sensor_names:
