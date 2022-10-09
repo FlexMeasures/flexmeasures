2546bca650e97fab72c3bb55fa88b7b71af35788
F.N. Claessen <felix@seita.nl>
2022-10-09 11:04:42 +0200

    Allow to filter sensors by name
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/services/sensors.py
+++ flexmeasures/data/services/sensors.py
@@ -9,12 +9,14 @@ from flexmeasures.data.models.generic_assets import GenericAsset
 def get_sensors(
     accounts: list[Account | None] = None,
     filter_by_sensor_ids: list[int] | None = None,
+    filter_by_sensor_names: list[str] | None = None,
 ) -> list[Sensor]:
     """Return a list of Sensor objects that belong to any of the given accounts, and/or public sensors.
 
-    :param accounts:             optionally, select only sensors from this list of accounts
-                                 (include None to select sensors that belong to a public asset).
-    :param filter_by_sensor_ids: optionally, filter by sensor id.
+    :param accounts:                optionally, select only sensors from this list of accounts
+                                    (include None to select sensors that belong to a public asset).
+    :param filter_by_sensor_ids:    optionally, filter by sensor id.
+    :param filter_by_sensor_names:  optionally, filter by sensor name.
     """
     sensor_query = Sensor.query
     if accounts is not None:
@@ -33,4 +35,6 @@ def get_sensors(
             sensor_query = sensor_query.filter(GenericAsset.account_id.in_(account_ids))
     if filter_by_sensor_ids:
         sensor_query = sensor_query.filter(Sensor.id.in_(filter_by_sensor_ids))
+    if filter_by_sensor_names:
+        sensor_query = sensor_query.filter(Sensor.name.in_(filter_by_sensor_names))
     return sensor_query.all()
