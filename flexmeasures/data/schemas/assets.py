7633d7322f9d5b5109785ea3bc273440a2694055
F.N. Claessen <felix@seita.nl>
2022-11-01 09:39:48 +0100

    Raise rather than round
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/assets.py
+++ flexmeasures/data/schemas/assets.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from marshmallow import validates, ValidationError, validates_schema, fields, validate
 
 from flexmeasures.data import ma
@@ -5,27 +7,87 @@ from flexmeasures.data.models.assets import Asset, AssetType
 from flexmeasures.data.models.time_series import Sensor
 from flexmeasures.data.models.user import User
 from flexmeasures.data.schemas.sensors import SensorSchemaMixin
-from flexmeasures.data.schemas.utils import MarshmallowClickMixin
+from flexmeasures.data.schemas.utils import FMValidationError, MarshmallowClickMixin
+
+
+class LatitudeLongitudeValidator(validate.Validator):
+    """Validator which succeeds if the value passed has at most 7 decimal places."""
+
+    def __init__(self, *, error: str | None = None):
+        self.error = error
+
+    def __call__(self, value):
+        if not round(value, 7) == value:
+            raise FMValidationError(
+                "Latitudes and longitudes are limited to 7 decimal places."
+            )
+        return value
+
+
+class LatitudeValidator(validate.Validator):
+    """Validator which succeeds if the value passed is in the range [-90, 90]."""
+
+    def __init__(self, *, error: str | None = None, allow_none: bool = False):
+        self.error = error
+        self.allow_none = allow_none
+
+    def __call__(self, value):
+        if self.allow_none and value is None:
+            return
+        if value < -90:
+            raise FMValidationError(
+                f"Latitude {value} exceeds the minimum latitude of -90 degrees."
+            )
+        if value > 90:
+            raise ValidationError(
+                f"Latitude {value} exceeds the maximum latitude of 90 degrees."
+            )
+        return value
+
+
+class LongitudeValidator(validate.Validator):
+    """Validator which succeeds if the value passed is in the range [-180, 180]."""
+
+    def __init__(self, *, error: str | None = None, allow_none: bool = False):
+        self.error = error
+        self.allow_none = allow_none
+
+    def __call__(self, value):
+        if self.allow_none and value is None:
+            return
+        if value < -180:
+            raise FMValidationError(
+                f"Longitude {value} exceeds the minimum longitude of -180 degrees."
+            )
+        if value > 180:
+            raise ValidationError(
+                f"Longitude {value} exceeds the maximum longitude of 180 degrees."
+            )
+        return value
 
 
 class LatitudeField(MarshmallowClickMixin, fields.Float):
-    """Field that deserializes to a latitude with 7 places."""
+    """Field that deserializes to a latitude float with max 7 decimal places."""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(validate=validate.Range(min=-90, max=90))
-
-    def _deserialize(self, value, attr, obj, **kwargs) -> float:
-        return round(super()._deserialize(value, attr, obj, **kwargs), 7)
+        super().__init__()
+        # Insert validation into self.validators so that multiple errors can be stored.
+        self.validators.insert(0, LatitudeLongitudeValidator())
+        self.validators.insert(
+            0, LatitudeValidator(allow_none=kwargs.get("allow_none", False))
+        )
 
 
 class LongitudeField(MarshmallowClickMixin, fields.Float):
-    """Field that deserializes to a longitude with 7 places."""
+    """Field that deserializes to a longitude float with max 7 decimal places."""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(validate=validate.Range(min=-180, max=180))
-
-    def _deserialize(self, value, attr, obj, **kwargs) -> float:
-        return round(super()._deserialize(value, attr, obj, **kwargs), 7)
+        super().__init__()
+        # Insert validation into self.validators so that multiple errors can be stored.
+        self.validators.insert(0, LatitudeLongitudeValidator())
+        self.validators.insert(
+            0, LongitudeValidator(allow_none=kwargs.get("allow_none", False))
+        )
 
 
 class AssetSchema(SensorSchemaMixin, ma.SQLAlchemySchema):
--- flexmeasures/data/schemas/tests/test_latitude_longitude.py
+++ flexmeasures/data/schemas/tests/test_latitude_longitude.py
@@ -10,8 +10,9 @@ from flexmeasures.data.schemas.utils import ValidationError
     ("input", "exp_deserialization"),
     [
         (0, 0),
-        (0.12345678, 0.1234568),  # note the rounding to 7 digits
+        (0.1234567, 0.1234567),
         (-90, -90),
+        (90, 90),
     ],
 )
 def test_latitude(input, exp_deserialization):
@@ -23,30 +24,34 @@ def test_latitude(input, exp_deserialization):
 
 
 @pytest.mark.parametrize(
-    ("input", "error_msg"),
+    ("input", "error_messages"),
     [
-        (-90.01, "Must be greater than or equal to -90 and less than or equal to 90."),
-        # Even though it would have been a valid latitude after rounding to 7 decimal places
+        ("ninety", ["Not a valid number."]),
+        (90.01, ["Latitude 90.01 exceeds the maximum latitude of 90 degrees."]),
+        (0.12345678, ["Latitudes and longitudes are limited to 7 decimal places."]),
         (
-            -90.0000001,
-            "Must be greater than or equal to -90 and less than or equal to 90.",
+            -90.00000001,
+            [
+                "Latitude -90.00000001 exceeds the minimum latitude of -90 degrees.",
+                "Latitudes and longitudes are limited to 7 decimal places.",
+            ],
         ),
-        ("ninety", "Not a valid number."),
     ],
 )
-def test_latitude_field_invalid(input, error_msg):
+def test_latitude_field_invalid(input, error_messages):
     lf = LatitudeField()
     with pytest.raises(ValidationError) as ve:
         lf.deserialize(input, None, None)
-    assert error_msg in str(ve)
+    assert error_messages == ve.value.messages
 
 
 @pytest.mark.parametrize(
     ("input", "exp_deserialization"),
     [
         (0, 0),
-        (0.12345678, 0.1234568),  # note the rounding to 7 digits
+        (0.1234567, 0.1234567),
         (-180, -180),
+        (180, 180),
     ],
 )
 def test_longitude(input, exp_deserialization):
@@ -58,22 +63,24 @@ def test_longitude(input, exp_deserialization):
 
 
 @pytest.mark.parametrize(
-    ("input", "error_msg"),
+    ("input", "error_messages"),
     [
+        ("one-hundred-and-eighty", ["Not a valid number."]),
         (
             -180.01,
-            "Must be greater than or equal to -180 and less than or equal to 180.",
+            ["Longitude -180.01 exceeds the minimum longitude of -180 degrees."],
         ),
-        # Even though it would have been a valid latitude after rounding to 7 decimal places
         (
-            -180.0000001,
-            "Must be greater than or equal to -180 and less than or equal to 180.",
+            -180.00000001,
+            [
+                "Longitude -180.00000001 exceeds the minimum longitude of -180 degrees.",
+                "Latitudes and longitudes are limited to 7 decimal places.",
+            ],
         ),
-        ("one-hundred-and-eighty", "Not a valid number."),
     ],
 )
-def test_longitude_field_invalid(input, error_msg):
+def test_longitude_field_invalid(input, error_messages):
     lf = LongitudeField()
     with pytest.raises(ValidationError) as ve:
         lf.deserialize(input, None, None)
-    assert error_msg in str(ve)
+    assert error_messages == ve.value.messages
