3bf1b973232b641ccc13ca51dab5760a14f90ae7
F.N. Claessen <felix@seita.nl>
2022-11-10 19:33:39 +0100

    flake8
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/battery.py
+++ flexmeasures/data/models/planning/battery.py
@@ -5,7 +5,7 @@ def schedule_battery(*args, **kwargs):
     import warnings
 
     warnings.warn(
-        f"The schedule_battery method is deprecated and will be removed from flexmeasures in a future version. Replace with StorageScheduler().schedule to suppress this warning.",
+        "The schedule_battery method is deprecated and will be removed from flexmeasures in a future version. Replace with StorageScheduler().schedule to suppress this warning.",
         FutureWarning,
     )
     return StorageScheduler().schedule(*args, **kwargs)
--- flexmeasures/data/models/planning/charging_station.py
+++ flexmeasures/data/models/planning/charging_station.py
@@ -5,7 +5,7 @@ def schedule_charging_station(*args, **kwargs):
     import warnings
 
     warnings.warn(
-        f"The schedule_charging_station method is deprecated and will be removed from flexmeasures in a future version. Replace with StorageScheduler().schedule to suppress this warning.",
+        "The schedule_charging_station method is deprecated and will be removed from flexmeasures in a future version. Replace with StorageScheduler().schedule to suppress this warning.",
         FutureWarning,
     )
     return StorageScheduler().schedule(*args, **kwargs)
