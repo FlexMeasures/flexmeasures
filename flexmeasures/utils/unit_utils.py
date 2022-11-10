e77803e157ffd8bed4dcb20462faac1085437a16
F.N. Claessen <felix@seita.nl>
2022-11-10 14:28:38 +0100

    Refactor and add inline comment explaining why we check to_unit for a digit
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/utils/unit_utils.py
+++ flexmeasures/utils/unit_utils.py
@@ -217,16 +217,15 @@ def _convert_time_units(
 
     Use Unix epoch or the requested time unit, respectively.
     """
-    if to_unit[0].isdigit():
-        to_unit = pd.Timedelta(to_unit)
-    else:
-        to_unit = pd.Timedelta(1, to_unit)
+    if not to_unit[0].isdigit():
+        # unit abbreviations passed to pd.Timedelta need a number (so, for example, h becomes 1h)
+        to_unit = f"1{to_unit}"
     if from_unit == "datetime":
         return (
             pd.to_datetime(data, utc=True) - pd.Timestamp("1970-01-01", tz="utc")
-        ) // to_unit
+        ) // pd.Timedelta(to_unit)
     else:
-        return data / to_unit
+        return data / pd.Timedelta(to_unit)
 
 
 def convert_units(
