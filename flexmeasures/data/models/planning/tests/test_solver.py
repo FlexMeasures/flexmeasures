51e5809ed9e105ed3472264f49d311a38cac2000
F.N. Claessen <felix@seita.nl>
2022-11-11 15:46:57 +0100

    Refactor: a single parameterized test instead of 2 tests with a lot of duplicate code
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/tests/test_solver.py
+++ flexmeasures/data/models/planning/tests/test_solver.py
@@ -272,120 +272,49 @@ def test_fallback_to_unsolvable_problem(target_soc, charging_station_name):
     )
 
 
+@pytest.mark.parametrize(
+    "market_scenario",
+    [
+        "dynamic contract",
+        "fixed contract",
+    ],
+)
 def test_building_solver_day_2(
     db,
     add_battery_assets,
     add_market_prices,
-    add_inflexible_device_forecasts,
-    inflexible_devices,
-    flexible_devices,
-):
-    """Check battery scheduling results within the context of a building with PV, for day 2,
-    which is set up with 8 expensive, then 8 cheap, then again 8 expensive hours.
-    We expect the scheduler to:
-    - completely discharge within the first 8 hours (with high prices)
-    - completely charge within the next 8 hours (with low prices)
-    - completely discharge within the last 8 hours (with high prices)
-    """
-    epex_da = Sensor.query.filter(Sensor.name == "epex_da").one_or_none()
-    battery = flexible_devices["battery power sensor"]
-    building = battery.generic_asset
-    assert battery.get_attribute("market_id") == epex_da.id
-    tz = pytz.timezone("Europe/Amsterdam")
-    start = tz.localize(datetime(2015, 1, 2))
-    end = tz.localize(datetime(2015, 1, 3))
-    resolution = timedelta(minutes=15)
-    soc_at_start = 2.5
-    soc_min = 0.5
-    soc_max = 4.5
-    storage_specs = ensure_storage_specs(
-        dict(
-            soc_at_start=soc_at_start,
-            soc_min=soc_min,
-            soc_max=soc_max,
-        ),
-        battery,
-        start,
-        end,
-        resolution,
-    )
-    schedule = StorageScheduler().schedule(
-        battery,
-        start,
-        end,
-        resolution,
-        storage_specs=storage_specs,
-        inflexible_device_sensors=inflexible_devices.values(),
-    )
-    soc_schedule = integrate_time_series(schedule, soc_at_start, decimal_precision=6)
-
-    with pd.option_context("display.max_rows", None, "display.max_columns", 3):
-        print(soc_schedule)
-
-    # Check if constraints were met
-    capacity = pd.DataFrame(
-        data=np.sum(np.array(list(add_inflexible_device_forecasts.values())), axis=0),
-        columns=["inflexible"],
-    ).tail(
-        -4 * 24
-    )  # remove first 96 quarter-hours (the schedule is about the 2nd day)
-    capacity["max"] = building.get_attribute("capacity_in_mw")
-    capacity["min"] = -building.get_attribute("capacity_in_mw")
-    capacity["production headroom"] = capacity["max"] - capacity["inflexible"]
-    capacity["consumption headroom"] = capacity["inflexible"] - capacity["min"]
-    capacity["battery production headroom"] = capacity["production headroom"].clip(
-        upper=battery.get_attribute("capacity_in_mw")
-    )
-    capacity["battery consumption headroom"] = capacity["consumption headroom"].clip(
-        upper=battery.get_attribute("capacity_in_mw")
-    )
-    capacity[
-        "schedule"
-    ] = schedule.values  # consumption is positive, production is negative
-    with pd.option_context(
-        "display.max_rows", None, "display.max_columns", None, "display.width", 2000
-    ):
-        print(capacity)
-    assert (capacity["schedule"] >= -capacity["battery production headroom"]).all()
-    assert (capacity["schedule"] <= capacity["battery consumption headroom"]).all()
-
-    for soc in soc_schedule.values:
-        assert soc >= max(soc_min, battery.get_attribute("min_soc_in_mwh"))
-        assert soc <= battery.get_attribute("max_soc_in_mwh")
-
-    # Check whether the resulting soc schedule follows our expectations for 8 expensive, 8 cheap and 8 expensive hours
-    assert soc_schedule.iloc[-1] == max(
-        soc_min, battery.get_attribute("min_soc_in_mwh")
-    )  # Battery sold out at the end of its planning horizon
-
-    assert soc_schedule.loc[start + timedelta(hours=8)] == max(
-        soc_min, battery.get_attribute("min_soc_in_mwh")
-    )  # Sell what you begin with
-    assert soc_schedule.loc[start + timedelta(hours=16)] == min(
-        soc_max, battery.get_attribute("max_soc_in_mwh")
-    )  # Buy what you can to sell later
-
-
-def test_building_solver_with_fixed_tariffs_day_2(
-    db,
-    add_battery_assets,
     create_test_tariffs,
     add_inflexible_device_forecasts,
     inflexible_devices,
     flexible_devices,
+    market_scenario: str,
 ):
-    """Check battery scheduling results within the context of a building with PV, for day 2,
-    against a fixed consumption tariff and a fixed feed-in tariff that is lower.
-    We expect the scheduler to:
-    - completely discharge within the first 8 hours (with net consumption)
-    - completely charge within the next 8 hours (with net production)
-    - completely discharge within the last 8 hours (with net consumption)
+    """Check battery scheduling results within the context of a building with PV, for day 2, against the following market scenarios:
+    1) a dynamic tariff with equal consumption and feed-in tariffs, that is set up with 8 expensive, then 8 cheap, then again 8 expensive hours.
+    2) a fixed consumption tariff and a fixed feed-in tariff that is lower.
+    In the test data:
+    - Hours with net production coincide with low dynamic market prices.
+    - Hours with net consumption coincide with high dynamic market prices.
+    In either scenario, we expect the scheduler to:
+    - completely discharge within the first 8 hours (either due to 1) high prices, or 2) net consumption)
+    - completely charge within the next 8 hours (either due to 1) low prices, or 2) net production)
+    - completely discharge within the last 8 hours (either due to 1) high prices, or 2) net consumption)
     """
-    consumption_price_sensor = create_test_tariffs["consumption_price_sensor"]
-    production_price_sensor = create_test_tariffs["production_price_sensor"]
     battery = flexible_devices["battery power sensor"]
     building = battery.generic_asset
-
+    if market_scenario == "dynamic contract":
+        consumption_price_sensor = Sensor.query.filter(
+            Sensor.name == "epex_da"
+        ).one_or_none()
+        production_price_sensor = consumption_price_sensor
+        assert battery.get_attribute("market_id") == consumption_price_sensor.id
+    elif market_scenario == "fixed contract":
+        consumption_price_sensor = create_test_tariffs["consumption_price_sensor"]
+        production_price_sensor = create_test_tariffs["production_price_sensor"]
+    else:
+        raise NotImplementedError(
+            f"Missing test case for market conditions '{market_scenario}'"
+        )
     tz = pytz.timezone("Europe/Amsterdam")
     start = tz.localize(datetime(2015, 1, 2))
     end = tz.localize(datetime(2015, 1, 3))
@@ -410,8 +339,6 @@ def test_building_solver_with_fixed_tariffs_day_2(
         end,
         resolution,
         storage_specs=storage_specs,
-        consumption_price_sensor=consumption_price_sensor,
-        production_price_sensor=production_price_sensor,
         inflexible_device_sensors=inflexible_devices.values(),
     )
     soc_schedule = integrate_time_series(schedule, soc_at_start, decimal_precision=6)
@@ -450,14 +377,24 @@ def test_building_solver_with_fixed_tariffs_day_2(
         assert soc >= max(soc_min, battery.get_attribute("min_soc_in_mwh"))
         assert soc <= battery.get_attribute("max_soc_in_mwh")
 
-    # Check whether the resulting soc schedule follows our expectations for 8 net-consumption, 8 net-production and 8 net-consumption hours
+    # Check whether the resulting soc schedule follows our expectations for:
+    # 1) 8 expensive, 8 cheap and 8 expensive hours
+    # 2) 8 net-consumption, 8 net-production and 8 net-consumption hours
+
+    # 1) The battery sold out at the end of its planning horizon
+    # 2) The battery discharged as far as it could during the last 8 net-consumption hours
     assert soc_schedule.iloc[-1] == max(
         soc_min, battery.get_attribute("min_soc_in_mwh")
-    )  # The battery discharged as far as it could during the last 8 net-consumption hours
+    )
 
+    # 1) Sell what you begin with
+    # 2) The battery discharged as far as it could during the first 8 net-consumption hours
     assert soc_schedule.loc[start + timedelta(hours=8)] == max(
         soc_min, battery.get_attribute("min_soc_in_mwh")
-    )  # The battery discharged as far as it could during the first 8 net-consumption hours
+    )
+
+    # 1) Buy what you can to sell later
+    # 2) The battery charged with PV power as far as it could during the middle 8 net-production hours
     assert soc_schedule.loc[start + timedelta(hours=16)] == min(
         soc_max, battery.get_attribute("max_soc_in_mwh")
-    )  # battery charged with PV power as far as it could during the middle 8 net-production hours
+    )
