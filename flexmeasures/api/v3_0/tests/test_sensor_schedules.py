510afa2b33210e98839d3390bc7af6a10d476771
F.N. Claessen <felix@seita.nl>
2022-12-31 14:50:59 +0100

    Add todo
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/tests/test_sensor_schedules.py
+++ flexmeasures/api/v3_0/tests/test_sensor_schedules.py
@@ -28,6 +28,7 @@ from flexmeasures.utils.calculations import integrate_time_series
             "Not a valid number",
         ),
         (message_for_trigger_schedule(), "soc-unit", "MWH", "Must be one of"),
+        # todo: add back test in case we stop grandfathering ignoring too-far-into-the-future targets, or amend otherwise
         # (
         #     message_for_trigger_schedule(
         #         with_targets=True, too_far_into_the_future_targets=True
