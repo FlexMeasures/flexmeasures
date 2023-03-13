4364b5286768b15805c5fc5729717319e553f017
Nicolas Höning <nicolas@seita.nl>
2023-03-13 18:27:24 +0100

    use utility function for headers, fix typo
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/api/v3_0/tests/test_accounts_api.py
+++ flexmeasures/api/v3_0/tests/test_accounts_api.py
@@ -10,8 +10,9 @@ def test_get_accounts_missing_auth(client):
     Attempt to get accounts with missing auth.
     """
     # the case without auth: authentication will fail
-    headers = {"content-type": "application/json"}
-    get_accounts_response = client.get(url_for("AccountAPI:index"), headers=headers)
+    get_accounts_response = client.get(
+        url_for("AccountAPI:index"), headers=make_headers_for(None, client)
+    )
     print("Server responded with:\n%s" % get_accounts_response.data)
     assert get_accounts_response.status_code == 401
 
@@ -21,12 +22,10 @@ def test_get_accounts(client, setup_api_test_data, as_admin):
     """
     Get accounts
     """
-    headers = {"content-type": "application/json"}
     if as_admin:
-        user_email = "test_admin_user@seita.nl"
+        headers = make_headers_for("test_admin_user@seita.nl", client)
     else:
-        user_email = "test_prosumer_user@seita.nl"
-    headers["Authorization"] = get_auth_token(client, user_email, "testtest")
+        headers = make_headers_for("test_prosumer_user@seita.nl", client)
     get_accounts_response = client.get(
         url_for("AccountAPI:index"),
         headers=headers,
@@ -40,29 +39,34 @@ def test_get_accounts(client, setup_api_test_data, as_admin):
 
 
 @pytest.mark.parametrize(
-    "requesting_user,status_code",
+    "requesting_user_email,status_code",
     [
         (None, 401),  # no auth is not allowed
-        ("test_prosumer_user_2@seita.nl", 200),  # gets their won account, ok
+        ("test_prosumer_user_2@seita.nl", 200),  # gets their own account, okay
         ("test_dummy_user_3@seita.nl", 403),  # gets from other account
         ("test_admin_user@seita.nl", 200),  # admin can do this from another account
     ],
 )
-def test_get_one_account(client, setup_api_test_data, requesting_user, status_code):
+def test_get_one_account(
+    client, setup_api_test_data, requesting_user_email, status_code
+):
     """Get one account"""
     test_user2_account_id = find_user_by_email(
         "test_prosumer_user_2@seita.nl"
     ).account.id
-    headers = {"content-type": "application/json"}
-    if requesting_user:
-        headers["Authorization"] = get_auth_token(client, requesting_user, "testtest")
-
     get_account_response = client.get(
         url_for("AccountAPI:get", id=test_user2_account_id),
-        headers=headers,
+        headers=make_headers_for(requesting_user_email, client),
     )
     print("Server responded with:\n%s" % get_account_response.data)
     assert get_account_response.status_code == status_code
     if status_code == 200:
         assert get_account_response.json["name"] == "Test Prosumer Account"
         assert get_account_response.json["account_roles"] == [1]
+
+
+def make_headers_for(user_email: str | None, client) -> dict:
+    headers = {"content-type": "application/json"}
+    if user_email:
+        headers["Authorization"] = get_auth_token(client, user_email, "testtest")
+    return headers
