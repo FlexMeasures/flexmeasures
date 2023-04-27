064c250d7ab6d753302d38736baee900fc5b2af0
Ahmad <ahmedwahid16101@gmail.com>
2023-04-27 14:13:21 +0200

    update database bash file to create backup in the current dir of flexmeasures
    Signed-off-by: Ahmad <ahmedwahid16101@gmail.com>

--- flexmeasures/data/scripts/clean_database.sh
+++ flexmeasures/data/scripts/clean_database.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd /temp
+MAIN_DIR=$(pwd)
 
 # function for checking database existence
 function is_database() {
+ cd /tmp
  sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -wq $1
+ cd ${MAIN_DIR}
 }
-
 
 # function for creating a new database
 function create_database() {
@@ -15,38 +16,43 @@ function create_database() {
  sudo -i -u postgres psql -c "\c $1" -c "CREATE EXTENSION cube;"
  echo "Creating earthdistance extension in $1 ..."
  sudo -i -u postgres psql -c "\c $1" -c "CREATE EXTENSION earthdistance;"
-
  echo "Updating database structure ..."
  flexmeasures db upgrade
 }
 
+# function for deleting the old database
+function delete_database() {
+ echo "Deleting database ..."
+ sudo -i -u postgres dropdb -U postgres $1
+}
 
 # Check if the database name is provided
+
 if [ -z "$1" ]; then
   echo "Error: db_name is required. Please provide a value for db_name, e.g., make clean_db db_name=flexmeasures-db"
   exit 1
 fi
 
-
 # Check if the database exists
 if is_database $1
 then
   echo "$1 exists"
-
   read -r -p "Make a backup first? [y/N] " response
   response=${response,,}    # tolower
   if [[ "$response" =~ ^(yes|y)$ ]]; then
     echo "Making db dump ..."
+    cd ${MAIN_DIR}
     flexmeasures db-ops dump
   fi
 
   read -r -p "This will drop your database and re-create a clean one. Continue?[y/N] " response
   response=${response,,}
+
   if [[ "$response" =~ ^(yes|y)$ ]]; then
-     echo "Deleting database ..."
-     sudo -i -u postgres dropdb -U postgres $1
+     delete_database $1
      create_database $1
   fi
+
 # otherwise, create a fresh database
 else
   echo "$1 does not exist"
