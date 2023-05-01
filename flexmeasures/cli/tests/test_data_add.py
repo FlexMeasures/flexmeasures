ba8c8ef4deaf98682153368462e4c6904c5ca473
Victor Garcia Reolid <victor@seita.nl>
2023-05-01 09:33:50 +0200

    Add command flexmeasures add report.
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/cli/tests/test_data_add.py
+++ flexmeasures/cli/tests/test_data_add.py
@@ -1,4 +1,9 @@
 import pytest
+import json
+from datetime import datetime, timedelta
+from pytz import utc
+import os
+
 
 from flexmeasures.cli.tests.utils import to_flags
 from flexmeasures.data.models.annotations import (
@@ -6,6 +11,9 @@ from flexmeasures.data.models.annotations import (
     AccountAnnotationRelationship,
 )
 from flexmeasures.data.models.data_sources import DataSource
+from flexmeasures.data.models.generic_assets import GenericAsset, GenericAssetType
+from flexmeasures.data.models.time_series import Sensor, TimedBelief
+
 from flexmeasures.cli.tests.utils import get_click_commands
 
 
@@ -87,3 +95,156 @@ def test_cli_help(app):
         result = runner.invoke(cmd, ["--help"])
         assert result.exit_code == 0
         assert "Usage" in result.output
+
+
+@pytest.fixture(scope="module")
+def setup_dummy_data(db, app):
+
+    """
+    Create Sensors 2, 1 Asset and 1 AssetType
+    """
+    dummy_asset_type = GenericAssetType(name="DummyGenericAssetType")
+    report_asset_type = GenericAssetType(name="ReportAssetType")
+
+    db.session.add_all([dummy_asset_type, report_asset_type])
+
+    dummy_asset = GenericAsset(
+        name="DummyGenericAsset", generic_asset_type=dummy_asset_type
+    )
+
+    pandas_report = GenericAsset(
+        name="PandasReport", generic_asset_type=report_asset_type
+    )
+
+    db.session.add_all([dummy_asset, pandas_report])
+
+    sensor1 = Sensor(
+        "sensor 1", generic_asset=dummy_asset, event_resolution=timedelta(hours=1)
+    )
+
+    db.session.add(sensor1)
+    sensor2 = Sensor(
+        "sensor 2", generic_asset=dummy_asset, event_resolution=timedelta(hours=1)
+    )
+    db.session.add(sensor2)
+    report_sensor = Sensor(
+        "report sensor",
+        generic_asset=pandas_report,
+        event_resolution=timedelta(hours=2),
+    )
+    db.session.add(report_sensor)
+
+    """
+        Create 1 DataSources
+    """
+    source = DataSource("source1")
+
+    """
+        Create TimedBeliefs
+    """
+    beliefs = []
+    for sensor in [sensor1, sensor2]:
+        for t in range(20):
+            beliefs.append(
+                TimedBelief(
+                    event_start=datetime(2023, 4, 10, tzinfo=utc) + timedelta(hours=t),
+                    belief_time=datetime(2023, 4, 9, tzinfo=utc),
+                    event_value=t,
+                    sensor=sensor,
+                    source=source,
+                )
+            )
+
+    db.session.add_all(beliefs)
+    db.session.commit()
+
+    yield sensor1, sensor2, report_sensor
+
+    db.session.delete(sensor1)
+    db.session.delete(sensor2)
+
+    for b in beliefs:
+        db.session.delete(b)
+
+    db.session.delete(dummy_asset)
+    db.session.delete(dummy_asset_type)
+
+    db.session.commit()
+
+
+@pytest.fixture(scope="module")
+def reporter_config_raw(app, db, setup_dummy_data):
+    sensor1, sensor2, report_sensor = setup_dummy_data
+
+    reporter_config_raw = dict(
+        start="2023-04-10T00:00:00 00:00",
+        end="2023-04-10T10:00:00 00:00",
+        tb_query_config=[dict(sensor=sensor1.id), dict(sensor=sensor2.id)],
+        transformations=[
+            dict(
+                df_input="sensor_1",
+                df_output="df_agg",
+                method="add",
+                args=["@sensor_2"],
+            ),
+            dict(method="resample_events", args=["2h"]),
+        ],
+        final_df_output="df_agg",
+    )
+
+    return reporter_config_raw
+
+
+def test_add_reporter(app, db, setup_dummy_data, reporter_config_raw):
+    from flexmeasures.cli.data_add import add_report
+
+    sensor1, sensor2, report_sensor = setup_dummy_data
+    report_sensor_id = report_sensor.id
+
+    runner = app.test_cli_runner()
+
+    cli_input_params = {
+        "sensor-id": report_sensor_id,
+        "reporter-config-file": "reporter_config.json",
+        "reporter": "PandasReporter",
+        "start": "2023-04-10T00:00:00 00:00",
+        "end": "2023-04-10T10:00:00 00:00",
+        "timezone": "UTC",
+        "output_file": "test.csv",
+    }
+
+    cli_input = to_flags(cli_input_params)
+
+    flags = ["--save-to-database"]
+    cli_input.extend(flags)
+
+    with runner.isolated_filesystem():
+
+        # save reporter_config to a json file
+        with open("reporter_config.json", "w") as f:
+            json.dump(reporter_config_raw, f)
+
+        # call command
+        result = runner.invoke(add_report, cli_input)
+
+        print(result)
+
+        assert result.exit_code == 0  # run command without errors
+
+        assert "Reporter PandasReporter found" in result.output
+        assert "Report computation done." in result.output
+
+        # Check report is save to the database
+        report_sensor = (
+            db.session.query(Sensor).filter(Sensor.id == report_sensor_id).one_or_none()
+        )  # get fresh report sensor instance
+        stored_report = report_sensor.search_beliefs(
+            event_starts_after=cli_input_params.get("start").replace(" ", "+"),
+            event_ends_before=cli_input_params.get("end").replace(" ", "+"),
+        )
+        assert len(stored_report) == 5
+
+        assert os.path.exists("test.csv")  # check that the file has been created
+        assert (
+            os.path.getsize("test.csv") > 100
+        )  # bytes. Check that the file is not empty
--- flexmeasures/cli/utils.py
+++ flexmeasures/cli/utils.py
@@ -1,3 +1,7 @@
+from typing import Tuple, Mapping, Optional
+from datetime import datetime, timedelta
+
+
 import click
 from click_default_group import DefaultGroup
 
@@ -10,9 +14,9 @@ class MsgStyle(object):
 
     """
 
-    SUCCESS = {"fg": "green"}
-    WARN = {"fg": "yellow"}
-    ERROR = {"fg": "red"}
+    SUCCESS: Mapping = {"fg": "green"}
+    WARN: Mapping = {"fg": "yellow"}
+    ERROR: Mapping = {"fg": "red"}
 
 
 class DeprecatedDefaultGroup(DefaultGroup):
@@ -59,3 +63,44 @@ class DeprecatedDefaultGroup(DefaultGroup):
             click.echo(click.style(self.deprecation_message, fg="red"), err=True)
             ctx.invoked_default = self.default_cmd_name
         return super().get_command(ctx, cmd_name)
+
+
+def get_timerange_from_flags(
+    start: Optional[datetime], end: Optional[datetime], timezone, **kwargs
+) -> Tuple[Optional[datetime], Optional[datetime]]:
+
+    flags = ["last_hour", "last_day", "last_week", "last_month", "last_year"]
+
+    # if any of the flag in `flags` is passed in kwargs
+    if len([kwarg for kwarg in kwargs if kwarg in flags]) > 0:
+        end = datetime.now(tz=timezone).replace(microsecond=0, second=0, minute=0)
+    else:
+        return start, end
+
+    """ Handle different flags """
+    if kwargs.get("last_hour"):  # last finished hour
+        end = end
+        start = end - timedelta(hours=1)
+
+    if kwargs.get("last_day"):  # yesterday
+        end = end.replace(hour=0)
+        start = end - timedelta(days=1)
+
+    if kwargs.get("last_week"):  # last finished 7 week period.
+        end = end.replace(hour=0)
+        start = end - timedelta(days=7)
+
+    if kwargs.get("last_month"):
+        end = end.replace(hour=0) - timedelta(
+            days=end.day
+        )  # to get the last day of the previous month
+        start = end - timedelta(
+            days=end.day - 1
+        )  # equivalent to start.day = end.day-end.day +1
+
+    if kwargs.get("last_year"):
+        end = end.replace(hour=0) - timedelta(
+            days=end.day
+        )  # to get the last day of the previous month
+
+    return start, end
