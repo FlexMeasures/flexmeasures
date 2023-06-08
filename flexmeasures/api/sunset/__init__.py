e907309860241d7132cf2bda745fe8820acca27c
F.N. Claessen <felix@seita.nl>
2023-06-08 14:23:04 +0200

    refactor: use loop to deprecate and sunset blueprints
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/sunset/__init__.py
+++ flexmeasures/api/sunset/__init__.py
@@ -6,84 +6,55 @@ from flexmeasures.api.common.utils.deprecation_utils import (
 )
 
 
-# The api blueprint. It is registered with the Flask app (see register_at)
+# The sunset API blueprints. They are registered with the Flask app (see register_at)
 flexmeasures_api_v1 = Blueprint("flexmeasures_api_v1", __name__)
-deprecate_blueprint(
-    flexmeasures_api_v1,
-    deprecation_date="2022-12-14",
-    deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
-    sunset_date="2023-05-01",
-    sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1.html",
-)
-sunset_blueprint(
-    flexmeasures_api_v1,
-    "1.0",
-    "https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1.html",
-    rollback_possible=False,
-)
-
-# The api blueprint. It is registered with the Flask app (see app.py)
 flexmeasures_api_v1_1 = Blueprint("flexmeasures_api_v1_1", __name__)
-deprecate_blueprint(
-    flexmeasures_api_v1_1,
-    deprecation_date="2022-12-14",
-    deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/v1_1.html",
-    sunset_date="2023-05-01",
-    sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_1.html",
-)
-sunset_blueprint(
-    flexmeasures_api_v1_1,
-    "1.1",
-    "https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_1.html",
-    rollback_possible=False,
-)
-
-# The api blueprint. It is registered with the Flask app (see app.py)
 flexmeasures_api_v1_2 = Blueprint("flexmeasures_api_v1_2", __name__)
-deprecate_blueprint(
-    flexmeasures_api_v1_2,
-    deprecation_date="2022-12-14",
-    deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/v1_2.html",
-    sunset_date="2023-05-01",
-    sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_2.html",
-)
-sunset_blueprint(
-    flexmeasures_api_v1_2,
-    "1.2",
-    "https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_2.html",
-    rollback_possible=False,
-)
-
-# The api blueprint. It is registered with the Flask app (see app.py)
 flexmeasures_api_v1_3 = Blueprint("flexmeasures_api_v1_3", __name__)
-deprecate_blueprint(
-    flexmeasures_api_v1_3,
-    deprecation_date="2022-12-14",
-    deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/v1_3.html",
-    sunset_date="2023-05-01",
-    sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_3.html",
-)
-sunset_blueprint(
-    flexmeasures_api_v1_3,
-    "1.3",
-    "https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_3.html",
-    rollback_possible=False,
-)
-
 flexmeasures_api_v2_0 = Blueprint("flexmeasures_api_v2_0", __name__)
-deprecate_blueprint(
-    flexmeasures_api_v2_0,
-    deprecation_date="2022-12-14",
-    deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/v2_0.html",
-    sunset_date="2023-05-01",
-    sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v2_0.html",
-)
-sunset_blueprint(
-    flexmeasures_api_v2_0,
-    "2.0",
-    "https://flexmeasures.readthedocs.io/en/v0.13.0/api/v2_0.html",
-    rollback_possible=False,
-)
+
+SUNSET_INFO = [
+    dict(
+        blueprint=flexmeasures_api_v1,
+        api_version_sunset="1.0",
+        sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1.html",
+    ),
+    dict(
+        blueprint=flexmeasures_api_v1_1,
+        api_version_sunset="1.1",
+        sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_1.html",
+    ),
+    dict(
+        blueprint=flexmeasures_api_v1_2,
+        api_version_sunset="1.2",
+        sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_2.html",
+    ),
+    dict(
+        blueprint=flexmeasures_api_v1_3,
+        api_version_sunset="1.3",
+        sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_3.html",
+    ),
+    dict(
+        blueprint=flexmeasures_api_v2_0,
+        api_version_sunset="2.0",
+        sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v2_0.html",
+    ),
+]
+
+for info in SUNSET_INFO:
+    deprecate_blueprint(
+        blueprint=info["blueprint"],
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
+        sunset_link=info["sunset_link"],
+    )
+    sunset_blueprint(
+        blueprint=info["blueprint"],
+        api_version_sunset=info["api_version_sunset"],
+        sunset_link=info["sunset_link"],
+        rollback_possible=False,
+    )
 
 
 def register_at(app: Flask):
