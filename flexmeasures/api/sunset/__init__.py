aec94b31993953310bfea047defef35da3e60130
F.N. Claessen <felix@seita.nl>
2023-06-12 22:48:11 +0200

    Move dates into SUNSET_INFO
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/sunset/__init__.py
+++ flexmeasures/api/sunset/__init__.py
@@ -16,39 +16,48 @@ flexmeasures_api_v2_0 = Blueprint("flexmeasures_api_v2_0", __name__)
 SUNSET_INFO = [
     dict(
         blueprint=flexmeasures_api_v1,
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
         api_version_sunset="1.0",
         sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1.html",
     ),
     dict(
         blueprint=flexmeasures_api_v1_1,
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
         api_version_sunset="1.1",
         sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_1.html",
     ),
     dict(
         blueprint=flexmeasures_api_v1_2,
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
         api_version_sunset="1.2",
         sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_2.html",
     ),
     dict(
         blueprint=flexmeasures_api_v1_3,
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
         api_version_sunset="1.3",
         sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v1_3.html",
     ),
     dict(
         blueprint=flexmeasures_api_v2_0,
+        deprecation_date="2022-12-14",
+        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
+        sunset_date="2023-05-01",
         api_version_sunset="2.0",
         sunset_link="https://flexmeasures.readthedocs.io/en/v0.13.0/api/v2_0.html",
     ),
 ]
 
 for info in SUNSET_INFO:
-    deprecate_blueprint(
-        blueprint=info["blueprint"],
-        deprecation_date="2022-12-14",
-        deprecation_link="https://flexmeasures.readthedocs.io/en/latest/api/introduction.html#deprecation-and-sunset",
-        sunset_date="2023-05-01",
-        sunset_link=info["sunset_link"],
-    )
+    deprecate_blueprint(**info)
     sunset_blueprint(
         blueprint=info["blueprint"],
         api_version_sunset=info["api_version_sunset"],
