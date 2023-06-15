64b9632520ba3b4472dd71203ab57cbafe176f72
F.N. Claessen <felix@seita.nl>
2023-06-15 14:01:11 +0200

    The `and` statement was not working as intended. Also, using `first` is more robust against duplicate asset names already being present in the database.
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/generic_assets.py
+++ flexmeasures/data/schemas/generic_assets.py
@@ -50,9 +50,9 @@ class GenericAssetSchema(ma.SQLAlchemySchema):
     def validate_name_is_unique_in_account(self, data, **kwargs):
         if "name" in data and "account_id" in data:
             asset = GenericAsset.query.filter(
-                GenericAsset.name == data["name"]
-                and GenericAsset.account_id == data["account_id"]
-            ).one_or_none()
+                GenericAsset.name == data["name"],
+                GenericAsset.account_id == data["account_id"],
+            ).first()
             if asset:
                 raise ValidationError(
                     f"An asset with the name {data['name']} already exists in this account.",
