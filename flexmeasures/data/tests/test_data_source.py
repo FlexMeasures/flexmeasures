632534cfeff8034e67912376a5a6238b935c7915
Victor Garcia Reolid <victor@seita.nl>
2023-06-27 17:06:36 +0200

    fix: use a DataGenerator with defined schemas
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/data/tests/test_data_source.py
+++ flexmeasures/data/tests/test_data_source.py
@@ -1,14 +1,14 @@
 import pytest
 
 from flexmeasures.data.models.reporting import Reporter
-from flexmeasures.data.models.data_sources import DataGenerator
 
 from datetime import datetime
 from pytz import UTC
 
 
-def test_get_reporter_from_source(db, app, aggregator_reporter_data_source):
-
+def test_get_reporter_from_source(
+    db, app, aggregator_reporter_data_source, add_nearby_weather_sensors
+):
     reporter = aggregator_reporter_data_source.data_generator
 
     assert isinstance(reporter, Reporter)
@@ -26,23 +26,27 @@ def test_get_reporter_from_source(db, app, aggregator_reporter_data_source):
         reporter.compute(start=datetime(2023, 1, 1, tzinfo=UTC), end="not a date")
 
 
-def test_data_source(db, app):
-    class TestDataGenerator(DataGenerator):
-        pass
+def test_creation_of_new_data_source(
+    db, app, aggregator_reporter_data_source, add_nearby_weather_sensors
+):
+    sensor1 = add_nearby_weather_sensors.get("temperature")
+    sensor2 = add_nearby_weather_sensors.get("farther_temperature")
 
-    ds1 = TestDataGenerator(config={"a": "b"})
+    TestReporter = app.data_generators["TestReporter"]
+
+    ds1 = TestReporter(config={"sensor": sensor1.id})
 
     db.session.add(ds1.data_source)
     db.session.commit()
 
-    ds2 = TestDataGenerator(config={"a": "b"})
+    ds2 = TestReporter(sensor=sensor1)
 
     assert ds1.data_source == ds2.data_source
     assert ds1.data_source.attributes.get("config") == ds2.data_source.attributes.get(
         "config"
     )
 
-    ds3 = TestDataGenerator(config={"a": "c"})
+    ds3 = TestReporter(config={"sensor": sensor2.id})
 
     assert ds3.data_source != ds2.data_source
     assert ds3.data_source.attributes.get("config") != ds2.data_source.attributes.get(
