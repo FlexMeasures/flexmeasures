132cf4636d34482f9f8a5032bf10b3fb82077f5c
Victor Garcia Reolid <victor@seita.nl>
2023-07-10 16:27:37 +0200

    test: change output sensor id
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/data/schemas/tests/test_reporting.py
+++ flexmeasures/data/schemas/tests/test_reporting.py
@@ -108,7 +108,7 @@ def test_pandas_reporter_config_schema(config, is_valid, db, app, setup_dummy_se
     [
         (
             {
-                "sensor": 1,
+                "sensor": 2,
                 "input_sensors": {"sensor_1": {"sensor": 1}},
                 "start": "2023-06-06T00:00:00+02:00",
                 "end": "2023-06-06T00:00:00+02:00",
@@ -123,7 +123,7 @@ def test_pandas_reporter_config_schema(config, is_valid, db, app, setup_dummy_se
         ),
         (
             {
-                "sensor": 1,
+                "sensor": 2,
                 "input_sensors": {
                     "sensor_1": {
                         "sensor": 1,
