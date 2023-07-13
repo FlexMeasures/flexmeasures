d38cb688708066e5b8dfe607f18abbab568de5b9
F.N. Claessen <felix@seita.nl>
2023-07-13 13:14:38 +0200

    fix: broken import after merge
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/__init__.py
+++ flexmeasures/data/schemas/__init__.py
@@ -2,7 +2,6 @@
 Data schemas (Marshmallow)
 """
 
-from .assets import LatitudeField, LongitudeField  # noqa F401
 from .generic_assets import GenericAssetIdField as AssetIdField  # noqa F401
 from .locations import LatitudeField, LongitudeField  # noqa F401
 from .sensors import SensorIdField  # noqa F401
