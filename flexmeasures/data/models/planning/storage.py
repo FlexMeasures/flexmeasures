dc44545f58aa0e73d99890dc5e7d1487bb553c88
Victor Garcia Reolid <victor@seita.nl>
2023-08-01 14:22:58 +0200

    increment StorageScheduler version
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -26,7 +26,7 @@ from flexmeasures.utils.coding_utils import deprecated
 
 class StorageScheduler(Scheduler):
 
-    __version__ = "1"
+    __version__ = "2"
     __author__ = "Seita"
 
     COLUMNS = [
