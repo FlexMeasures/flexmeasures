b48145728a887de089be9f20ea478eb9a54db36f
Victor Garcia Reolid <victor@seita.nl>
2023-08-01 18:03:37 +0200

    fix fixture
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/cli/tests/conftest.py
+++ flexmeasures/cli/tests/conftest.py
@@ -10,26 +10,42 @@ from flexmeasures.data.models.time_series import Sensor, TimedBelief
 
 @pytest.fixture(scope="module")
 @pytest.mark.skip_github
-def setup_dummy_data(db, app):
+def setup_dummy_asset(db, app):
+    """
+    Create an Asset to add sensors to and return the id.
+    """
+    dummy_asset_type = GenericAssetType(name="DummyGenericAssetType")
+
+    db.session.add(dummy_asset_type)
+
+    dummy_asset = GenericAsset(
+        name="DummyGenericAsset", generic_asset_type=dummy_asset_type
+    )
+    db.session.add(dummy_asset)
+    db.session.commit()
+
+    return dummy_asset.id
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.skip_github
+def setup_dummy_data(db, app, setup_dummy_asset):
     """
     Create an asset with two sensors (1 and 2), and add the same set of 200 beliefs with an hourly resolution to each of them.
     Return the two sensors and a result sensor (which has no data).
     """
 
-    dummy_asset_type = GenericAssetType(name="DummyGenericAssetType")
     report_asset_type = GenericAssetType(name="ReportAssetType")
 
-    db.session.add_all([dummy_asset_type, report_asset_type])
-
-    dummy_asset = GenericAsset(
-        name="DummyGenericAsset", generic_asset_type=dummy_asset_type
-    )
+    db.session.add(report_asset_type)
 
     pandas_report = GenericAsset(
         name="PandasReport", generic_asset_type=report_asset_type
     )
 
-    db.session.add_all([dummy_asset, pandas_report])
+    db.session.add(pandas_report)
+
+    dummy_asset = GenericAsset.query.get(setup_dummy_asset)
 
     sensor1 = Sensor(
         "sensor 1", generic_asset=dummy_asset, event_resolution=timedelta(hours=1)
