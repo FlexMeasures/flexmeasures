a03a27cbab4664f0f447f6ced054a400660ed7e6
Nicolas Höning <nicolas@seita.nl>
2023-10-18 21:58:15 +0200

    add parameters for sensors, start and duration - turn into a click command, as well
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- ci/benchmark.py
+++ ci/benchmark.py
@@ -1,24 +1,62 @@
 #!/usr/bin/env python3
-# This file is a script to run the benchmarking tests for the project.
+"""
+This file is a script to run bench-marking tests for the FlexMeasures project.
+It can be useful to compare two states, and see if performance has improved or got worse.
+
+Call like this:
+
+./ci/benchmark.py --iterations 5  --sensor 6 --sensor 7 --start 2023-01-01T05:00:00.000Z --duration P1M
+"""
 
 import subprocess
 import time
+import isodate
 import numpy as np
 import tqdm
+import click
+
+from flexmeasures.data.schemas import DurationField, AwareDateTimeField
 
 
-def main():
+@click.command()
+@click.option("--iterations", type=int, default=10)
+@click.option("--sensor", "sensors", type=int, multiple=True, required=True)
+@click.option(
+    "--start",
+    "start",
+    type=AwareDateTimeField(format="iso"),
+    required=True,
+    help="Start queries at this datetime. Follow up with a timezone-aware datetime in ISO 6801 format.",
+)
+@click.option(
+    "--duration",
+    "duration",
+    type=DurationField(),
+    default="P30D",
+    help="Duration of data to be queried, after --start. Follow up with a duration in ISO 6801 format, e.g. PT1H (1 hour) or PT45M (45 minutes).",
+)
+def benchmark(iterations, sensors, start, duration):
+    """
+    This command queries belief data a couple times and tells you how long that took on average (and the STD).
+    """
+    start_str = str(start).replace(" ", "T")
+    start_str = isodate.datetime_isoformat(start)
+    duration_str = isodate.strftime(duration, "P%P")
+    sensor_ids = " ".join([f"--sensor-id {sensor_id}" for sensor_id in sensors])
     commands = [
-        "flexmeasures show beliefs --sensor-id 6 --sensor-id 16 --sensor-id 15 --sensor-id 8 --sensor-id 5 --sensor-id 4 --start 2023-01-01T05:00:00.000Z --duration P0Y0M1D",
-        "flexmeasures show beliefs --sensor-id 6 --sensor-id 16 --sensor-id 15 --sensor-id 8 --sensor-id 5 --sensor-id 4 --start 2023-01-01T05:00:00.000Z --duration P0Y0M30D",
-        "flexmeasures show beliefs --sensor-id 6 --sensor-id 16 --sensor-id 15 --sensor-id 8 --sensor-id 5 --sensor-id 4 --start 2023-01-01T05:00:00.000Z --duration P0Y0M60D",
+        f"flexmeasures show beliefs {sensor_ids} --start {start_str} --duration {duration_str}",
     ]
 
+    print("I'll be running:")
+    print("\n".join(commands))
+    print()
+    print(f"I'll repeat this {iterations} times.")
+    print()
+
     timings = {}
-    max_iterations = 10
 
     for idx, command in enumerate(commands):
-        for _ in tqdm.tqdm(range(max_iterations)):
+        for _ in tqdm.tqdm(range(iterations)):
             start = time.perf_counter()
             run_command(command)
             stop = time.perf_counter()
@@ -28,7 +66,7 @@ def main():
                 timings[idx] = [start - stop]
 
     # Get mean and standard deviation of timings
-    print(f"All timings are in seconds and iterated {max_iterations} times")
+    print(f"All timings are in seconds and iterated {iterations} times")
     for idx, timing in timings.items():
         print(f"Command {commands[idx]}: {np.mean(timing)} ± {np.std(timing)}")
 
@@ -43,4 +81,4 @@ def run_command(command):
 
 
 if __name__ == "__main__":
-    main()
+    benchmark()
