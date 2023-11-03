00fb43b45b4939a234c318cc347cb4c16279ba33
GustaafL <guus@seita.nl>
2023-11-03 14:27:48 +0100

    feat(scripts): add customer-manager to default user roles
    Signed-off-by: GustaafL <guus@seita.nl>

--- flexmeasures/data/scripts/data_gen.py
+++ flexmeasures/data/scripts/data_gen.py
@@ -86,6 +86,7 @@ def add_default_user_roles(db: SQLAlchemy):
     for role_name, role_description in (
         ("admin", "Super user"),
         ("admin-reader", "Can read everything"),
+        ("customer-manager", "Consultant that manages another customer"),
     ):
         role = Role.query.filter(Role.name == role_name).one_or_none()
         if role:
