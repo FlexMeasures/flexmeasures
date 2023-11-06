da922f93ad8ee585bacf90209b55fac55f7b1a27
F.N. Claessen <felix@seita.nl>
2023-11-06 14:59:31 +0100

    refactor: use flake8 per-file-ignore option for import statements
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/__init__.py
+++ flexmeasures/data/schemas/__init__.py
@@ -2,9 +2,9 @@
 Data schemas (Marshmallow)
 """
 
-from .account import AccountIdField  # noqa F401
-from .generic_assets import GenericAssetIdField as AssetIdField  # noqa F401
-from .locations import LatitudeField, LongitudeField  # noqa F401
-from .sensors import SensorIdField  # noqa F401
-from .sources import DataSourceIdField as SourceIdField  # noqa F401
-from .times import AwareDateTimeField, DurationField, TimeIntervalField  # noqa F401
+from .account import AccountIdField
+from .generic_assets import GenericAssetIdField as AssetIdField
+from .locations import LatitudeField, LongitudeField
+from .sensors import SensorIdField
+from .sources import DataSourceIdField as SourceIdField
+from .times import AwareDateTimeField, DurationField, TimeIntervalField
