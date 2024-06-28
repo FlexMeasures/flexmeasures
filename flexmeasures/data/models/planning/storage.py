145c5f62a5717d807026b4f3afe692a0873431ed
F.N. Claessen <felix@seita.nl>
2024-06-28 15:03:02 +0200

    style: remove redundant parentheses
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -188,8 +188,8 @@ class MetaStorageScheduler(Scheduler):
                 sensor=curtailable_sensor,
             )
             # Assume both consumption and production of device can be fully curtailed
-            device_constraints[i + 1]["derivative min"] = (power_values).clip(None, 0)
-            device_constraints[i + 1]["derivative max"] = (power_values).clip(0, None)
+            device_constraints[i + 1]["derivative min"] = power_values.clip(None, 0)
+            device_constraints[i + 1]["derivative max"] = power_values.clip(0, None)
 
         # Inflexible devices
         for i, inflexible_sensor in enumerate(inflexible_device_sensors):
