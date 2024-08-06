2202077dfc53eaa869b8da744a4965c7a2686918
F.N. Claessen <felix@seita.nl>
2024-08-06 09:57:09 +0200

    feat: minimum list length for soc-gain and soc-usage
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/scheduling/storage.py
+++ flexmeasures/data/schemas/scheduling/storage.py
@@ -139,10 +139,16 @@ class StorageFlexModelSchema(Schema):
     prefer_charging_sooner = fields.Bool(data_key="prefer-charging-sooner")
 
     soc_gain = fields.List(
-        TimeSeriesOrQuantityOrSensor("MW"), data_key="soc-gain", required=False
+        TimeSeriesOrQuantityOrSensor("MW"),
+        data_key="soc-gain",
+        required=False,
+        validate=validate.Length(min=1),
     )
     soc_usage = fields.List(
-        TimeSeriesOrQuantityOrSensor("MW"), data_key="soc-usage", required=False
+        TimeSeriesOrQuantityOrSensor("MW"),
+        data_key="soc-usage",
+        required=False,
+        validate=validate.Length(min=1),
     )
 
     def __init__(
