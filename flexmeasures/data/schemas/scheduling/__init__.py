68e50154bae546a0b8250d88d1e638a144178bf5
F.N. Claessen <felix@seita.nl>
2024-08-13 14:46:15 +0200

    feature: add new flex-context fields to schema
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/schemas/scheduling/__init__.py
+++ flexmeasures/data/schemas/scheduling/__init__.py
@@ -8,12 +8,17 @@ class FlexContextSchema(Schema):
     This schema lists fields that can be used to describe sensors in the optimised portfolio
     """
 
+    # Energy commitments
     ems_power_capacity_in_mw = QuantityOrSensor(
         "MW",
         required=False,
         data_key="site-power-capacity",
         validate=validate.Range(min=0),
     )
+    consumption_price_sensor = SensorIdField(data_key="consumption-price-sensor")
+    production_price_sensor = SensorIdField(data_key="production-price-sensor")
+
+    # Capacity commitments
     ems_production_capacity_in_mw = QuantityOrSensor(
         "MW",
         required=False,
@@ -26,8 +31,34 @@ class FlexContextSchema(Schema):
         data_key="site-consumption-capacity",
         validate=validate.Range(min=0),
     )
-    consumption_price_sensor = SensorIdField(data_key="consumption-price-sensor")
-    production_price_sensor = SensorIdField(data_key="production-price-sensor")
+    ems_site_consumption_breach_price = fields.Float(
+        data_key="site-consumption-breach-price", required=False, default=None
+    )  # in EUR/MW
+    ems_site_production_breach_price = fields.Float(
+        data_key="site-production-breach-price", required=False, default=None
+    )  # in EUR/MW
+
+    # Peak commitments
+    ems_peak_production_in_mw = QuantityOrSensor(
+        "MW",
+        required=False,
+        data_key="site-peak-production",
+        validate=validate.Range(min=0),
+    )
+    ems_peak_consumption_in_mw = QuantityOrSensor(
+        "MW",
+        required=False,
+        data_key="site-peak-consumption",
+        validate=validate.Range(min=0),
+    )
+    ems_peak_consumption_price = fields.Float(
+        data_key="site-peak-consumption-price", required=False, default=None
+    )  # in EUR/MW
+    ems_peak_production_price = fields.Float(
+        data_key="site-peak-production-price", required=False, default=None
+    )  # in EUR/MW
+    # todo: group by month start (MS), something like a commitment resolution, or a list of datetimes representing splits of the commitments
+
     inflexible_device_sensors = fields.List(
         SensorIdField(), data_key="inflexible-device-sensors"
     )
