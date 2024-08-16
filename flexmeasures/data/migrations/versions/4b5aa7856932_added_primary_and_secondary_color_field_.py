c54e081b0bfa89a7e199cbd5b9384b7f00080497
joshuaunity <oghenerobojosh01@gmail.com>
2024-08-16 12:08:50 +0100

    fix: resolving migration issues
    - issue is being caused because of the existence of TWO HEADS
    
    Foxed by manually editing the down_revisions of the new migration file

--- flexmeasures/data/migrations/versions/4b5aa7856932_added_primary_and_secondary_color_field_.py
+++ flexmeasures/data/migrations/versions/4b5aa7856932_added_primary_and_secondary_color_field_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "4b5aa7856932"
-down_revision = "126d65cbe6b4"
+down_revision = "3eb0564948ca"
 branch_labels = None
 depends_on = None
 
