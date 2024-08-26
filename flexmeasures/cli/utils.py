de35b10e1cc4d9883e188b769437c2d9af1d3173
Victor <victor@seita.nl>
2024-08-26 20:12:50 +0200

    fix: add default inside the function (#1158)
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/cli/utils.py
+++ flexmeasures/cli/utils.py
@@ -155,7 +155,7 @@ def get_timerange_from_flag(
     last_7_days: bool = False,
     last_month: bool = False,
     last_year: bool = False,
-    timezone: pytz.BaseTzInfo = get_timezone(),
+    timezone: pytz.BaseTzInfo | None = None,
 ) -> tuple[datetime, datetime]:
     """This function returns a time range [start,end] of the last-X period.
     See input parameters for more details.
@@ -168,6 +168,9 @@ def get_timerange_from_flag(
     :param timezone: timezone object to represent
     :returns: start:datetime, end:datetime
     """
+
+    if timezone is None:
+        timezone = get_timezone()
 
     current_hour = get_most_recent_hour().astimezone(timezone)
 
