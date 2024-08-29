b6f3bd8d768040a360116847b4734f98878685e5
joshuaunity <oghenerobojosh01@gmail.com>
2024-08-29 11:00:33 +0100

    chore: more concise code
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/api/v3_0/accounts.py
+++ flexmeasures/api/v3_0/accounts.py
@@ -200,14 +200,13 @@ class AccountAPI(FlaskView):
             "logo_url",
             "consultancy_account_id",
         ]
-        modified_fields = {
-            field: getattr(account, field)
-            for field in fields_to_check
-            if account_data.get(field) != getattr(account, field)
-        }
 
         # Compile modified fields string
-        modified_fields_str = ", ".join(modified_fields.keys())
+        modified_fields_str = ", ".join(
+            field
+            for field in fields_to_check
+            if account_data.get(field) != getattr(account, field)
+        )
 
         for k, v in account_data.items():
             setattr(account, k, v)
