5e4e8eb5338c8a965b79ddbf41f16481d822bef5
F.N. Claessen <felix@seita.nl>
2024-09-02 16:52:57 +0200

    docs: typo
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/services/time_series.py
+++ flexmeasures/data/services/time_series.py
@@ -69,7 +69,7 @@ def drop_unchanged_beliefs(bdf: tb.BeliefsDataFrame) -> tb.BeliefsDataFrame:
     ex_ante_bdf = bdf[bdf.belief_horizons > timedelta(0)]
     ex_post_bdf = bdf[bdf.belief_horizons <= timedelta(0)]
     if not ex_ante_bdf.empty and not ex_post_bdf.empty:
-        # We treat each part separately to avoid the ex-post knowledge would be lost
+        # We treat each part separately to avoid that ex-post knowledge would be lost
         ex_ante_bdf = drop_unchanged_beliefs(ex_ante_bdf)
         ex_post_bdf = drop_unchanged_beliefs(ex_post_bdf)
         bdf = pd.concat([ex_ante_bdf, ex_post_bdf])
