0f9435ca460938444e5bdd2bbc8f8dc5799812cf
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2024-09-08 16:22:07 +0200

    Asset search filter applies to both asset names and account names (#1164)
    * feature: use filter on account names in addition to asset names
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * feature: allow grouping words as one search key
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: rephrase (terms instead of words)
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * refactor: one less line
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: rephrase changelog entries
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    ---------
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/assets.py
+++ flexmeasures/api/v3_0/assets.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from shlex import split
 import json
 
 from flask import current_app
@@ -13,7 +14,7 @@ from marshmallow import fields
 import marshmallow.validate as validate
 
 from webargs.flaskparser import use_kwargs, use_args
-from sqlalchemy import select, delete, func
+from sqlalchemy import select, delete, func, or_, and_
 
 from flexmeasures.auth.decorators import permission_required_for_context
 from flexmeasures.data import db
@@ -104,6 +105,7 @@ class AssetAPI(FlaskView):
 
             - If the `page` parameter is not provided, all assets are returned, without pagination information. The result will be a list of assets.
             - If a `page` parameter is provided, the response will be paginated, showing a specific number of assets per page as defined by `per_page` (default is 10).
+            - If a search 'filter' such as 'solar "ACME corp"' is provided, the response will filter out assets where each search term is either present in their name or account name.
               The response schema for pagination is inspired by https://datatables.net/manual/server-side#Returned-data
 
 
@@ -160,10 +162,23 @@ class AssetAPI(FlaskView):
             select(func.count(GenericAsset.id)).where(filter_statement)
         )
 
+        select_statement = select(GenericAsset)
         if filter is not None:
-            filter_statement = filter_statement & GenericAsset.name.ilike(f"%{filter}%")
+            # Search terms in the search filter should either come back in the asset name or account name
+            select_statement = select_statement.join(
+                Account, Account.id == GenericAsset.account_id
+            )
+            filter_statement = filter_statement & and_(
+                *(
+                    or_(
+                        GenericAsset.name.ilike(f"%{term}%"),
+                        Account.name.ilike(f"%{term}%"),
+                    )
+                    for term in split(filter)
+                )
+            )
 
-        query = select(GenericAsset).where(filter_statement)
+        query = select_statement.where(filter_statement)
 
         if page is None:
             response = asset_schema.dump(db.session.scalars(query).all(), many=True)
