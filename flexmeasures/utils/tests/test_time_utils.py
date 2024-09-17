c4a75a4a8a60a8c3f1d1c1acda79ec21954f3af2
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2024-09-17 19:59:35 +0200

    Add unit tests for to_utc_timestamp
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/utils/tests/test_time_utils.py
+++ flexmeasures/utils/tests/test_time_utils.py
@@ -13,6 +13,7 @@ from flexmeasures.utils.time_utils import (
     naturalized_datetime_str,
     get_most_recent_clocktime_window,
     apply_offset_chain,
+    to_utc_timestamp,
 )
 
 
@@ -206,3 +207,21 @@ def test_apply_offset_chain(
     output_date: pd.Timestamp | datetime,
 ):
     assert apply_offset_chain(input_date, offset_chain) == output_date
+
+
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        (datetime(2024, 4, 28, 8, 55, 58), 1714294558.0),
+        (pd.Timestamp("2024-04-28 08:55:58", tz="UTC").to_pydatetime(), 1714294558.0),
+        ("Sun, 28 Apr 2024 08:55:58 GMT", 1714294558.0),
+        ("Invalid date string", None),
+        (None, None),
+    ],
+)
+def test_to_utc_timestamp(input_value, expected_output):
+    result = to_utc_timestamp(input_value)
+    if expected_output is None:
+        assert result is None
+    else:
+        assert result == pytest.approx(expected_output)
