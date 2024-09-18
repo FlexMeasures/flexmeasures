7fd91344d3a957ade5c1987169f0b6d190ec1aca
JDev <45713692+joshuaunity@users.noreply.github.com>
2024-09-18 07:50:21 +0100

    fix: set date range for calendar to be persistent on session and UI (#1165)
    * fix: set date range for calendar to be persisten on session and UI
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * refactor: moved datepicker UI(color related)  persitence logic to base.html
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * chore: removed redundant code
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * fix: unpersistent daterange(not remebering its state even though range is in session) in datepicker
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * fix: datepicker UI persitency(WIP)
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * chore: processed event dates before sending to frontend - some deduction and addition
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * refactor: updating test case to new loic behaviour
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * refactor: undo written logic due to aditional code complexity
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    * chore: clean up
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    
    ---------
    
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>
    Signed-off-by: JDev <45713692+joshuaunity@users.noreply.github.com>
    Co-authored-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/ui/utils/view_utils.py
+++ flexmeasures/ui/utils/view_utils.py
@@ -33,11 +33,12 @@ def render_flexmeasures_template(html_filename: str, **variables):
 
     # use event_starts_after and event_ends_before from session if not given
     variables["event_starts_after"] = variables.get(
-        "event_starts_after", session.get("event_starts_after")
+        "event_starts_after"
+    ) or session.get("event_starts_after")
+    variables["event_ends_before"] = variables.get("event_ends_before") or session.get(
+        "event_ends_before"
     )
-    variables["event_ends_before"] = variables.get(
-        "event_ends_before", session.get("event_ends_before")
-    )
+
     variables["chart_type"] = session.get("chart_type", "bar_chart")
 
     variables["page"] = html_filename.split("/")[-1].replace(".html", "")
