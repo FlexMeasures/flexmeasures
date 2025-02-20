cf4feb17dfc9cce6944b23ad29419ea9fc16952e
F.N. Claessen <felix@seita.nl>
2025-02-20 17:38:28 +0100

    fix: ProcessScheduler robust against both old and new consumption price field
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/process.py
+++ flexmeasures/data/models/planning/process.py
@@ -60,8 +60,9 @@ class ProcessScheduler(Scheduler):
         resolution = self.resolution
         belief_time = self.belief_time
         sensor = self.sensor
+
         consumption_price_sensor: Sensor = self.flex_context.get(
-            "consumption_price_sensor"
+            "consumption_price_sensor", self.flex_context.get("consumption_price")
         )
         duration: timedelta = self.flex_model.get("duration")
         power = self.flex_model.get("power")
