485a7df54af48f809225e00a05b0bc094a8d3710
F.N. Claessen <felix@seita.nl>
2025-03-04 18:17:54 +0100

    refactor: more idiomatic syntax
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/linear_optimization.py
+++ flexmeasures/data/models/planning/linear_optimization.py
@@ -385,11 +385,8 @@ def device_scheduler(  # noqa C901
         """Apply conversion efficiencies to conversion from flow to stock change and vice versa,
         and apply storage efficiencies to stock levels from one datetime to the next."""
         if isinstance(initial_stock, list):
-            try:
-                initial_stock_d = initial_stock[d]
-            except IndexError:
-                # No initial stock defined for inflexible devices
-                initial_stock_d = 0
+            # No initial stock defined for inflexible device
+            initial_stock_d = initial_stock[d] if d < len(initial_stock) else 0
         else:
             initial_stock_d = initial_stock
 
