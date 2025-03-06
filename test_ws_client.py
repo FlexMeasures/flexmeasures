902c8cd1ed6e238fde5095e7fc7613f5be4ee7b1
Victor Garcia Reolid <victor@seita.nl>
2025-03-06 12:39:57 +0100

    apply pre-commit
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- test_ws_client.py
+++ test_ws_client.py
@@ -1,15 +1,17 @@
 from simple_websocket import Client, ConnectionClosed
 
+
 def main():
-    ws = Client.connect('ws://0.0.0.0:5000/ping2')
+    ws = Client.connect("ws://0.0.0.0:5000/ping2")
     try:
         while True:
-            data = input('> ')
+            data = input("> ")
             ws.send(data)
             data = ws.receive()
-            print(f'< {data}')
+            print(f"< {data}")
     except (KeyboardInterrupt, EOFError, ConnectionClosed):
         ws.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
