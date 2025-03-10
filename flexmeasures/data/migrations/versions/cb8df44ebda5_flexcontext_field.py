cf645bb29cd9e891b26b25df3e059f0a9cda364c
JDev <45713692+joshuaunity@users.noreply.github.com>
2025-03-10 17:35:53 +0100

    handle unexpected Datatypes in flex-context Db regression (#1354)
    * refactor: refactored downgrade fo flexcontext to handle unexpected datatypes
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * fix: put in correct datatype to check at downgrade
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * chore: handle dumped flexcontext keys dumped from db downrgade of column
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * refactor: a more finetuned conditional statements handling possible edgecases
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * fix: Fixed DBFlexContextSchema prices validation
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * refactor: handling for edgecases
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * refactor: handling wider range energy units
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    
    * add PR to changelog entry
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    ---------
    
    Signed-off-by: Joshua Edward <oghenerobojosh01@gmail.com>
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    Co-authored-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/data/migrations/versions/cb8df44ebda5_flexcontext_field.py
+++ flexmeasures/data/migrations/versions/cb8df44ebda5_flexcontext_field.py
@@ -9,11 +9,21 @@ Create Date: 2024-12-16 18:39:34.168732
 from alembic import op
 import sqlalchemy as sa
 
+from flexmeasures.utils.unit_utils import is_power_unit, ur
+
 # revision identifiers, used by Alembic.
 revision = "cb8df44ebda5"
 down_revision = "2ba59c7c954e"
 branch_labels = None
 depends_on = None
+
+
+def get_price_info(price_data):
+    if isinstance(price_data, str):
+        return price_data, None
+    elif isinstance(price_data, dict):
+        return None, price_data.get("sensor") if price_data else None
+    return None, None
 
 
 def build_flex_context(
@@ -61,6 +71,16 @@ def build_flex_context(
         "inflexible-device-sensors": [s[0] for s in inflexible_device_sensors],
     }
 
+    if flex_context["consumption-price"]["sensor"] is None and market_id is None:
+        if attributes_data.get("consumption_price") is not None:
+            flex_context["consumption-price"] = attributes_data.get("consumption_price")
+            attributes_data.pop("consumption_price", None)
+
+    if flex_context["production-price"]["sensor"] is None:
+        if attributes_data.get("production_price") is not None:
+            flex_context["production-price"] = attributes_data.get("production_price")
+            attributes_data.pop("production_price", None)
+
     capacity_data = {
         "site-power-capacity": capacity_in_mw,
         "site-consumption-capacity": consumption_capacity_in_mw,
@@ -68,7 +88,10 @@ def build_flex_context(
     }
     for key, value in capacity_data.items():
         if value is not None:
-            flex_context[key] = f"{int(value * 1000)} KW"
+            if isinstance(value, (int, float)):
+                flex_context[key] = f"{int(value * 1000)}kW"
+            else:
+                flex_context[key] = value
 
     price_data = {
         "site-peak-consumption-price": ems_peak_consumption_price,
@@ -85,7 +108,9 @@ def build_flex_context(
 
 def upgrade():
     with op.batch_alter_table("generic_asset", schema=None) as batch_op:
-        batch_op.add_column(sa.Column("flex_context", sa.JSON(), nullable=True))
+        batch_op.add_column(
+            sa.Column("flex_context", sa.JSON(), nullable=False, server_default="{}")
+        )
 
     generic_asset_table = sa.Table(
         "generic_asset",
@@ -295,18 +320,19 @@ def downgrade():
 
     for row in results:
         asset_id, flex_context, attributes_data = row
-        consumption_price_sensor_id = (
-            flex_context.get("consumption-price")["sensor"]
-            if flex_context.get("consumption-price")
-            else None
+
+        consumption_price_as_str, consumption_price_sensor_id = get_price_info(
+            flex_context.get("consumption-price")
         )
-        production_price_sensor_id = (
-            flex_context.get("production-price")["sensor"]
-            if flex_context.get("production-price")
-            else None
+        production_price_as_str, production_price_sensor_id = get_price_info(
+            flex_context.get("production-price")
         )
 
-        market_id = consumption_price_sensor_id
+        if consumption_price_sensor_id is not None:
+            market_id = consumption_price_sensor_id
+        else:
+            market_id = None
+
         capacity_in_mw = (
             flex_context.get("site-power-capacity")
             if flex_context.get("site-power-capacity")
@@ -344,17 +370,36 @@ def downgrade():
         )
 
         if capacity_in_mw is not None:
-            capacity_in_mw = float(capacity_in_mw.replace(" KW", "")) / 1000
+            if isinstance(capacity_in_mw, str) and is_power_unit(capacity_in_mw):
+                capacity_in_mw = (
+                    ur.Quantity(capacity_in_mw).to(ur.Quantity("MW")).magnitude
+                )
 
         if consumption_capacity_in_mw is not None:
-            consumption_capacity_in_mw = (
-                float(consumption_capacity_in_mw.replace(" KW", "")) / 1000
-            )
+            if isinstance(consumption_capacity_in_mw, str) and is_power_unit(
+                consumption_capacity_in_mw
+            ):
+                consumption_capacity_in_mw = (
+                    ur.Quantity(consumption_capacity_in_mw)
+                    .to(ur.Quantity("MW"))
+                    .magnitude
+                )
 
         if production_capacity_in_mw is not None:
-            production_capacity_in_mw = (
-                float(production_capacity_in_mw.replace(" KW", "")) / 1000
-            )
+            if isinstance(production_capacity_in_mw, str) and is_power_unit(
+                production_capacity_in_mw
+            ):
+                production_capacity_in_mw = (
+                    ur.Quantity(production_capacity_in_mw)
+                    .to(ur.Quantity("MW"))
+                    .magnitude
+                )
+
+        if consumption_price_as_str is not None:
+            attributes_data["consumption_price"] = consumption_price_as_str
+
+        if production_price_as_str is not None:
+            attributes_data["production_price"] = production_price_as_str
 
         attributes_data["market_id"] = market_id
         attributes_data["capacity_in_mw"] = capacity_in_mw
