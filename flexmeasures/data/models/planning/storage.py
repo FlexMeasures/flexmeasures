735e98c458f372ce6a991aa96bb547ca7db7f51d
F.N. Claessen <felix@seita.nl>
2025-03-12 16:49:55 +0100

    feat: allow passing capacity sensors with negative values, which we'll clip to 0, because capacities are by definition strictly non-negative (this is useful for modelling PV curtailment, where PV output may show small negative values during the night)
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -526,6 +526,7 @@ class MetaStorageScheduler(Scheduler):
                 query_window=(start, end),
                 resolution=resolution,
                 beliefs_before=belief_time,
+                min_value=0,  # capacities are positive by definition
                 resolve_overlaps="min",
             )
 
@@ -543,6 +544,7 @@ class MetaStorageScheduler(Scheduler):
                     beliefs_before=belief_time,
                     fallback_attribute="production_capacity",
                     max_value=power_capacity_in_mw[d],
+                    min_value=0,  # capacities are positive by definition
                     resolve_overlaps="min",
                 )
             if sensor_d.get_attribute("is_strictly_non_negative"):
@@ -557,6 +559,7 @@ class MetaStorageScheduler(Scheduler):
                         resolution=resolution,
                         beliefs_before=belief_time,
                         fallback_attribute="consumption_capacity",
+                        min_value=0,  # capacities are positive by definition
                         max_value=power_capacity_in_mw[d],
                         resolve_overlaps="min",
                     )
