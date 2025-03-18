2917c140eaca0388d8e27b6d4965967290691beb
Muhammad-Moiz626 <muhammadmoiz4843@gmail.com>
2025-03-18 15:18:09 +0500

    moved map to modal

--- flexmeasures/ui/crud/assets/views.py
+++ flexmeasures/ui/crud/assets/views.py
@@ -14,7 +14,6 @@ from flexmeasures.data.services.job_cache import NoRedisConfigured
 from flexmeasures.data.models.generic_assets import (
     GenericAsset,
     get_center_location_of_assets,
-    GenericAssetType,
 )
 from flexmeasures.ui.utils.view_utils import ICON_MAPPING, available_units
 from flexmeasures.data.models.user import Account
@@ -32,13 +31,6 @@ from flexmeasures.data.services.sensors import (
     build_sensor_status_data,
     build_asset_jobs_data,
 )
-
-from flexmeasures.auth.policy import user_has_admin_access
-from flexmeasures.data.queries.generic_assets import get_asset_group_queries
-from flexmeasures.data.services.asset_grouping import (
-    AssetGroup,
-)
-from sqlalchemy import select
 
 """
 Asset crud view.
@@ -351,48 +343,4 @@ class AssetCrudUI(FlaskView):
         return render_flexmeasures_template(
             "crud/asset_audit_log.html",
             asset=asset,
-        )
-
-    @login_required
-    def map(self, **kwargs):
-        """GET from /assets/map"""
-
-        aggregate_type_groups = current_app.config.get(
-            "FLEXMEASURES_ASSET_TYPE_GROUPS", {}
-        )
-        group_by_accounts = request.args.get("group_by_accounts", "0") != "0"
-        asset_type = request.args.get("asset_type")
-
-        if user_has_admin_access(current_user, "read") and group_by_accounts:
-            asset_groups = get_asset_group_queries(
-                group_by_type=False, group_by_account=True
-            )
-        else:
-            asset_groups = get_asset_group_queries(
-                group_by_type=True, custom_aggregate_type_groups=aggregate_type_groups
-            )
-
-        map_asset_groups = {}
-        for asset_group_name, asset_group_query in asset_groups.items():
-            asset_group = AssetGroup(asset_group_name, asset_query=asset_group_query)
-            if asset_type:
-                asset_group.assets = [
-                    a
-                    for a in asset_group.assets
-                    if a.generic_asset_type.name == asset_type
-                ]
-            if any([a.location for a in asset_group.assets]):
-                map_asset_groups[asset_group_name] = asset_group
-
-        known_asset_types = [
-            gat.name for gat in db.session.scalars(select(GenericAssetType)).all()
-        ]
-
-        return render_flexmeasures_template(
-            "crud/context_map.html",
-            mapboxAccessToken=current_app.config.get("MAPBOX_ACCESS_TOKEN", ""),
-            map_center=get_center_location_of_assets(user=current_user),
-            known_asset_types=known_asset_types,
-            asset_groups=map_asset_groups,
-            aggregate_type_groups=aggregate_type_groups,
         )
