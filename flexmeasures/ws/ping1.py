b54c530e745fe195cb9ad761057b9eb16985c406
Victor Garcia Reolid <victor@seita.nl>
2025-03-18 13:32:35 +0100

    poc: test if we can get the app context
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- flexmeasures/ws/ping1.py
+++ flexmeasures/ws/ping1.py
@@ -1,14 +1,19 @@
 import logging
 from flexmeasures.ws import sock
+from flask import current_app
+from flexmeasures import Sensor
+from sqlalchemy import select, func
 
 logger = logging.Logger(__name__)
 
 
 @sock.route("/ping1")
 def echo1(ws):
+
     while True:
         data = ws.receive()
         logger.error("ping1>" + data)
         if data == "close":
             break
-        ws.send(data)
+        sensors = current_app.db.session.execute(select(func.count(Sensor.id))).scalar()
+        ws.send(str(sensors))
