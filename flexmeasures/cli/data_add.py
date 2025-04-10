b7f8a2b393a0e70079f3155957da8102eb0e3af8
joshuaunity <oghenerobojosh01@gmail.com>
2025-04-10 16:29:39 +0100

    refactor: refactored tutorial toy data
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/cli/data_add.py
+++ flexmeasures/cli/data_add.py
@@ -1398,7 +1398,7 @@ def add_schedule_for_storage(  # noqa C901
             **MsgStyle.ERROR,
         )
         raise click.Abort()
-    if production_price_sensor is None:
+    if production_price_sensor is None and consumption_price_sensor is not None:
         production_price_sensor = consumption_price_sensor
     end = start + duration
 
@@ -1446,11 +1446,20 @@ def add_schedule_for_storage(  # noqa C901
             "roundtrip-efficiency": roundtrip_efficiency,
         },
         flex_context={
-            "consumption-price-sensor": consumption_price_sensor.id,
-            "production-price-sensor": production_price_sensor.id,
+            "consumption-price-sensor": (
+                consumption_price_sensor.id if consumption_price_sensor else None
+            ),
+            "production-price-sensor": (
+                production_price_sensor.id if production_price_sensor else None
+            ),
             "inflexible-device-sensors": [s.id for s in inflexible_device_sensors],
         },
     )
+
+    # remove None value from flex_context
+    scheduling_kwargs["flex_context"] = {
+        k: v for k, v in scheduling_kwargs["flex_context"].items() if v is not None
+    }
 
     if state_of_charge is not None:
         scheduling_kwargs["flex_model"]["state-of-charge"] = {
@@ -2125,12 +2134,13 @@ def add_toy_account(kind: str, name: str):
             "battery",
             "discharging",
             parent_asset_id=building_asset.id,
-            flex_context={"consumption-price-sensor": {"sensor": day_ahead_sensor.id}},
-            attributes={
+            flex_context={"consumption-price": {"sensor": day_ahead_sensor.id}},
+            unit="MW",
+            **{
                 "site-power-capacity": "500 kVA",
+                "min_soc_in_mwh": 0.05,
+                "max_soc_in_mwh": 0.45,
             },
-            min_soc_in_mwh=0.05,
-            max_soc_in_mwh=0.45,
         )
 
         # create solar
