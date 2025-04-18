5e6130eabd0141467ffe4ce9e5135ea2d8716f04
joshuaunity <oghenerobojosh01@gmail.com>
2025-04-18 10:36:51 +0100

    refactor: skip field if it doesnt exist
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/data/migrations/versions/f0ee99278f6f_added_flexmodel_to_generic_asset_model.py
+++ flexmeasures/data/migrations/versions/f0ee99278f6f_added_flexmodel_to_generic_asset_model.py
@@ -69,30 +69,31 @@ def upgrade():
             )
 
         sensor = sensors_with_key[0]
-        # check if value is a int or float
-        if not isinstance(sensor.attributes["soc-min"], (int, float)):
-            raise Exception(
-                f"Invalid value for 'soc-min' in sensor {sensor.id}: {sensor.attributes['soc-min']}"
+        if sensor.attributes["soc-min"] is not None:
+            # check if value is a int or float
+            if not isinstance(sensor.attributes["soc-min"], (int, float)):
+                raise Exception(
+                    f"Invalid value for 'soc-min' in sensor {sensor.id}: {sensor.attributes['soc-min']}"
+                )
+            soc_min_in_kwh = f"{sensor.attributes["soc-min"] * 1000} kWh"
+            flex_model_data = {"soc-min": soc_min_in_kwh}
+
+            stmt = (
+                generic_asset_table.update()
+                .where(generic_asset_table.c.id == asset_id)
+                .values(flex_model=flex_model_data)
             )
-        soc_min_in_kwh = f"{sensor.attributes["soc-min"] * 1000} kWh"
-        flex_model_data = {"soc-min": soc_min_in_kwh}
 
-        stmt = (
-            generic_asset_table.update()
-            .where(generic_asset_table.c.id == asset_id)
-            .values(flex_model=flex_model_data)
-        )
+            conn.execute(stmt)
 
-        conn.execute(stmt)
-
-        # Update the sensor attributes to remove 'soc-min'
-        sensor.attributes.pop("soc-min", None)
-        stmt = (
-            sensor_table.update()
-            .where(sensor_table.c.id == sensor.id)
-            .values(attributes=sensor.attributes)
-        )
-        conn.execute(stmt)
+            # Update the sensor attributes to remove 'soc-min'
+            sensor.attributes.pop("soc-min", None)
+            stmt = (
+                sensor_table.update()
+                .where(sensor_table.c.id == sensor.id)
+                .values(attributes=sensor.attributes)
+            )
+            conn.execute(stmt)
 
     # ### end Alembic commands ###
 
