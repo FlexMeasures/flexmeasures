107d7861af95506196b03ef970889acd2b581fa8
F.N. Claessen <felix@seita.nl>
2025-04-25 20:52:38 +0200

    fix: add_schedule_for_storage stops using deprecated fields
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/cli/data_add.py
+++ flexmeasures/cli/data_add.py
@@ -1447,11 +1447,15 @@ def add_schedule_for_storage(  # noqa C901
             "roundtrip-efficiency": roundtrip_efficiency,
         },
         flex_context={
-            "consumption-price-sensor": (
-                consumption_price_sensor.id if consumption_price_sensor else None
+            "consumption-price": (
+                {"sensor": consumption_price_sensor.id}
+                if consumption_price_sensor
+                else None
             ),
-            "production-price-sensor": (
-                production_price_sensor.id if production_price_sensor else None
+            "production-price": (
+                {"sensor": production_price_sensor.id}
+                if production_price_sensor
+                else None
             ),
             "inflexible-device-sensors": [s.id for s in inflexible_device_sensors],
         },
