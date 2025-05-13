0b0b32464efdcc1786db7f513f8a9550d2ac484a
joshuaunity <oghenerobojosh01@gmail.com>
2025-05-13 10:53:37 +0100

    chore: removed auth test prefix
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/api/v3_0/tests/test_api_v3_0_users_fresh_db.py
+++ flexmeasures/api/v3_0/tests/test_api_v3_0_users_fresh_db.py
@@ -70,7 +70,7 @@ def test_user_reset_password(
     ],
     indirect=["requesting_user"],
 )
-def test_auth_user_role_modification_permission(
+def test_user_role_modification_permission(
     client,
     setup_api_fresh_test_data,
     requesting_user,
