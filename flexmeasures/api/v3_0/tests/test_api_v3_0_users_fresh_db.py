7ce069d4aa39116d0253d66bcc8625aa24004ee1
joshuaunity <oghenerobojosh01@gmail.com>
2025-05-21 11:39:06 +0100

    chore: better comments on testcase
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/api/v3_0/tests/test_api_v3_0_users_fresh_db.py
+++ flexmeasures/api/v3_0/tests/test_api_v3_0_users_fresh_db.py
@@ -66,8 +66,10 @@ def test_user_reset_password(
 @pytest.mark.parametrize(
     "requesting_user, expected_status_code, user_to_update, expected_role",
     [
-        ("test_admin_user@seita.nl", 200, 4, [3, 4]),  # admin user
-        ("test_dummy_account_admin@seita.nl", 200, 8, [3, 4]),  # account-admin user
+        # Admin updates user 4 (initially no roles) to become admin-reader & consultant
+        ("test_admin_user@seita.nl", 200, 4, [3, 4]),
+        # Admin updates user 5 (initial an account-admin) removing the account-admin role
+        ("test_admin_user@seita.nl", 200, 5, []),
     ],
     indirect=["requesting_user"],
 )
