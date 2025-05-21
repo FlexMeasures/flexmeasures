8ffac67da9428d361a1df5bc6d494f773884591b
F.N. Claessen <felix@seita.nl>
2025-05-21 18:48:03 +0200

    fix: increase the price slope to match the slope to prefer charging sooner
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -455,11 +455,11 @@ class MetaStorageScheduler(Scheduler):
         # Flow commitments per device
 
         # Add tiny price slope to prefer curtailing later rather than now.
-        # The price slope is ranged to be a smaller fraction of the energy price spread than the slope to prefer charging sooner
+        # The price slope is the same as that of the slope to prefer charging sooner
         for d, prefer_curtailing_later_d in enumerate(prefer_curtailing_later):
             if prefer_curtailing_later_d:
                 tiny_price_slope = (
-                    add_tiny_price_slope(up_deviation_prices, "event_value", d=10**-4)
+                    add_tiny_price_slope(up_deviation_prices, "event_value")
                     - up_deviation_prices
                 )
                 commitment = FlowCommitment(
