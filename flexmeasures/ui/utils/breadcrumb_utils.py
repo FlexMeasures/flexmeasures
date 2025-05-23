4710989fa7065dfcfe27d07593f8474979806830
Muhammad-Moiz626 <muhammadmoiz4843@gmail.com>
2025-05-23 17:34:21 +0500

    Updated the siblings rendering for assets with owner as Account.

--- flexmeasures/ui/utils/breadcrumb_utils.py
+++ flexmeasures/ui/utils/breadcrumb_utils.py
@@ -141,7 +141,10 @@ def get_siblings(entity: Sensor | Asset | Account | None) -> list[dict]:
         if entity.parent_asset is not None:
             sibling_assets = entity.parent_asset.child_assets
         elif entity.owner is not None:
-            sibling_assets = entity.owner.generic_assets
+            if isinstance(entity.owner, Account):
+                sibling_assets = []
+            else:
+                sibling_assets = entity.owner.generic_assets
         else:
             session = current_app.db.session
             sibling_assets = session.scalars(
