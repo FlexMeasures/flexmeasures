0b098900c64559c7c3ceafcf9f4ed88c8ef1dfee
F.N. Claessen <felix@seita.nl>
2025-06-13 21:07:15 +0200

    fix: update test accordingly
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/v3_0/tests/test_asset_schedules_fresh_db.py
+++ flexmeasures/api/v3_0/tests/test_asset_schedules_fresh_db.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from flask import url_for
 import pytest
 from isodate import parse_datetime, parse_duration
@@ -6,6 +8,7 @@ from numpy.testing import assert_equal
 import pandas as pd
 from rq.job import Job
 
+from flexmeasures import Sensor
 from flexmeasures.api.v3_0.tests.utils import message_for_trigger_schedule
 from flexmeasures.data.tests.utils import work_on_rq
 from flexmeasures.data.services.scheduling import (
@@ -137,16 +140,43 @@ def test_asset_trigger_and_get_schedule(
     scheduler_source = get_data_source_for_job(scheduling_job)
     assert scheduler_source is not None
 
+    def compute_expected_length(
+        message: dict, sensors: list[Sensor], sequential: bool
+    ) -> list[int]:
+        """We expect a longer schedule if the targets exceeds the original duration in the trigger.
+
+        If the planning happens sequentially, individual schedules may be extended to accommodate for far-away targets.
+        If the planning happens jointly, we expect all schedules to be extended.
+        """
+        expected_durations = [pd.Timedelta(message["duration"])] * len(sensors)
+        for d, (sensor, flex_model) in enumerate(zip(sensors, message["flex-model"])):
+            assert (
+                flex_model["sensor"] == sensor.id
+            ), "make sure we are dealing with the assumed sensor"
+            if "soc-targets" in flex_model:
+                for t in flex_model["soc-targets"]:
+                    duration = pd.Timestamp(t["datetime"]) - pd.Timestamp(
+                        message["start"]
+                    )
+                    if duration > expected_durations[d]:
+                        if sequential:
+                            expected_durations[d] = duration
+                        else:
+                            expected_durations = [duration] * len(sensors)
+
+        # Convert duration to number of steps in the sensor's resolution
+        expected_lengths = [
+            expected_durations[d] / sensor.event_resolution
+            for d, sensor in enumerate(sensors)
+        ]
+
+        return expected_lengths
+
+    sensors = [sensor_1, sensor_2]
+    expected_length_of_schedule = compute_expected_length(message, sensors, sequential)
+
     # try to retrieve the schedule for each sensor through the /sensors/<id>/schedules/<job_id> [GET] api endpoint
-    for flex_model in message["flex-model"]:
-
-        # We expect a longer schedule if the targets exceeds the original duration in the trigger
-        if "soc-targets" in flex_model:
-            for t in flex_model["soc-targets"]:
-                duration = pd.Timestamp(t["datetime"]) - pd.Timestamp(message["start"])
-                if duration > pd.Timedelta(message["duration"]):
-                    expected_length_of_schedule = duration / resolution
-
+    for d, flex_model in enumerate(message["flex-model"]):
         sensor_id = flex_model["sensor"]
         get_schedule_response = client.get(
             url_for("SensorAPI:get_schedule", id=sensor_id, uuid=scheduling_job.id),
@@ -155,7 +185,9 @@ def test_asset_trigger_and_get_schedule(
         print("Server responded with:\n%s" % get_schedule_response.json)
         assert get_schedule_response.status_code == 200
         # assert get_schedule_response.json["type"] == "GetDeviceMessageResponse"
-        assert len(get_schedule_response.json["values"]) == expected_length_of_schedule
+        assert (
+            len(get_schedule_response.json["values"]) == expected_length_of_schedule[d]
+        )
 
         if sequential:
             if sensor_id == sensor_1.id:
@@ -202,13 +234,17 @@ def test_asset_trigger_and_get_schedule(
                     get_schedule_response.json["values"],
                     # fmt: off
                     [
-                        -2.0, -2.0, -2.0, -2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
-                        -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, 2.0,  # noqa: E128
-                        2.0, 2.0, -2.0, -2.0, -2.0, -2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, -2.0, -1.323137, -2.0,  # noqa: E128
-                        -2.0, -2.0, -2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
-                        -2.0, -2.0, -1.667577, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,  # noqa: E128
-                        2.0, 2.0, 2.0, -2.0, -2.0, -2.0, -1.309551, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
-                        -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        2.0, -0.0, -0.0, -0.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0,  # noqa: E128
+                        -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        -1.784316, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, -0.0, -0.0, -0.0, -0.0, 2.0, 2.0, 2.0, 2.0, 2.0,  # noqa: E128
+                        2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,  # noqa: E128
+                        2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,  # noqa: E128
+                        2.0, 2.0, 2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        -2.0, -2.0, -2.0, -2.0, -1.694362, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
+                        -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0,  # noqa: E128
                     ]
                     # fmt: on
                 )
@@ -217,13 +253,18 @@ def test_asset_trigger_and_get_schedule(
                     get_schedule_response.json["values"],
                     # fmt: off
                     [
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
-                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, 2.0, 2.0, 2.0, 2.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, 2.0, 2.0, 2.0, 2.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        2.0, 2.0, 2.0, 2.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        2.0, 2.0, 1.749626, -0.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
+                        -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0,  # noqa: E128
                     ]
                     # fmt: on
                 )
