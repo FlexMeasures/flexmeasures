9354979ef672e27f5650ccf8e9dea00e767459e3
F.N. Claessen <felix@seita.nl>
2025-06-16 15:20:08 +0200

    refactor: simplify
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/tests/utils.py
+++ flexmeasures/data/models/planning/tests/utils.py
@@ -47,15 +47,11 @@ def check_constraints(
     assert min(schedule.values) >= capacity * -1 - tolerance
     assert max(schedule.values) <= capacity + tolerance
     for soc in soc_schedule.values:
-        assert (
-            soc >= soc_min
-            if soc_min is not None
-            else soc >= sensor.get_attribute("min_soc_in_mwh")
+        assert soc >= (
+            soc_min if soc_min is not None else sensor.get_attribute("min_soc_in_mwh")
         )
-        assert (
-            soc <= soc_max
-            if soc_max is not None
-            else soc <= sensor.get_attribute("max_soc_in_mwh")
+        assert soc <= (
+            soc_max if soc_max is not None else sensor.get_attribute("max_soc_in_mwh")
         )
     return soc_schedule
 
