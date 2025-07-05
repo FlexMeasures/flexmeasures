72d16d7c7fcd524a9a089eaf67f205a8a83d2212
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-07-05 17:33:42 +0200

    refactor: remove unused utils functions.
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/data/models/forecasting/residential/utils.py
+++ flexmeasures/data/models/forecasting/residential/utils.py
@@ -1,91 +1,9 @@
-import os
-
 import numpy as np
 import pandas as pd
 import timely_beliefs as tb
 from flexmeasures.data.models.time_series import Sensor, TimedBelief
 from flexmeasures.data.utils import get_data_source
 
-
-def _get_source_from_path(path: str) -> tb.BeliefSource:
-    """
-    Extracts the belief source identifier from a given file path and returns a BeliefSource object.
-
-    The function performs the following steps:
-    1. Extracts the file name from the given pzath.
-    2. Removes the file extension from the file name.
-    3. Splits the file name by underscores and selects parts from the 7th element onwards.
-    4. Joins these parts back together with underscores to form the belief source identifier.
-    5. Creates and returns a BeliefSource object initialized with the identifier.
-
-    Example:
-    "df_48_hour_heating_demand_point_rf.csv" -> "rf"
-    "df_48_hour_heating_demand_probabilistic_lgbm.csv" -> "lgbm"
-    """
-    file_name_with_extension = os.path.split(path)[-1]
-    file_name = os.path.splitext(file_name_with_extension)[0]
-
-    # Splitting the file name by underscore and selecting parts from the 7th element onwards
-    belief_source_parts = file_name.split("_")[6:]
-    belief_source_identifier = "_".join(belief_source_parts)
-    belief_source = tb.BeliefSource(belief_source_identifier)
-
-    return belief_source
-
-
-def csv_to_bdf(path: str, nrows: int, probabilistic: bool) -> tb.BeliefsDataFrame:
-    sensor = tb.Sensor(
-        "heating demand", unit="W", event_resolution=pd.Timedelta(hours=1)
-    )
-    source = _get_source_from_path(path)
-    timezone = "Europe/Amsterdam"
-
-    # Read the CSV file
-    df = pd.read_csv(path, nrows=nrows).rename(
-        {
-            "datetime": "belief_time",
-            "heating demand": "0h",
-        },
-        axis=1,
-    )
-
-    if probabilistic:
-        # Extract cumulative probability from the 'component' column and drop it
-        df["cumulative_probability"] = (
-            df["component"].str.extract(r"(\d+\.\d+)").astype(float)
-        )
-        df = df.drop(columns=["component"])
-        id_vars = ["belief_time", "cumulative_probability"]
-    else:
-        id_vars = ["belief_time"]
-
-    df = pd.melt(
-        df,
-        id_vars=id_vars,
-        value_vars=[col for col in df.columns if col not in id_vars],
-        var_name="belief_horizon",
-        value_name="event_value",
-    )
-
-    # Ensure correct dtypes
-    df["belief_time"] = pd.to_datetime(
-        df["belief_time"], infer_datetime_format=True
-    ).dt.tz_localize(timezone)
-    df["belief_horizon"] = pd.to_timedelta(df["belief_horizon"])
-    df["event_start"] = (
-        df["belief_time"] + df["belief_horizon"] - sensor.event_resolution
-    )
-
-    # Create the BeliefsDataFrame
-    bdf_kwargs = {
-        "sensor": sensor,
-        "source": source,
-    }
-    if probabilistic:
-        bdf_kwargs["cumulative_probability"] = df["cumulative_probability"].values
-
-    bdf = tb.BeliefsDataFrame(df, **bdf_kwargs)
-    return bdf
 
 
 def negative_to_zero(x: np.ndarray) -> np.ndarray:
