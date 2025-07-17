2bf099bf66e21262a9bae3570baebf391ff703e4
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-07-18 00:39:05 +0200

    feat: make fields required True or False in ForecastingPipelineSchema for better validation
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/data/schemas/forecasting/pipeline.py
+++ flexmeasures/data/schemas/forecasting/pipeline.py
@@ -5,20 +5,20 @@ from flexmeasures.data.schemas import AwareDateTimeField, SensorIdField
 
 class ForecastingPipelineSchema(Schema):
 
-    sensors = fields.Str()
-    regressors = fields.Str()
-    future_regressors = fields.Str()
-    target = fields.Str()
-    model_save_dir = fields.Str()
+    sensors = fields.Str(required=True)  # expects JSON string like '{"target": 123, "Ta": 456}'
+    regressors = fields.Str(required=False, allow_none=True)  # expects comma-separated string like "target,Ta"
+    future_regressors = fields.Str(required=False, allow_none=True)  # expects comma-separated string
+    target = fields.Str(required=True)
+    model_save_dir = fields.Str(required=True)
     output_path = fields.Str(required=False, allow_none=True)
-    start_date = AwareDateTimeField()
-    end_date = AwareDateTimeField()
+    start_date = AwareDateTimeField(required=True)
+    end_date = AwareDateTimeField(required=True)
     train_period = fields.Int(required=False, allow_none=True)
     start_predict_date = AwareDateTimeField(required=False, allow_none=True)
     predict_period = fields.Int(required=False, allow_none=True)
     max_forecast_horizon = fields.Int(required=False, allow_none=True)
     forecast_frequency = fields.Int(required=False, allow_none=True)
-    probabilistic = fields.Bool()
+    probabilistic = fields.Bool(required=True)
     sensor_to_save = SensorIdField(required=False, allow_none=True)
 
     @validates_schema()
