53986a4ccc4e4fb957265762a879d26aad1feda1
F.N. Claessen <felix@seita.nl>
2025-08-19 10:45:08 +0200

    fix: NaN datetimes in url upon refresh, but keep human-friendly datetime format in url
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/ui/utils/view_utils.py
+++ flexmeasures/ui/utils/view_utils.py
@@ -56,12 +56,12 @@ def render_flexmeasures_template(html_filename: str, **variables):
         variables["documentation_exists"] = True
 
     # use event_starts_after and event_ends_before from session if not given
-    variables["event_starts_after"] = variables.get(
-        "event_starts_after"
-    ) or session.get("event_starts_after")
-    variables["event_ends_before"] = variables.get("event_ends_before") or session.get(
-        "event_ends_before"
-    )
+    variables["event_starts_after"] = (
+        variables.get("event_starts_after") or session.get("event_starts_after")
+    ).replace(" ", "+")
+    variables["event_ends_before"] = (
+        variables.get("event_ends_before") or session.get("event_ends_before")
+    ).replace(" ", "+")
 
     variables["chart_type"] = session.get("chart_type", "bar_chart")
 
