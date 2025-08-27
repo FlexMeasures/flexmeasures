81e36016427c92c4b281612d30e7a010e072f8f2
F.N. Claessen <felix@seita.nl>
2025-08-27 14:26:13 +0200

    feat: add test for unhashable entities used as dict keys
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/utils/tests/test_coding_utils.py
+++ flexmeasures/utils/tests/test_coding_utils.py
@@ -1,3 +1,6 @@
+import pytest
+
+from flexmeasures import Asset, AssetType, Sensor
 from flexmeasures.utils.coding_utils import deprecated
 
 
@@ -29,3 +32,19 @@ def test_deprecated_decorator(caplog, app):
     assert (
         value == 1
     )  # check that the decorator is returning the value of `other_function`
+
+
+def test_unhashable_entities_as_dict_keys(db):
+    asset_type = AssetType(name="foo")
+    asset = Asset(name="bar", generic_asset_type=asset_type)
+    sensor_a = Sensor(name="A", unit="m", event_resolution="PT15M", generic_asset=asset)
+    sensor_b = Sensor(name="B", unit="m", event_resolution="PT15M", generic_asset=asset)
+    db.session.add(sensor_a)
+    db.session.add(sensor_b)
+    sensors = [sensor_a, sensor_b]
+    with pytest.raises(TypeError) as exc_info:
+        sensor_dict = {s: s.name for s in sensors}  # noqa: F841
+    assert (
+        "Consider calling `db.session.flush()` before using Sensor objects in sets or as dictionary keys."
+        in str(exc_info)
+    )
