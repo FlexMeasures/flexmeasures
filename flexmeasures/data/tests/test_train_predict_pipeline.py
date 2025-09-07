db687e2dafbbdb49bbc56570ff680ef137f45fc2
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-09-08 01:26:44 +0200

    fix: correct comment for start_predict_date in test case to clarify its relation to available data
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/data/tests/test_train_predict_pipeline.py
+++ flexmeasures/data/tests/test_train_predict_pipeline.py
@@ -36,7 +36,7 @@ from flexmeasures.data.models.forecasting.pipelines import TrainPredictPipeline
                 "model_save_dir": "flexmeasures/data/models/forecasting/artifacts/models",
                 "output_path": None,
                 "start_date": "2025-01-01T00:00+02:00",
-                "start_predict_date": "2025-01-08T00:00+02:00",  # start_predict_date after coincides with end of available data in sensor
+                "start_predict_date": "2025-01-08T00:00+02:00",  # start_predict_date coincides with end of available data in sensor
                 "end_date": "2025-01-09T00:00+02:00",
                 "sensor_to_save": None,
                 "max_forecast_horizon": "PT1H",
