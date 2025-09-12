afffdd9d2999225ba502bc1f3ab99e83949871f2
F.N. Claessen <felix@seita.nl>
2025-09-12 12:17:25 +0200

    dev: comment out a call
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/ws/s2_ws_sync.py
+++ flexmeasures/ws/s2_ws_sync.py
@@ -412,13 +412,13 @@ class S2FlaskWSServerSync:
             # Use the S2Scheduler to create and store device state
             if hasattr(self, "s2_scheduler") and self.s2_scheduler is not None:
                 # Create S2FrbcDeviceState from FRBC messages and store in scheduler
-                device_state = self.s2_scheduler.create_device_states_from_frbc_data(
-                    resource_id=resource_id,
-                    system_description=device_data.system_description,
-                    fill_level_target_profile=device_data.fill_level_target_profile,
-                    storage_status=device_data.storage_status,
-                    actuator_status=device_data.actuator_status,
-                )
+                # self.device_state = self.s2_scheduler.create_device_states_from_frbc_data(
+                #     resource_id=resource_id,
+                #     system_description=device_data.system_description,
+                #     fill_level_target_profile=device_data.fill_level_target_profile,
+                #     storage_status=device_data.storage_status,
+                #     actuator_status=device_data.actuator_status,
+                # )
 
                 # Generate instructions using the scheduler
                 schedule_results = self.s2_scheduler.compute()
