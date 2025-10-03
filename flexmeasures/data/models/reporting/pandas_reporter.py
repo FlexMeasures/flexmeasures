f76125ca7639fd7c427a6130af0ad49cb718ca1c
F.N. Claessen <felix@seita.nl>
2025-10-03 12:52:33 +0200

    feat: add suggestion to error message
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/reporting/pandas_reporter.py
+++ flexmeasures/data/models/reporting/pandas_reporter.py
@@ -349,8 +349,12 @@ class PandasReporter(Reporter):
             if (_any_empty(args) or _any_empty(kwargs.values())) and skip_if_empty:
                 self.data[df_output] = self.data[df_input]
             else:
-                self.data[df_output] = getattr(self.data[df_input], method)(
-                    *args, **kwargs
-                )
+                try:
+                    self.data[df_output] = getattr(self.data[df_input], method)(
+                        *args, **kwargs
+                    )
+                except TypeError as exc:
+                    if "unhashable type" in str(exc) and method == "sum":
+                        raise TypeError("Maybe use 'add' instead of 'sum'") from exc
 
             previous_df = df_output
