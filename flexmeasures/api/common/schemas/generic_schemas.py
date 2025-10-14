ef969a4f66d5e5088e9a67d1db2e652028610407
Nicolas Höning <nicolas@seita.nl>
2025-10-14 12:31:18 +0200

    Update flexmeasures/api/common/schemas/generic_schemas.py
    Signed-off-by: Nicolas Höning <iam@nicolashoening.de>

--- flexmeasures/api/common/schemas/generic_schemas.py
+++ flexmeasures/api/common/schemas/generic_schemas.py
@@ -3,6 +3,7 @@ from flexmeasures.api.common.schemas.search import SearchFilterField
 
 
 class PaginationSchema(Schema):
+    # note: the absence of this parameter would signal to the API to not paginate (so there is no default set here)
     page = fields.Int(required=False, validate=validate.Range(min=1))
     per_page = fields.Int(
         required=False, validate=validate.Range(min=1), load_default=10
