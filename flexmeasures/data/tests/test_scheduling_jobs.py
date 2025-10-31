a9d9b1c0a7b77d8eacabc479010b65e25fd9bc36
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-10-31 23:06:16 +0100

    Revert "chore: remove unnecessary fixtures from scheduling unit conversion test"
    This reverts commit f004fe17a5d0e678eef1f08cad44e7896c67966d.

--- flexmeasures/data/tests/test_scheduling_jobs.py
+++ flexmeasures/data/tests/test_scheduling_jobs.py
@@ -392,6 +392,8 @@ def test_scheduling_unit_conversion(
     fresh_db,
     app,
     add_battery_assets_fresh_db,
+    setup_fresh_test_data,
+    add_market_prices_fresh_db,
 ):
     """Test scheduling of a battery SOC sensor with kWh units, ensuring correct data creation and unit handling."""
 
