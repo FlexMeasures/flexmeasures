8c6022c10dc8a334d140007bfdbf4acb0eb10393
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-11-03 23:21:11 +0100

    doc: update test docstring
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/data/tests/test_scheduling_jobs.py
+++ flexmeasures/data/tests/test_scheduling_jobs.py
@@ -395,7 +395,7 @@ def test_scheduling_unit_conversion(
     setup_fresh_test_data,
     add_market_prices_fresh_db,
 ):
-    """Test scheduling of a battery SOC sensor with kWh units, ensuring correct data creation and unit handling."""
+    """Test scheduling of a battery consumption sensor with kWh units, ensuring correct data creation and unit handling."""
 
     battery_consumption_sensor = add_battery_assets_fresh_db["Test battery"].sensors[2]
 
