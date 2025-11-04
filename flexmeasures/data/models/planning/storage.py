a61e056a9c28537cee79f21f3dbb9bf0d23839bb
Nicolas Höning <nicolas@seita.nl>
2025-11-04 17:53:23 +0100

    improve error messages w.r.t. to asset ID
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -1173,7 +1173,7 @@ class MetaStorageScheduler(Scheduler):
                     site_power_capacity = site_power_capacity.get("sensor", None)
                     if site_power_capacity is None:
                         raise ValueError(
-                            "site-power-capacity attribute is a dict, but has no sensor key."
+                            f"site-power-capacity attribute on asset {asset.id} is a dict, but has no sensor key."
                         )
 
                 power_capacities.append(
@@ -1181,7 +1181,9 @@ class MetaStorageScheduler(Scheduler):
                 )
                 continue
 
-            raise ValueError("Power capacity is not defined in the flex-model.")
+            raise ValueError(
+                f"Power capacity on asset {asset.id} is not defined in the flex-model."
+            )
         return power_capacities
 
     def _ensure_variable_quantity(
