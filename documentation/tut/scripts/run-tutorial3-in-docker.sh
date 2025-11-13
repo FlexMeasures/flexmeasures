44f4d10d32aafc677cfba9c82be2f30ad6e347f2
Nicolas Höning <nicolas@seita.nl>
2025-11-13 16:26:41 +0100

    improve flex context for PV curtailment setup (in script only so far)
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- documentation/tut/scripts/run-tutorial3-in-docker.sh
+++ documentation/tut/scripts/run-tutorial3-in-docker.sh
@@ -5,13 +5,32 @@ echo "--------------------------------------------------------------------------
 
 TOMORROW=$(date --date="next day" '+%Y-%m-%d')
 
-echo "[TUTORIAL-RUNNER] Re-computing schedule with curtailment asset added ..."
+echo "[TUTORIAL-RUNNER] Computing schedule for PV curtailment (using artificial price schema) ..."
+
+echo '''{
+  "consumption-price": [
+    {"start": "'${TOMORROW}'T00:00+00", "duration": "PT24H", "value": "0 EUR/MWh"}
+  ],
+  "production-price": [
+    {"start": "'${TOMORROW}'T05:00+00", "duration": "PT7H", "value": "4 EUR/MWh"},
+    {"start": "'${TOMORROW}'T12:00+00", "duration": "PT2H", "value": "-10 EUR/MWh"},
+    {"start": "'${TOMORROW}'T14:00+00", "duration": "PT7H", "value": "4 EUR/MWh"}
+  ]
+}''' > tutorial3-flex-context.json
+docker cp tutorial3-flex-context.json flexmeasures-server-1:/app/ 
 
 docker exec -it flexmeasures-server-1 flexmeasures add schedule --sensor 3 \
   --start ${TOMORROW}T07:00+00:00 --duration PT12H \
-  --flex-model '{"consumption-capacity": "0 kW", "production-capacity": {"sensor": 3}, "soc-at-start": "1kWh"}'\
-  --flex-context '{"production-price": [{"start": "2025-11-12T00:07+00", "duration": "PT5H", "value": "4 EUR/MWh"}, {"start": "2025-11-12T12:00+00", "duration": "PT2H", "value": "-10 EUR/MWh"}, {"start": "2025-11-12T14:00+00", "duration": "PT5H", "value": "4 EUR/MWh"}]}'
-#
+  --flex-model '{"consumption-capacity": "0 kW", "production-capacity": {"sensor": 3}}'\
+  --flex-context tutorial3-flex-context.json 
+echo "[TUTORIAL-RUNNER] showing PV schedule ..."
+docker exec -it flexmeasures-server-1 flexmeasures show beliefs --sensor 3 --start ${TOMORROW}T07:00:00+00:00 --duration PT12H
+
+exit
+
+
+echo "[TUTORIAL-RUNNER] Re-computing schedule in multi-asset manner ..."
+
 #docker exec -it flexmeasures-server-1 flexmeasures add schedule --asset 2 \
 #  --start ${TOMORROW}T07:00+00:00 --duration PT12H \
 #  --flex-model '[{"sensor": 3, "consumption-capacity": "0 kW", "production-capacity": {"sensor": 3}}]'\
@@ -21,6 +40,3 @@ docker exec -it flexmeasures-server-1 flexmeasures add schedule --sensor 3 \
 
 echo "[TUTORIAL-RUNNER] showing battery and PV schedules ..."
 docker exec -it flexmeasures-server-1 flexmeasures show beliefs --sensor 2 --sensor 3 --start ${TOMORROW}T07:00:00+00:00 --duration PT12H
-
-# echo "[TUTORIAL-RUNNER] showing PV schedule ..."
-# docker exec -it flexmeasures-server-1 flexmeasures show beliefs --sensor 3 --start ${TOMORROW}T07:00:00+00:00 --duration PT12H
