e9b6f8585981010e8c8485de970ad5174d47461f
F.N. Claessen <felix@seita.nl>
2025-11-26 15:33:55 +0100

    feat: let charts show all versions of data sources
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/dev/sensors.py
+++ flexmeasures/api/dev/sensors.py
@@ -87,6 +87,7 @@ class SensorAPI(FlaskView):
             "beliefs_after": AwareDateTimeField(format="iso", required=False),
             "beliefs_before": AwareDateTimeField(format="iso", required=False),
             "resolution": DurationField(required=False),
+            "use_latest_version_per_event": fields.Boolean(required=False),
             "most_recent_beliefs_only": fields.Boolean(
                 required=False, load_default=True
             ),
