036c22d8f1c1215ea8c51b354d55f6238d4aac7f
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-12-05 03:26:01 +0100

    feat: set random seed for consistent solar sensor values in tests
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/conftest.py
+++ flexmeasures/conftest.py
@@ -1762,6 +1762,8 @@ def add_test_solar_sensor_and_irradiance_with_forecasts(
         )
         db.session.add(sensor)
 
+        random_seed = 42 if name == "irradiance-sensor" else 43  # to keep solar-sensor and solar-sensor-1 values the same
+        seed(random_seed)
         values = [
             random() * (1 + np.sin(x / 15)) * config["multiplier"]
             for x in range(len(time_slots))
