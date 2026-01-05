62b4f200198598bacb2ba38e248def843fc7f54f
Nicolas Höning <nicolas@seita.nl>
2026-01-06 00:06:53 +0100

    add `fields` param to the asset-listing endpoints, controlling fields in response (#1884)
    * feat: index assets with a given root asset
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * feat: index assets up to a certain level
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * fix: in case of None max_level
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * fix: in case of None asset
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * refactor: rename max_level to max_depth
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * feat: rely on max recursion setting of db
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * fix: OpenAPI specs generation
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * chore: OpenAPI specs
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: change order of query parameters explained
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * style: fix double spaces
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * refactor: rename new query parameter from 'asset' to 'root'
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: API changelog entry
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: main changelog entry
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * docs: attempt to clarify a confusing endpoint summary
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * add included-fields param to the asset-listing endpoint; (the endpoint for pubic for now returns a default set); also improve some parameter descriptions
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * make asset listing pages say which fields they do need
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * do not use hyphen
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * add changelog entries
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * Add new field also to public assets endpoint
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * simplify name of the parameter to just 'fields'
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * check if fields actually are valid fields that could be returned
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * adjust docs to new param name, as well
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * warn users in changelog of less data being returned per asset
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * use load_default so SwaggerDocs shows it in the intented way
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * better example fields for getting public assets
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * further work after the merge conflicts
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * be more robust against empty strings
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * build in the API_SUNSET_ACTIVE switch
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * adapt documentation to the sunset switch
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * remove redundant condition
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    ---------
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    Co-authored-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/api/common/schemas/assets.py
+++ flexmeasures/api/common/schemas/assets.py
@@ -1,14 +1,55 @@
-from marshmallow import fields, validate
+from marshmallow import Schema, fields, validate
 
 from flexmeasures.api.common.schemas.generic_schemas import PaginationSchema
 from flexmeasures.api.common.schemas.users import AccountIdField
 from flexmeasures.data.schemas import AssetIdField
+from flexmeasures.data.schemas.generic_assets import GenericAssetSchema
+
+
+default_response_fields = ["id", "name", "account_id", "generic_asset_type"]
+
+
+class PipedAssetFieldListField(fields.Str):
+    """
+    Field that represents a list of Strings, in serialized form joined by "|".
+    Each one should represent a field in the AssetAPIQuerySchema.
+    """
+
+    def _deserialize(self, values: str, attr, obj, **kwargs) -> list[str]:
+        if not isinstance(values, str):
+            raise validate.ValidationError(
+                "Invalid input type, should be a string, separable by '|'."
+            )
+        parameters = values.split("|") if values else []
+        non_empty_parameters = [p for p in parameters if p.strip()]
+        for parameter in non_empty_parameters:
+            if parameter not in GenericAssetSchema._declared_fields:
+                raise validate.ValidationError(
+                    f"Parameter '{parameter}' is not a valid asset field."
+                )
+        return non_empty_parameters
+
+    def _serialize(self, values: list[str], attr, obj, **kwargs) -> str:
+        if not values:
+            return ""
+        return "|".join(values)
 
 
 class AssetAPIQuerySchema(PaginationSchema):
     sort_by = fields.Str(
         required=False,
         validate=validate.OneOf(["id", "name", "owner"]),
+        metadata=dict(
+            description="Sort results by this field.",
+        ),
+    )
+    fields_in_response = PipedAssetFieldListField(
+        data_key="fields",
+        load_default=default_response_fields,
+        metadata=dict(
+            description="Which fields to include in response. List fields separated by '|' (pipe).",
+            example="id|name|flex_model",
+        ),
     )
     account = AccountIdField(
         data_key="account_id",
@@ -49,6 +90,17 @@ class AssetAPIQuerySchema(PaginationSchema):
         metadata=dict(
             description="Whether to include public assets. Ignored if an `account_id` is set. To fetch only public assets, use [/assets/public/](#/Assets/get_api_v3_0_assets_public) instead.",
             example=False,
+        ),
+    )
+
+
+class PublicAssetAPISchema(Schema):
+    fields_in_response = PipedAssetFieldListField(
+        data_key="fields",
+        load_default=default_response_fields,
+        metadata=dict(
+            description="Which fields to include in response. List fields separated by '|' (pipe).",
+            example="id|name|sensors",
         ),
     )
 
