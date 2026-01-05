0675d2146df2c2e1e775654fafc34e6211e80868
F.N. Claessen <felix@seita.nl>
2026-01-06 00:49:35 +0100

    fix: support overriding the scheduling resolution in case of multi-asset scheduling
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/models/planning/storage.py
+++ flexmeasures/data/models/planning/storage.py
@@ -105,11 +105,12 @@ class MetaStorageScheduler(Scheduler):
                 s.asset if s is not None else flex_model_d.get("asset")
                 for s, flex_model_d in zip(sensors, self.flex_model)
             ]
-            # in case of no sensors with a non-instantaneous resolution, schedule with a 15-minute resolution
-            resolution = determine_minimum_resampling_resolution(
-                [s.event_resolution for s in sensors if s is not None],
-                fallback_resolution=self.default_resolution,
-            )
+            if resolution is None:
+                # in case of no sensors with a non-instantaneous resolution, schedule with a 15-minute resolution
+                resolution = determine_minimum_resampling_resolution(
+                    [s.event_resolution for s in sensors if s is not None],
+                    fallback_resolution=self.default_resolution,
+                )
             asset = self.asset
         else:
             # For backwards compatibility with the single asset scheduler
