3ba89180cbd4a013108c46e6e0ddcd428e70499d
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2026-01-09 16:02:51 +0100

    test(feat): update forecasts end_dates to generate only two forecasts this is to reduce test runtime
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/api/v3_0/tests/test_forecasting_api.py
+++ flexmeasures/api/v3_0/tests/test_forecasting_api.py
@@ -88,7 +88,7 @@ def test_trigger_and_fetch_forecasts(
     payload = {
         "start_date": "2025-01-01T00:00:00+00:00",
         "start_predict_date": "2025-01-04T00:00:00+00:00",
-        "end_date": "2025-01-04T04:00:00+00:00",
+        "end_date": "2025-01-04T00:30:00+00:00",
     }
 
     trigger_url = url_for("SensorAPI:trigger_forecast", sensor=sensor.id)
@@ -143,7 +143,7 @@ def test_trigger_and_fetch_forecasts(
 
         forecasts = sensor.search_beliefs(
             event_starts_after="2025-01-04T00:00:00+00:00",
-            event_ends_before="2025-01-04T04:00:00+00:00",
+            event_ends_before="2025-01-04T00:30:00+00:00",
             source=data_source,
             most_recent_beliefs_only=True,
             use_latest_version_per_event=True,
