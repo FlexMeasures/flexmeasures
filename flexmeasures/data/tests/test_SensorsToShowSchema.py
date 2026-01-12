fbb9791aab0ac438c3aca4c80ada19caa0f3d611
joshuaunity <oghenerobojosh01@gmail.com>
2026-01-12 13:48:54 +0100

    tests: adapting more testcases to new schema shape
    Signed-off-by: joshuaunity <oghenerobojosh01@gmail.com>

--- flexmeasures/data/tests/test_SensorsToShowSchema.py
+++ flexmeasures/data/tests/test_SensorsToShowSchema.py
@@ -7,7 +7,7 @@ from flexmeasures.data.schemas.generic_assets import SensorsToShowSchema
 def test_single_sensor_id():
     schema = SensorsToShowSchema()
     input_value = [42]
-    expected_output = [{"title": None, "sensors": [42]}]
+    expected_output = [{"title": None, "plots": [{"sensor": 42}]}]
     assert schema.deserialize(input_value) == expected_output
 
 
@@ -15,23 +15,23 @@ def test_list_of_sensor_ids():
     schema = SensorsToShowSchema()
     input_value = [42, 43]
     expected_output = [
-        {"title": None, "sensors": [42]},
-        {"title": None, "sensors": [43]},
+        {"title": None, "plots": [{"sensor": 42}]},
+        {"title": None, "plots": [{"sensor": 43}]},
     ]
     assert schema.deserialize(input_value) == expected_output
 
 
 def test_dict_with_title_and_single_sensor():
     schema = SensorsToShowSchema()
-    input_value = [{"title": "Temperature", "sensor": 42}]
-    expected_output = [{"title": "Temperature", "sensors": [42]}]
+    input_value = [{"title": "Temperature", "plots": [{"sensor": 42}]}]
+    expected_output = [{"title": "Temperature", "plots": [{"sensor": 42}]}]
     assert schema.deserialize(input_value) == expected_output
 
 
 def test_dict_with_title_and_multiple_sensors():
     schema = SensorsToShowSchema()
-    input_value = [{"title": "Pressure", "sensors": [42, 43]}]
-    expected_output = [{"title": "Pressure", "sensors": [42, 43]}]
+    input_value = [{"title": "Pressure", "plots": [{"sensors": [42, 43]}]}]
+    expected_output = [{"title": "Pressure", "plots": [{"sensors": [42, 43]}]}]
     assert schema.deserialize(input_value) == expected_output
 
 
@@ -71,9 +71,9 @@ def test_mixed_valid_inputs():
         5,
     ]
     expected_output = [
-        {"title": "Test", "sensors": [1, 2]},
-        {"title": None, "sensors": [3, 4]},
-        {"title": None, "sensors": [5]},
+        {"title": "Test", "plots": [{"sensors": [1, 2]}]},
+        {"title": None, "plots": [{"sensors": [3, 4]}]},
+        {"title": None, "plots": [{"sensor": 5}]},
     ]
     assert schema.deserialize(input_value) == expected_output
 
