74e7ccb9e626049f4deef4a24d14c3dfb4b00966
JDev <45713692+joshuaunity@users.noreply.github.com>
2026-01-20 11:21:42 +0100

    Update flexmeasures/utils/config_defaults.py
    Co-authored-by: Nicolas Höning <nicolas@seita.nl>
    Signed-off-by: JDev <45713692+joshuaunity@users.noreply.github.com>

--- flexmeasures/utils/config_defaults.py
+++ flexmeasures/utils/config_defaults.py
@@ -188,7 +188,7 @@ class Config(object):
     SECURITY_PASSWORD_CHECK_BREACHED = (
         "best-effort"  # will try to check https://api.pwnedpasswords.com
     )
-
+SECURITY_PASSWORD_BREACHED_COUNT = 3
 
 #  names of settings which cannot be None
 #  SECRET_KEY is also required but utils.app_utils.set_secret_key takes care of this better.
