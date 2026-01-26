485349e425b6efe40b5abbfed45aaf457bbd2246
Ahmad-Wahid <ahmedwahid16101@gmail.com>
2026-01-26 18:34:58 +0100

    add commitments for multi group
    Signed-off-by: Ahmad-Wahid <ahmedwahid16101@gmail.com>

--- flexmeasures/data/models/planning/tests/test_commitments.py
+++ flexmeasures/data/models/planning/tests/test_commitments.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from flexmeasures.data.models.planning import StockCommitment
-from flexmeasures.data.models.planning.utils import initialize_index
+from flexmeasures.data.models.planning import StockCommitment, FlowCommitment
+from flexmeasures.data.models.planning.utils import (
+    initialize_index,
+    add_tiny_price_slope,
+)
 from flexmeasures.data.models.planning.linear_optimization import device_scheduler
 
 
@@ -19,23 +22,36 @@ def test_multi_feed_device_scheduler_shared_buffer():
     # ---- device grouping
     device_group = pd.Series(
         {
-            0: "shared thermal buffer", # gas boiler
-            1: "shared thermal buffer", # "heat pump power"
-            2: "battery SoC", #"battery power"
+            0: "shared thermal buffer",  # gas boiler
+            1: "shared thermal buffer",  # "heat pump power"
+            2: "battery SoC",  # "battery power"
         }
     )
-
+    device_commodity = pd.Series(
+        {
+            0: "gas",  # gas boiler
+            1: "electricity",  # "heat pump power"
+            2: "electricity",  # "battery power"
+        }
+    )
+    equals = pd.Series(np.nan, index=index)
+    equals[-1] = 100
     # ---- trivial device constraints (stocks unconstrained individually)
     device_constraints = []
-    for _ in devices:
+    for d, device_name in enumerate(devices):
+        # 0 and 1 : derivative min 0
+        # 2 : derivative min = - production capacity
+
         df = pd.DataFrame(
             {
-                "min": -np.inf,
-                "max": np.inf,
+                "min": 0,
+                "max": 100,
                 "equals": np.nan,
-                "derivative min": -np.inf,
-                "derivative max": np.inf,
+                "derivative min": 0 if d in (0, 1) else -20,
+                "derivative max": 20,
                 "derivative equals": np.nan,
+                "derivative down efficiency": 0.9,
+                "derivative up efficiency": 0.9,
             },
             index=index,
         )
@@ -44,8 +60,8 @@ def test_multi_feed_device_scheduler_shared_buffer():
     # ---- no EMS-level constraints
     ems_constraints = pd.DataFrame(
         {
-            "derivative min": -np.inf,
-            "derivative max": np.inf,
+            "derivative min": -40,
+            "derivative max": 40,
         },
         index=index,
     )
@@ -53,9 +69,36 @@ def test_multi_feed_device_scheduler_shared_buffer():
     # ---- shared buffer max = 100 (soft)
     max_soc = 100.0
     breach_price = 1_000.0
+    min_soc = pd.Series(0, index=index)
+    min_soc[-1] = 100
+
+    # default commodity: electricity
+    # choice: electricity or gas
+    gas_price = pd.Series(300, index=index)
+    electricity_price = pd.Series(600, index=index, name="event_value")
+    electricity_price.iloc[12:14] = 200
+    prices = {"gas": gas_price, "electricity": electricity_price}
+
+    sloped_prices = (
+        add_tiny_price_slope(electricity_price.to_frame())
+        - electricity_price.to_frame()
+    )
 
     commitments = []
-    for d,dev in enumerate(devices):
+
+    # todo: fix this commitment for the group[boiler, heat pump] to reach 100 kW together
+    commitments.append(
+        StockCommitment(
+            name="buffer min",
+            index=index,
+            quantity=min_soc,
+            upwards_deviation_price=0,
+            downwards_deviation_price=-breach_price,
+            device=None,
+            device_group=device_group,
+        )
+    )
+    for d, dev in enumerate(devices):
         commitments.append(
             StockCommitment(
                 name="buffer max",
@@ -65,6 +108,29 @@ def test_multi_feed_device_scheduler_shared_buffer():
                 downwards_deviation_price=0,
                 device=pd.Series(d, index=index),
                 device_group=device_group,
+            )
+        )
+        commitments.append(
+            FlowCommitment(
+                name=device_commodity[d],
+                index=index,
+                quantity=0,
+                upwards_deviation_price=prices[device_commodity[d]],
+                downwards_deviation_price=prices[device_commodity[d]],
+                device=pd.Series(d, index=index),
+                device_group=device_commodity,
+            )
+        )
+
+        commitments.append(
+            FlowCommitment(
+                name="preferred_charge_sooner",
+                index=index,
+                quantity=0,
+                upwards_deviation_price=sloped_prices,
+                downwards_deviation_price=sloped_prices,
+                device=pd.Series(d, index=index),
+                device_group=device_commodity,
             )
         )
 
@@ -85,6 +151,17 @@ def test_multi_feed_device_scheduler_shared_buffer():
     #
     # i.e. NOT three (which would indicate per-device baselines)
     commitment_groups = set(commitments[0].device_group.values)
+
+    # commitment_costs = [
+    #     {
+    #         "name": "commitment_costs",
+    #         "data": {
+    #             c.name: costs
+    #             for c, costs in zip(commitments, model.commitment_costs.values())
+    #         },
+    #     },
+    # ]
+
     assert commitment_groups == {"shared thermal buffer"}
 
     # ---- key behavioural check:
