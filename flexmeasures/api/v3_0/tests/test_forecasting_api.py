61329d1b5bdb4770e73ffa3d9e111bef54ea1344
F.N. Claessen <claessen@seita.nl>
2026-02-20 16:36:22 +0100

    dev: partial fix for failing test
    Signed-off-by: F.N. Claessen <claessen@seita.nl>

--- flexmeasures/api/v3_0/tests/test_forecasting_api.py
+++ flexmeasures/api/v3_0/tests/test_forecasting_api.py
@@ -38,7 +38,9 @@ def test_trigger_and_fetch_forecasts(
         "end-date": "2025-01-05T02:00:00+00:00",
         "max-forecast-horizon": "PT1H",
         "forecast-frequency": "PT1H",
-        "retrain-frequency": "PT1H",
+        "config": {
+            "retrain-frequency": "PT1H",
+        },
     }
 
     trigger_url = url_for("SensorAPI:trigger_forecast", id=sensor_0.id)
