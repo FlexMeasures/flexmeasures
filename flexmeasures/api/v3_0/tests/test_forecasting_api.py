d0f0bf65bf2a29dfddfcad19f4a6c4cf40c61630
F.N. Claessen <claessen@seita.nl>
2026-02-20 14:18:10 +0100

    fix: update test case that tries to get two cycles out of the API
    Signed-off-by: F.N. Claessen <claessen@seita.nl>

--- flexmeasures/api/v3_0/tests/test_forecasting_api.py
+++ flexmeasures/api/v3_0/tests/test_forecasting_api.py
@@ -37,6 +37,7 @@ def test_trigger_and_fetch_forecasts(
         "start-predict-date": "2025-01-05T00:00:00+00:00",
         "end-date": "2025-01-05T02:00:00+00:00",
         "max-forecast-horizon": "PT1H",
+        "forecast-frequency": "PT1H",
         "retrain-frequency": "PT1H",
     }
 
