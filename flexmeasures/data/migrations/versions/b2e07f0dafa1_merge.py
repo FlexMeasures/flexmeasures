4724959678efc2a38ef3508bfeb57271c133f7f9
F.N. Claessen <claessen@seita.nl>
2026-04-02 15:36:52 +0200

    style: flake8
    Signed-off-by: F.N. Claessen <claessen@seita.nl>

--- flexmeasures/data/migrations/versions/b2e07f0dafa1_merge.py
+++ flexmeasures/data/migrations/versions/b2e07f0dafa1_merge.py
@@ -1,14 +1,10 @@
 """merge
 
 Revision ID: b2e07f0dafa1
-Revises: 
+Revises: 3f4a6f9d2b11, b2c3d4e5f6a7
 Create Date: 2026-04-02 15:21:56.334943
 
 """
-
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = "b2e07f0dafa1"
