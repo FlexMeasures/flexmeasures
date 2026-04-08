42e664b3e1ae8d199ddaaf3eeedc9696cadcc56f
Nicolas Höning <nicolas@seita.nl>
2026-04-08 13:50:46 +0200

    add merge migration
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

---
+++ flexmeasures/data/migrations/versions/ea19499c79b5_merge.py
@@ -0,0 +1,24 @@
+"""merge
+
+Revision ID: ea19499c79b5
+Revises: 3f4a6f9d2b11, a5b26c3f8e91
+Create Date: 2026-04-08 13:50:02.009717
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ea19499c79b5'
+down_revision = ('3f4a6f9d2b11', 'a5b26c3f8e91')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
