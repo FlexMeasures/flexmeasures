d43b41d914e7b0bd7cf8b30a9ad54621ba5da542
F.N. Claessen <claessen@seita.nl>
2026-04-13 12:12:02 +0200

    fix: merge db revisions
    Signed-off-by: F.N. Claessen <claessen@seita.nl>

---
+++ flexmeasures/data/migrations/versions/2c6a4c8127c1_merge.py
@@ -0,0 +1,21 @@
+"""merge
+
+Revision ID: 2c6a4c8127c1
+Revises: 
+Create Date: 2026-04-13 12:10:52.322376
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "2c6a4c8127c1"
+down_revision = ("b2e07f0dafa1", "e26d02ed1621")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
