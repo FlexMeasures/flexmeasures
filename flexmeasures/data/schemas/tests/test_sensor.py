10fc632192785b28caf225590bfbb43b90501fbc
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2026-05-22 14:42:53 +0100

    test: compare quantities by value
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- flexmeasures/data/schemas/tests/test_sensor.py
+++ flexmeasures/data/schemas/tests/test_sensor.py
@@ -11,6 +11,11 @@ from flexmeasures.utils.unit_utils import ur
 from marshmallow import ValidationError
 
 
+def assert_quantity_matches(actual_quantity, expected_quantity) -> None:
+    assert str(actual_quantity.units) == str(expected_quantity.units)
+    assert actual_quantity.magnitude == pytest.approx(expected_quantity.magnitude)
+
+
 @pytest.mark.parametrize(
     "src_quantity, dst_unit, fails, exp_dst_quantity",
     [
@@ -84,10 +89,12 @@ def test_quantity_or_sensor_deserialize(
     try:
         dst_quantity = schema.deserialize(src_quantity)
         if isinstance(src_quantity, (ur.Quantity, int, float)):
-            assert dst_quantity == ur.Quantity(exp_dst_quantity)
+            assert_quantity_matches(dst_quantity, ur.Quantity(exp_dst_quantity))
             assert str(dst_quantity) == exp_dst_quantity
         elif isinstance(src_quantity, list):
-            assert dst_quantity[0]["value"] == ur.Quantity(exp_dst_quantity)
+            assert_quantity_matches(
+                dst_quantity[0]["value"], ur.Quantity(exp_dst_quantity)
+            )
             assert str(dst_quantity[0]["value"]) == exp_dst_quantity
         assert not fails
     except ValidationError as e:
