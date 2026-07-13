96a835840095b1e216ddeb2b33b8a93b309d17f4
Nicolas Höning <nicolas@seita.nl>
2026-07-13 20:49:23 +0200

    Chore: check on startup if the db schema is on alembic head  (#2309)
    * chore: check on startup if the db schema is on alembic head and print a warning otherwise - useful for hosts. Concrete use case: do not creste templates in this case, so db CLI commands are still free to run.
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * add changelog entry
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * fix missing app context
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * do not show the warning when we already run the upgrade, also show the actual revisions to be more useful
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * upgrade the warning to error status to increase visibility, add the revision identifiers as well, adapt warning if db connectivity is the problem
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    * more robust check if we are running db upgrade
    
    Co-authored-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>
    Signed-off-by: Nicolas Höning <iam@nicolashoening.de>
    
    * do without the util function database_schema_is_migrated_to_head()
    
    Co-authored-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>
    Signed-off-by: Nicolas Höning <iam@nicolashoening.de>
    
    ---------
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    Signed-off-by: Nicolas Höning <iam@nicolashoening.de>
    Co-authored-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>
    (cherry picked from commit 8d775f602b18b6e04ef36fba83df2bd825e19089)
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures/data/__init__.py
+++ flexmeasures/data/__init__.py
@@ -3,6 +3,7 @@ Models & schemata, as well as business logic (queries & services).
 """
 
 import os
+import sys
 
 from flask import Flask
 from flask_migrate import Migrate
@@ -15,10 +16,42 @@ from flexmeasures.data.transactional import after_request_exception_rollback_ses
 ma: Marshmallow = Marshmallow()
 
 
+def _is_running_db_upgrade_command() -> bool:
+    """Return whether this process is already running the Alembic upgrade command."""
+    args = sys.argv[1:]
+    return any(args[i : i + 2] == ["db", "upgrade"] for i in range(len(args) - 1))
+
+
 def register_at(app: Flask):
     # First configure the central db object and Alembic's migration tool
     configure_db_for(app)
     Migrate(app, db, directory=os.path.join(app.root_path, "data", "migrations"))
+
+    app.database_schema_is_migrated_to_head = True
+    if not app.testing and app.config.get("FLEXMEASURES_ENV") != "documentation":
+        from flexmeasures.data.utils import (
+            format_database_schema_revision_status,
+            get_database_schema_revision_status,
+        )
+
+        revision_status = get_database_schema_revision_status(app)
+        app.database_schema_is_migrated_to_head = revision_status.is_migrated_to_head
+        if (
+            not app.database_schema_is_migrated_to_head
+            and not _is_running_db_upgrade_command()
+        ):
+            if revision_status.inspection_error is not None:
+                app.logger.error(
+                    "Could not determine the database schema revision. "
+                    "Check database connectivity and configuration before starting the app. "
+                    f"Details: {revision_status.inspection_error}"
+                )
+            else:
+                app.logger.error(
+                    "Database schema is not at the Alembic head revision "
+                    f"({format_database_schema_revision_status(revision_status)}). "
+                    "Run `flexmeasures db upgrade` before starting the app."
+                )
 
     global ma
     ma.init_app(app)
